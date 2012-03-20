1f3a4e81b3d51d6fee6cde103da1a3e9f2169abc
Anurag Biyani <abiyani@dnanexus.com>
2012-03-20 13:11:40 -0700

    Added README

--- lib/cpp/dxjson/make_standard_lib.sh
+++ lib/cpp/dxjson/make_standard_lib.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+make clean
+cmake .
+make
 (ls /usr/local/lib >/dev/null 2>/dev/null) || sudo mkdir /usr/local/lib
 sudo cp libdxjson.a /usr/local/lib/
 (ls /usr/local/include/dxjson >/dev/null 2>/dev/null) || sudo mkdir /usr/local/include/dxjson
