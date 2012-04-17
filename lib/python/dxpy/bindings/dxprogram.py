ed1599a73a46595a70c4bc46f96724798bb2c7a9
Katherine Lai <klai@dnanexus.com>
2012-04-17 15:35:10 -0700

    updated dxprogram to point to program_builder; updated doc config file

--- lib/python/dxpy/bindings/dxprogram.py
+++ lib/python/dxpy/bindings/dxprogram.py
@@ -8,48 +8,6 @@ from dxpy.bindings import *
 # DXProgram #
 #############
 
-def make_run_spec(interpreter,
-                  codefile=None, codelocalfile=None, codestring=None,
-                  bundled_depends=None, exec_depends=None):
-    '''
-    :param interpreter: name of the interpreter to use (e.g. "v8cgi", "python2.7")
-    :type interpreter: string
-    :param codefile: file object ID containing code to be run
-    :type codefile: string
-    :param codelocalfile: local filename of the file containing code to be run
-    :type codelocalfile: string
-    :param codestring: code to be run
-    :type codestring: string
-    :param bundled_depends: list of assets the program requires
-    :type bundled_depends: list of dicts
-    :param exec_depends: list of package names and versions required
-    :type exec_depends: list of dicts
-
-    TODO: Flesh out stuff.  Would it be better if run_spec were a
-    class/struct?
-
-    '''
-    pass
-
-def new_dxprogram(run_spec, input_spec=None, output_spec=None, **kwargs):
-    '''
-
-    :rtype: :class:`dxpy.bindings.DXProgram`
-
-    Creates a new program with the given code.  See
-    :meth:`dxpy.bindings.DXProgram.new` for behavior.
-
-    Note that this function is shorthand for::
-
-        dxprogram = DXProgram()
-        dxprogram.new(code_file)
-
-    '''
-
-    dxprogram = DXProgram()
-    dxprogram.new(code_file, code_string)
-    return dxprogram
-
 class DXProgram(DXDataObject):
     '''
     Remote program object handler
@@ -76,29 +34,29 @@ class DXProgram(DXDataObject):
         '''
         :param dx_hash: Standard hash populated in :func:`dxpy.bindings.DXDataObject.new()`
         :type dx_hash: dict
-        :param codefile: filename containing code to be run
-        :type codefile: string
-        :param codestring: code to be run
-        :type codestring: string
+        :param inputs: input specification (optional)
+        :type inputs: dict
+        :param outputs: output specification (optional)
+        :type outputs: dict
+        :param run: run specification
+        :type run: dict
 
-        Creates a program with the code provided.  Exactly one argument
-        between codefile and codestring should be given.  The program is
-        not run until :meth:`dxpy.bindings.DXProgram.run` is called.
+        It is highly recommended that :mod:`dxpy.program_builder` is
+        used for program creation.
+
+        Creates a program with the given parameters (see API
+        documentation for the correct syntax).  The program is not run
+        until :meth:`run()` is called.
 
         '''
-
-        if code_file is not None:
-            if code_string is not None:
-                raise DXProgramError("Expecting 1 argument for code and got"+
-                                 " both code_file and code_string")
-            with open(code_file, 'r') as codefd:
-                code_string = codefd.read()
-        elif code_string is None:
-            raise DXProgramError("Expecting 1 argument for code and got"+
-                             " neither code_file nor code_string")
-
-        resp = dxpy.api.programNew({"code": code_string})
-        self.set_id(resp["id"])
+        if "inputs" in kwargs:
+            dx_hash["inputs"] = kwargs["inputs"]
+        if "outputs" in kwargs:
+            dx_hash["outputs"] = kwargs["outputs"]
+        if "run" in kwargs:
+            dx_hash["run"] = kwargs["run"]
+        resp = dxpy.api.programNew(dx_hash)
+        self.set_ids(resp["id"], dx_hash["project"])
 
     def get(self):
         """
@@ -106,7 +64,7 @@ class DXProgram(DXDataObject):
         """
         return dxpy.api.programGet(self._dxid)
 
-    def run(self, program_input):
+    def run(self, program_input, project=None, folder="/"):
         '''
         :param program_input: Hash of the program's input arguments
         :type program_input: dict
@@ -117,5 +75,9 @@ class DXProgram(DXDataObject):
         with the given input *program_input*.
 
         '''
+        if project is None:
+            project = self._proj
 
-        return DXJob(dxpy.api.programRun(self._dxid, {"input": program_input})["id"])
+        return DXJob(dxpy.api.programRun(self._dxid, {"input": program_input,
+                                                      "project": project,
+                                                      "folder": folder})["id"])
