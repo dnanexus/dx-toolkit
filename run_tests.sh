444affbe2cb6b47e7804302245cfd309ea91d69a
Phil Sung <psung@dnanexus.com>
2012-04-25 12:00:07 -0700

    Make client/run_tests.sh use new env variables.

--- run_tests.sh
+++ run_tests.sh
@@ -10,8 +10,8 @@ root=`dirname "$0"`
 
 source "$root/environment"
 export DB_PORT=`../bin/nuc-config mongod.port`
-export APISERVER_PORT=`../bin/nuc-config apiserver.port`
-export APISERVER_HOST=localhost
+export DX_APISERVER_PORT=`../bin/nuc-config apiserver.port`
+export DX_APISERVER_HOST=localhost
 export PYTHONPATH="$root/../execserver/env/lib/python2.7/site-packages:$PYTHONPATH"
 
 dbpath="$root/test_db"
