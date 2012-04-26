edc321bf794cfd14ca4ff5e802a69b4840a5145e
Joseph Dale <jdale@dnanexus.com>
2012-04-25 18:42:17 -0700

    projectId -> project, objectId -> id in search results.

--- lib/python/dxpy/program_builder.py
+++ lib/python/dxpy/program_builder.py
@@ -51,11 +51,11 @@ def upload_program(src_dir, uploaded_resources, check_name_collisions=True, over
         logging.debug("Searching for programs with name " + program_spec["name"])
         for result in dxpy.find_data_objects(classname="program", properties={"name": program_spec["name"]}, project=program_spec['project']):
             if overwrite:
-                logging.info("Deleting program %s" % (result['objectId']))
+                logging.info("Deleting program %s" % (result['id']))
                 # TODO: test me
-                dxpy.DXProject(program_spec['project']).remove_objects([result['objectId']])
+                dxpy.DXProject(program_spec['project']).remove_objects([result['id']])
             else:
-                raise ProgramBuilderException("A program with name %s already exists (id %s) and the overwrite option was not given" % (program_spec["name"], result['objectId']))
+                raise ProgramBuilderException("A program with name %s already exists (id %s) and the overwrite option was not given" % (program_spec["name"], result['id']))
 
     if "run" in program_spec and "file" in program_spec["run"]:
         code_filename = os.path.join(src_dir, program_spec["run"]["file"])
