d71b1e68ea4063d5d8f6381a691f8381ee9ff168
Joseph Dale <jdale@dnanexus.com>
2012-05-03 16:28:30 -0700

    Formatting.

--- lib/cpp/SimpleHttpLib/SimpleHttpHeaders.cpp
+++ lib/cpp/SimpleHttpLib/SimpleHttpHeaders.cpp
@@ -2,10 +2,12 @@
 
 void HttpHeaders::appendHeaderString(const std::string &s) {
   using namespace std;
-  using namespace HttpHelperUtils; 
+  using namespace HttpHelperUtils;
+
   // See: http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
   pair<string, std::string> h = splitOnFirstColon(s);
   h.second = stripWhitespaces(h.second);
+
   // If header field-name is already present, append the content with ","
   if (isPresent(h.first))
     header[h.first] += "," + h.second;
