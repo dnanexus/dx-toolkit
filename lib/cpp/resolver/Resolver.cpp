34931ef1c4e03afb7e1aeec41eda0432b277f17d
George Asimenos <george+github@dnanexus.com>
2012-06-05 19:24:50 -0700

    Changing resolver interface

--- lib/cpp/resolver/Resolver.cpp
+++ lib/cpp/resolver/Resolver.cpp
@@ -22,113 +22,99 @@ vector<string> Split(const string &s, char delim)
   return results;
 }
 
-Path::Path(const string &path_, const string &default_project)
+ObjectInfo::ObjectInfo(const string &path_, const string &default_project)
 {
-  string path = path_;
+  path = path_;
   vector<string> chunks = Split(path, ':');
   if (chunks.size() == 1)
     path = chunks[0];
   else if (chunks.size() == 2)
   {
-    project = chunks[0];
+    project.name = chunks[0];
     path = chunks[1];
   }
   else
     throw "Too many ':' in path '" + path + "'";
 
-  folder = "/";
+  object.folder = "/";
   chunks = Split(path, '/');
   for (unsigned i = 0; i != chunks.size(); ++i)
   {
-    if (name != "")
-      folder += (folder[folder.size()-1] != '/' ? "/" : "") + name;
-    name = chunks[i];
+    if (object.name != "")
+      object.folder += (object.folder[object.folder.size()-1] != '/' ? "/" : "") + object.name;
+    object.name = chunks[i];
   }
 
-  if (name == "")
-    throw "Empty name in path '" + path + "'";
+  if (object.name == "")
+    throw "Empty name in path '" + path_ + "'";
 
-  if (project == "")
-    project = default_project;
-}
+  if (project.name == "")
+    project.name = default_project;
 
-string Path::toString()
-{
-  return project + ":" + folder + (folder[folder.size()-1] == '/' ? "" : "/") + name;
-}
-
-Path Resolver::ParsePath(const string &path)
-{
-  return Path(path, default_project);
-}
-
-string Resolver::FindProject(const string &project)
-{
-  AutoLock pcl(project_cache_lock);
-  if (project_cache.find(project) != project_cache.end())
-    return project_cache[project];
-  pcl.Release();
-
-  string project_id = LookupProject(project);
-  if (project_id != "")
+  if (ObjectInfo::IsProjectId(project.name))
   {
-    pcl.Acquire();
-    project_cache[project] = project_id;
+    project.id = project.name;
+    project.name = "";
   }
-  return project_id;
-}
 
-string Resolver::EnsureProject(const string &project)
-{
-  string project_id = FindProject(project);
-  if (project_id == "")
-    project_id = CreateProject(project);
-
-  AutoLock pcl(project_cache_lock);
-  project_cache[project] = project_id;
-  return project_id;
-}
-
-Path Resolver::FindPath(const string &path)
-{
-  AutoLock pcl(path_cache_lock);
-  if (path_cache.find(path) != path_cache.end())
-    return path_cache[path];
-  pcl.Release();
-
-  Path p = ParsePath(path);
-  p.project_id = FindProject(p.project);
-  if (p.project_id == "")
-    return p;
-
-  p.object_id = LookupPath(p.project_id, p.name, p.folder);
-  if (p.object_id != "")
+  if (ObjectInfo::IsObjectId(object.name))
   {
-    pcl.Acquire();
-    path_cache[path] = p;
+    object.id = object.name;
+    object.name = "";
   }
-  return p;
+
+  path = path_;
 }
 
-Path Resolver::PreparePath(const string &path)
+bool ObjectInfo::IsId(const string &s)
 {
-  Path p = FindPath(path);
-  if (p.project_id == "")
-    p.project_id = EnsureProject(p.project);
+  const char alphabet[] = "0123456789BFGJKPQVXYZbfgjkpqvxyz";
 
-  if (p.object_id != "")
+  if (s.size() != 24)
+    return false;
+
+  for (unsigned i = 0; i != s.size(); ++i)
   {
-    DeleteObject(p.project_id, p.object_id);
-    p.object_id = "";
-    AutoLock pcl(path_cache_lock);
-    path_cache.erase(path);
+    bool found = false;
+    for (unsigned j = 0; j != sizeof(alphabet); ++j)
+    {
+      if (s[i] == alphabet[j])
+      {
+        found = true;
+        break;
+      }
+    }
+    if (!found)
+      return false;
   }
-  return p;
+  
+  return true;
 }
 
-string Resolver::LookupProject(const string &project)
+bool ObjectInfo::IsObjectId(const string &s)
 {
-  if ((project.size() == 32) && (project.substr(0, 8) == "project-"))
+  vector<string> pieces  = Split(s, '-');
+  return ((pieces.size() == 2) &&
+          (pieces[0] == "record" || pieces[0] == "gtable" || pieces[0] == "file" || pieces[0] == "table" || pieces[0] == "program") &&
+          (IsId(pieces[1])));
+}
+
+bool ObjectInfo::IsProjectId(const string &s)
+{
+  vector<string> pieces  = Split(s, '-');
+  return ((pieces.size() == 2) &&
+          (pieces[0] == "project") &&
+          (IsId(pieces[1])));
+}
+
+ObjectInfo Resolver::ParsePath(const string &path) const
+{
+  return ObjectInfo(path, default_project);
+}
+
+string Resolver::FindProject(const string &project) const
+{
+  if (ObjectInfo::IsProjectId(project))
     return project;
 
   dx::JSON results = DXHTTPRequest("/system/findProjects", "{\"describe\": true}")["results"];
@@ -139,30 +125,100 @@ string Resolver::LookupProject(const string &project)
   return "";
 }
 
-string Resolver::LookupPath(const string &project_id, const string &name, const string &folder)
+string Resolver::EnsureProject(const string &project) const
+{
+  string project_id = FindProject(project);
+  if (project_id == "")
+    project_id = CreateProject(project);
+
+  return project_id;
+}
+
+ObjectInfo Resolver::FindPath(const string &path) const
+{
+  ObjectInfo oi = ParsePath(path);
+
+  if (oi.object.id != "")
+  {
+    if (oi.project.id == "")
+      oi.project.id = FindProject(oi.project.name);
+
+    DescribeObject(oi.project.id, oi.object.id, oi);
+
+    oi.project.name = GetProjectName(oi.project.id);
+  }
+  else
+  {
+    if (oi.project.id == "")
+      oi.project.id = FindProject(oi.project.name);
+    else
+      oi.project.name = GetProjectName(oi.project.id);
+
+    LookupPath(oi.project.id, oi.object.name, oi.object.folder, oi);
+  }
+  return oi;
+}
+
+ObjectInfo Resolver::PreparePath(const string &path) const
+{
+  ObjectInfo oi = ParsePath(path);
+  if (oi.project.id == "")
+    oi.project.id = EnsureProject(oi.project.name);
+
+  oi = FindPath(path);
+
+  if (oi.object.id != "")
+    DeleteObject(oi.project.id, oi.object.id);
+  
+  oi.object.id = "";
+  return oi;
+}
+
+void Resolver::LookupPath(const string &project_id, const string &name, const string &folder, ObjectInfo &oi) const
 {
   dx::JSON input = dx::JSON(dx::JSON_HASH);
   input["name"] = name;
   input["visibility"] = "either";
+  input["describe"] = true;
   input["scope"] = dx::JSON(dx::JSON_HASH);
   input["scope"]["project"] = project_id;
   input["scope"]["folder"] = folder;
   input["scope"]["recurse"] = true;
   dx::JSON results = DXHTTPRequest("/system/findDataObjects", input.toString());
   if (results["results"].size() == 0)
-    return "";
+    return;
+  if (results["results"].size() > 1)
+    throw "Object collision";
 
-  return results["results"][0]["id"].get<string>();
+  oi.object.id = results["results"][0]["id"].get<string>();
+  oi.object.folder = results["results"][0]["describe"]["folder"].get<string>();
 }
 
-void Resolver::DeleteObject(const string &project_id, const string &object_id)
+void Resolver::DeleteObject(const string &project_id, const string &object_id) const
 {
   DXHTTPRequest("/" + project_id + "/removeObjects", "{\"objects\": [\"" + object_id + "\"], \"includeOrphanedHiddenLinks\": false}");
 }
 
-string Resolver::CreateProject(const string &name)
+string Resolver::CreateProject(const string &name) const
 {
   dx::JSON input = dx::JSON(dx::JSON_HASH);
   input["name"] = name;
   return DXHTTPRequest("/project/new", input.toString())["id"].get<string>();
+}
+
+string Resolver::GetProjectName(const string &project_id) const
+{
+  return DXHTTPRequest("/" + project_id + "/describe", "{}")["name"].get<string>();
+}
+
+void Resolver::DescribeObject(const string &project_id, const string &object_id, ObjectInfo &oi) const
+{
+  string input = "{}";
+  if (project_id != "")
+    input = "{\"project\": \"" + project_id + "\"}";
+  dx::JSON desc = DXHTTPRequest("/" + object_id + "/describe", input);
+
+  oi.project.id = desc["project"].get<string>();
+  oi.object.name = desc["name"].get<string>();
+  oi.object.folder = desc["folder"].get<string>();
 }
--- lib/cpp/resolver/Resolver.h
+++ lib/cpp/resolver/Resolver.h
@@ -5,8 +5,6 @@
 #include <string>
 #include <vector>
 
-#include "Thread.h"
-
 using namespace std;
 
 // Resolver
@@ -14,8 +12,12 @@ using namespace std;
 // Resolver is an unofficial, thin layer that facilitates symbolic name resolution.
 // Resolver can be used to resolve object paths, and other operations describe below.
 //
-// An object path is a string of the form: [ <project> ":" ] [ <folder_prefix> ] name
+// An object path is a string of the form:
+//
+// [ <project_id_or_name> ":" ] [ <folder_prefix> ] object_id_or_name
+//
 // Examples of object paths:
+// ------------------------
 //
 //   hg18
 //   /hg18 (same as above)
@@ -26,6 +28,8 @@ using namespace std;
 //   seed:genomes/hg18
 //   seed:/genomes/hg18 (same as above)
 //   project-000000000000000000000001:hg18
+//   record-9z4Qpfpyjv9FbvB5qVb00012
+//   project-9yzy0b2yjv9P7Zyv2v2Q00FK:record-9z4Qpfpyjv9FbvB5qVb00012
 //
 // How to use resolver:
 // -------------------
@@ -35,12 +39,47 @@ using namespace std;
 //   Resolver resolver(default_project);
 //   Resolver resolver("seed");
 //
-// To use the resolver to "parse" a path into parts:
+// Object lookup helpers:
+// ----------------------
 //
-//   Path p = resolver.ParsePath("genomes/hg18");
-//   cerr << p.project  // Prints "seed" (or whatever you gave to constructor)
-//   cerr << p.folder   // Prints "/genomes"
-//   cerr << p.name     // Prints "hg18"
+// To look up a path:
+//
+//   ObjectInfo oi = resolver.FindPath(object_path)
+//
+//   ObjectInfo contains the following string fields:
+//
+//     project.id
+//     project.name
+//     object.id
+//     object.name
+//     object.folder
+//
+//   Lookup works in the following way:
+//
+//   Using object names:
+//
+//   object_name -> alias for default_project:/object_name
+//   project_name:object_name -> alias for project_name:/object_name
+//   project_id:object_name -> alias for project_id:/object_name
+//   project_name:/some/folder/object_name -> resolves project_name to project.id using findProjects
+//                                            fills in rest of details using findDataObject with:
+//                                              name: object_name
+//                                              scope.project: project.id
+//                                              scope:folder: /some/folder
+//                                              scope:recurse: true
+//   project_id:/some/folder/object_name -> fills in project.name with project-xxxx/describe
+//                                          fills in rest of details using findDataObject as above
+//
+//   Using object ids: (folders in the path are ignored, but real folder is returned in ObjectInfo)
+//
+//   object_id -> alias for default_project:object_id
+//   project_name:object_id -> resolves project_name to project.id using findProjects
+//                             calls describe with particular object_id and project.id
+//                             overwrites project.id from describe's response
+//                             calls project-xxxx/describe to fill in project.name (may be different than input)
+//   project_id:object_id -> calls describe with particular object_id and project_id
+//                           overwrites project.id from describe's response
+//                           calls project-xxxx/describe to fill in project.name (may be different than input)
 //
 // To use the resolver to convert from project name to project id:
 //
@@ -53,24 +92,21 @@ using namespace std;
 //   // Returns an existing or new project_id
 //   string project_id = resolver.EnsureProject("seed");
 //
-// To use the resolver to convert from object path to object id:
+// Object creation helpers:
+// ------------------------
 //
-//   Path p = resolver.FindPath("genomes/hg18");
-//   cerr << p.project    // Prints "seed" (or whatever you gave to constructor)
-//   cerr << p.folder     // Prints "/genomes"
-//   cerr << p.name       // Prints "hg18"
-//   cerr << p.project_id // Prints the project id of "seed" (if the project exists)
-//   cerr << p.object_id  // Prints the object id of an object named "hg18" under any folder
-//                        // starting from "/genomes" inside project p.project_id (if
-//                        // such an object exists)
+// (The following functions work only with object names, not object ids)
 //
 // To use the resolver to "prepare for the creation of a path", that is, to ensure
 // that a particular project exists but also that a particular object does NOT
 // exist:
 //
-//   Path p = resolver.PreparePath("genomes/hg18");
+//   ObjectInfo oi = resolver.PreparePath(object_path)
 //   // Creates project "seed" if it doesn't exist, and removes object "hg18"
 //   // if it exists in that project.
+//   
+//   ObjectInfo in that case does not contain object.id, and the object.name and
+//   object.folder are taken from parsing the path
 //
 // Why would you want this? Symbolic lookup is only possible if you have a single
 // copy of an object with a particular name, otherwise the lookup is ambiguous.
@@ -79,20 +115,44 @@ using namespace std;
 // "command > FOO" would do on UNIX; it always 'overwrites' FOO, and the way to
 // emulate this behavior here is by first calling PreparePath before you make
 // a new FOO).
+//
+// To parse a path:
+//
+//   ObjectInfo oi = resolver.ParsePath(object_path);
+//   
+//   Fills in only project.name, object.name, object.path
 
-class Path
+class ObjectInfo
 {
   public:
 
-  string project;
-  string folder;
-  string name;
-  string project_id;
-  string object_id;
+  class Project
+  {
+    public:
 
-  Path() { }
-  Path(const string &path, const string &default_project = "");
-  string toString();
+    string id;
+    string name;
+  };
+
+  class Object
+  {
+    public:
+
+    string id;
+    string name;
+    string folder;
+  };
+
+  Project project;
+  Object object;
+  string path;
+
+  ObjectInfo() { }
+  ObjectInfo(const string &path_, const string &default_project = "");
+
+  static bool IsObjectId(const string &s);
+  static bool IsProjectId(const string &s);
+  static bool IsId(const string &s);
 };
 
 class Resolver
@@ -102,26 +162,22 @@ class Resolver
   Resolver() { }
   Resolver(const string &default_project_) : default_project(default_project_) { }
 
-  Path ParsePath(const string &path);
+  ObjectInfo FindPath(const string &path) const;
+  string FindProject(const string &project) const;
+  string EnsureProject(const string &project) const;
 
-  string FindProject(const string &project);
-  string EnsureProject(const string &project);
-
-  Path FindPath(const string &path);
-  Path PreparePath(const string &path);
+  ObjectInfo ParsePath(const string &path) const;
+  ObjectInfo PreparePath(const string &path) const;
 
   private:
 
-  string LookupProject(const string &project);
-  string LookupPath(const string &project_id, const string &name, const string &folder);
-  void DeleteObject(const string &project_id, const string &object_id);
-  string CreateProject(const string &name);
+  void DescribeObject(const string &project_id, const string &object_id, ObjectInfo &oi) const;
+  void LookupPath(const string &project_id, const string &name, const string &folder, ObjectInfo &oi) const;
+  void DeleteObject(const string &project_id, const string &object_id) const;
+  string CreateProject(const string &name) const;
+  string GetProjectName(const string &project_id) const;
 
   string default_project;
-  map<string, string> project_cache;
-  map<string, Path> path_cache;
-  Lock project_cache_lock;
-  Lock path_cache_lock;
 };
 
 #endif
