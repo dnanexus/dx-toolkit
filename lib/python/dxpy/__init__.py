9c7544896467caab24ddb046ec1bb67194de44fc
Andrey Kislyuk <akislyuk@dnanexus.com>
2012-06-18 23:24:44 -0700

    also disable gevent

--- lib/python/dxpy/__init__.py
+++ lib/python/dxpy/__init__.py
@@ -32,12 +32,12 @@ try:
 except ImportError:
     snappy_available = False
 
-gevent_available = True
-try:
-    import gevent, gevent.pool, gevent.monkey
-    gevent.monkey.patch_all(thread=False, select=False)
-except ImportError:
-    gevent_available = False
+#gevent_available = True
+#try:
+#    import gevent, gevent.pool, gevent.monkey
+#    gevent.monkey.patch_all(thread=False, select=False)
+#except ImportError:
+#    gevent_available = False
 
 API_VERSION = '1.0.0'
 AUTH_HELPER = None
