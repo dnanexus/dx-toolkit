fe47052080436fba5c1895b247e428b1524c7aa6
Andrey Kislyuk <kislyuk@gmail.com>
2012-08-05 01:48:36 -0700

    dx_watch -> dx watch

---
+++ src/python/dxpy/utils/job_log_client.py
@@ -0,0 +1,22 @@
+import os, sys, json, re
+from ws4py.client.threadedclient import WebSocketClient
+
+class DXJobLogStreamClient(WebSocketClient):
+    def opened(self):
+        self.send('5:1::' + json.dumps({"name": "streamingId", "args": [streaming_id]}))
+
+    def closed(self, code, reason):
+        print "Log socket closed:", code, reason
+
+    def received_message(self, message):
+        if not re.match("^([^:]+):([^:]*):([^:]*):(.+)", str(message)):
+            return
+
+        msg_type, msg_id, msg_endpoint, msg_data = re.match("^([^:]+):([^:]*):([^:]*):(.+)", str(message)).groups()
+        if msg_type == '5':
+            try:
+                msg_content = json.loads(msg_data)
+                if (msg_content["name"] == "log"):
+                    print msg_content["args"][0]
+            except:
+                print "Error while decoding message:", str(msg_content)
