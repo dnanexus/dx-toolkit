b56ae7c72ae4261a343054fdac749c2d23a852ec
Joseph Dale <jdale@dnanexus.com>
2012-08-13 20:55:07 -0700

    Add git version string to UA welcome message.

--- src/ua/main.cpp
+++ src/ua/main.cpp
@@ -330,7 +330,7 @@ void curlCleanup() {
 }
 
 int main(int argc, char * argv[]) {
-  LOG << "DNAnexus Upload Agent" << endl;
+  LOG << "DNAnexus Upload Agent " << GITVERSION << endl;
 
   try {
     opt.parse(argc, argv);
