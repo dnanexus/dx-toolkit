5a8588c58c91c4a1fd3d89926434b019407b80b0
Katherine Lai <klai@dnanexus.com>
2012-08-24 15:11:24 -0700

    adding --allprojects flag and providing a better error message when no project context is set for ls

--- src/python/dxpy/utils/resolver.py
+++ src/python/dxpy/utils/resolver.py
@@ -403,7 +403,9 @@ def resolve_existing_path(path, expected=None, ask_to_resolve=True, expected_cla
 
     project, folderpath, entity_name = resolve_path(path, expected)
 
-    if entity_name is None:
+    if project is None:
+        raise ResolutionError('Could not resolve \"' + path + '\" to a project context.  You should either set a default project using dx select or cd, or add a colon (":") after your project ID or name.')
+    elif entity_name is None:
         # Definitely a folder (or project)
         # FIXME? Should I check that the folder exists if expected="folder"?
         return project, folderpath, entity_name
