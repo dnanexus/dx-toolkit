2e453945ecdf525b3f118f66707d90997b52ad36
Phil Sung <psung@dnanexus.com>
2012-08-30 18:58:25 -0700

    Update C++ bindings home page and DX{App,Applet,Container,Project} docs.

--- src/cpp/dxcpp/bindings/dxproject.h
+++ src/cpp/dxcpp/bindings/dxproject.h
@@ -1,61 +1,98 @@
+/** \file
+ *
+ * \brief Containers and projects.
+ *
+ * Data containers (DXContainer) and projects (DXProject, a subclass of %DXContainer).
+ */
+
 #ifndef DXCPP_BINDINGS_DXPROJECT_H
 #define DXCPP_BINDINGS_DXPROJECT_H
 
 #include "../bindings.h"
 
+//! A generic container for data objects.
+
+///
+/// Every data object on the DNAnexus Platform must reside in a data container. When an object is
+/// first created, it may only reside in a single container. However, objects may be cloned into
+/// other containers once the objects have been closed (and their contents may no longer be
+/// modified). See <a
+/// href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Data-Object-Lifecycle">Data Object
+/// Lifecycle</a> in the API specification for more information.
+///
+/// Projects (DXProject) are containers that provide additional functionality for collaboration
+/// between users.
+///
+
 class DXContainer {
  private:
   std::string dxid_;
- public:
- DXContainer(const std::string &dxid=g_WORKSPACE_ID) : dxid_(dxid) { }
 
-  /** 
-   * @param dxid Associate the handler to given data container id.
-   * @note No error checking is done in this function
+ public:
+
+  /**
+   * Creates a %DXContainer handler for the specified data container ID.
+   *
+   * @param dxid A string containing a data container ID.
+   */
+  DXContainer(const std::string &dxid=g_WORKSPACE_ID) : dxid_(dxid) { }
+
+  /**
+   * @param dxid Associates the handler with the given data container ID.
+   *
+   * @note No error checking is done in this function.
    */
   void setID(const std::string &dxid) { dxid_ = dxid; }
-  
+
   /**
-   * @return ID of the associated data container
+   * @return ID of the associated data container.
    */
   std::string getID() const { return dxid_; }
-  
+
   /**
-   * Default conversion to string is to its project ID so a handler
-   * can always be passed in place of a string argument that expects
-   * a project ID.
+   * This default conversion to string returns the container ID so a handler can always be passed
+   * in place of a string argument that expects a container or project ID.
+   *
+   * @return ID of the associated data container.
    */
   operator std::string() { return dxid_;}
 
   /**
-   * @param folders If true, return list of folders in the data container
-   * @return The output of describe call (JSON hash) on the data container
+   * Returns a description of the associated container as given by the /container-xxxx/describe API
+   * method.
+   *
+   * @param folders If true, a complete list of the folders in the data container is included in
+   * the output.
+   *
+   * @return JSON hash containing the output of the describe call.
    */
+  // TODO: add wiki link for /class-xxx/describe
   dx::JSON describe(bool folders=false) const;
 
   /**
-   * Move specified objects and/or folders to other folder
-   * in the same data container.
+   * Moves the specified objects and/or folders in the associated data container to the specified
+   * folder.
    *
-   * @param objects a JSON array of object IDs to be moved
-   * Empty array denotes that no object is to be moved.
-   * @param folders An array of strings of the folder route to be
-   * moved. Empty array denotes that no folder is to be moved.
-   * @param dest_folder The full path of destination folder
+   * See the <a href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Folders%20and%20Deletion#API-method%3A-%2Fclass-xxxx%2Fmove">/class-xxxx/move</a> API method for more info.
+   *
+   * @param objects A JSON array of strings containing the object ID(s) to be moved.
+   * @param folders A JSON array of strings containing the folder route(s) to be moved.
+   * @param dest_folder The full path of the destination folder.
    */
   void move(const dx::JSON &objects,
             const dx::JSON &folders,
             const std::string &dest_folder) const;
+
   /**
-   * Clones the specified objects and/or folders to other
-   * data container.
+   * Clones the specified objects and/or folders from the associated data container to another data
+   * container.
    *
-   * @param objects a JSON array of object IDs to be cloned.
-   * Empty array denotes that no object is to be cloned.
-   * @param folders An array of strings of the folder route to be
-   * clones. Empty array denotes that no folder is to be cloned.
-   * @param dest_proj The project in which to clone.
-   * @param dest_folder The full path of destination folder
+   * See the <a href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Cloning#API-method%3A-%2Fclass-xxxx%2Fclone">/class-xxx/clone</a> API method for more info.
+   *
+   * @param objects A JSON array of strings containing the object ID(s) to be cloned.
+   * @param folders A JSON array of strings containing the folder route(s) to be cloned.
+   * @param dest_proj ID of the project into which the selected objects should be cloned.
+   * @param dest_folder The full path of the destination folder in the destination project.
    */
   void clone(const dx::JSON &objects,
              const dx::JSON &folders,
@@ -64,73 +101,75 @@ class DXContainer {
 
   // Folder specific
 
-  /** 
-   * Creates a new folder
+  /**
+   * Creates a new folder in the associated data container.
    *
-   * @param folder Full path of the new folder to be created in data container.
-   * @param parents If true then parent folder will be created if they
-   * do not exist.
+   * @param folder The full path of the new folder to be created.
+   * @param parents Whether to create the parent folders if they do not already exist.
    */
   void newFolder(const std::string &folder, bool parents=false) const;
-  
+
   /**
-   * Lists a folder of data container.
-   * 
-   * @param folder A string representing the full path to the folder to
-   * be listed.
+   * Lists the contents of a folder in the associated data container.
    *
-   * @return A JSON hash of this form:
-   * {"objects": [{"id": id-1}, {"id": id-2}, ...], "folders": ["full-path-to-folder-1",
-   "full-path-to-folder-2", ...]}
+   * @param folder A string representing the full path to the folder to be listed.
+   *
+   * @return A JSON hash of the form: <code>{"objects": [{"id": id-1}, {"id": id-2}, ...], "folders": ["full-path-to-folder-1", "full-path-to-folder-2", ...]}</code>
    */
   dx::JSON listFolder(const std::string &folder="/") const;
-  
-  /** 
-   * Move a folder to specified destination folder (including the objects
-   * and subfolders it contains)
+
+  /**
+   * Moves a folder in the associated data container (and all the objects and subfolders it
+   * contains) to the specified destination folder.
    *
-   * @param folder Full path of the folder to be moved
-   * @param dest_folder Full path of the destination folder
+   * See the <a href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Folders%20and%20Deletion#API-method%3A-%2Fclass-xxxx%2Fmove">/class-xxxx/move</a> API method for more info.
+   *
+   * @param folder The full path of the folder to be moved.
+   * @param dest_folder The full path of the destination folder.
    */
   void moveFolder(const std::string &folder,
                   const std::string &dest_folder) const;
 
   /**
-   * Removes a given folder and all it's data content
-   * It must be either empty or recurse must be set to true in input params
+   * Removes a given folder and all its data content.
    *
-   * @param folder Full path of the folder to be removed
-   * @param recurse Boolean indicating whether removal should propagate 
-   * to its contained folders and objects
+   * The folder must be empty unless <code>recurse</code> is set to <code>true</code>.
+   *
+   * @param folder The full path of the folder to be removed.
+   * @param recurse Boolean indicating whether to recursively remove all objects and folders contained in the target.
    */
   void removeFolder(const std::string &folder, const bool recurse=false) const;
 
   // Objects-specific
-  
-  /** 
-   * Move objects to specified destination folder in same data container.
-   * 
-   * @param objects JSON array of object id's (strings) to be moved
-   * @param dest_folder Full path of the destination folder
+
+  /**
+   * Move objects in the associated data container to the specified destination folder.
+   *
+   * See the <a href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Folders%20and%20Deletion#API-method%3A-%2Fclass-xxxx%2Fmove">/class-xxxx/move</a> API method for more info.
+   *
+   * @param objects A JSON array of strings containing the object ID(s) to be moved.
+   * @param dest_folder The full path of the destination folder.
    */
   void moveObjects(const dx::JSON &objects,
                    const std::string &dest_folder) const {
     move(objects, dx::JSON(dx::JSON_ARRAY), dest_folder);
   }
-  
+
   /**
-   * Removes object(s) from the data container
+   * Removes the selected object(s) from the data container.
    *
-   * @param JSON array of object id's (strings) to be removed
+   * @param objects A JSON array of strings containing the object ID(s) to be removed.
    */
   void removeObjects(const dx::JSON &objects) const;
-  
+
   /**
-   * Clone object(s) from one data container to other.
-   * 
-   * @param objects JSON array of object id's (strings) to be cloned.
-   * @param dest_proj Destination data container id.
-   * @param dest_folder Id of the destination folder.
+   * Clone the specified object(s) from the associated data container to another data container.
+   *
+   * See the <a href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Cloning#API-method%3A-%2Fclass-xxxx%2Fclone">/class-xxx/clone</a> API method for more info.
+   *
+   * @param objects A JSON array of strings containing the object ID(s) to be cloned.
+   * @param dest_proj ID of the project into which the selected objects should be cloned.
+   * @param dest_folder The full path of the destination folder in the destination project.
    */
   void cloneObjects(const dx::JSON &objects,
                     const std::string &dest_proj,
@@ -139,40 +178,52 @@ class DXContainer {
   }
 };
 
+//! A DXContainer with additional functionality for collaboration.
+
+///
+/// In most day-to-day operations on the DNAnexus Platform, users will interact directly with
+/// projects rather than generic containers.
+///
+
 class DXProject : public DXContainer {
  public:
  DXProject(const std::string &dxid=g_WORKSPACE_ID) : DXContainer(dxid) { }
-  
+
   /**
-   * Update the project with specified options.
-   * See route: /project-xxxx/update for details.
+   * Update the project with specified options, as specified in the <a
+   * href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Projects#API-method%3A-%2Fproject-xxxx%2Fupdate">/project-xxxx/update</a>
+   * method.
    *
-   * @param to_update JSON hash as expected by /project-xxxx/update route
+   * @param to_update JSON hash to be provided to <code>/project-xxxx/update</code>.
    */
   void update(const dx::JSON &to_update) const;
-  
+
   /**
-   * Destroys the specified project. All objects are removed.
-   * Any jobs running in the project context are terminated.
+   * Destroys the specified project. All objects in the project are removed. Any jobs running in
+   * the project context are terminated.
    */
   void destroy() const;
 
   /**
-   * Invite other people (or PUBLIC) to the project.
+   * Invites another person (or PUBLIC) to the project. If the invitee is another person, they will
+   * receive the specified permission when they accept the invitation.
    *
-   * @param invitee Username or email of the person to be invited to the project.
-   * Use "PUBLIC" to make it publically available.
-   * @param level Permission level that the invitee would get ("LIST", "VIEW",
-   * "CONTRIBUTE", "ADMINISTER")
+   * See the <a href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Invitations%20and%20Join%20Requests#API-method%3A-%2Fproject-xxxx%2Finvite">/project-xxxx/invite</a> API method for more info.
+   *
+   * @param invitee Username or email of the person to be invited to the project. Use "PUBLIC" to make it publicly available.
+   * @param level Permission level that the invitee would get ("LIST", "VIEW", "CONTRIBUTE", "ADMINISTER").
    */
   void invite(const std::string &invitee, const std::string &level) const;
-  
+
   /**
-   * @param member Username of the project member whose permission will be
-   * decreased
-   * @param level The new permission level for the user ("LIST", "VIEW",
-   * "CONTRIBUTE", "ADMINISTER")
+   * Decreases the permissions of the specified user in the associated project.
+   *
+   * See the <a href="http://wiki.dev.dnanexus.com/API-Specification-v1.0.0/Projects#API-method%3A-%2Fproject-xxxx%2FdecreasePermissions">/project-xxxx/decreasePermissions</a> API method for more info.
+   *
+   * @param member Username of the project member whose permission will be decreased.
+   * @param level The new permission level for the user ("LIST", "VIEW", "CONTRIBUTE", "ADMINISTER").
    */
+  // TODO: link to wiki docs
   void decreasePerms(const std::string &member, const std::string &level) const;
 };
 
