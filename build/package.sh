7784583f05269b8ad876a6e5eafe78859dcd1fe0
Andrey Kislyuk <kislyuk@gmail.com>
2012-10-04 08:56:36 -0700

    begin package/release script

---
+++ build/package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+source "$(dirname $0)/../environment"
+cd "${DNANEXUS_HOME}/src"
+make clean
+make
+make install
+temp_archive=$(mktemp --suffix .tar.gz)
+(cd "${DNANEXUS_HOME}/.."; tar --exclude-vcs -czf $temp_archive dx-toolkit)
+mv $temp_archive "${DNANEXUS_HOME}/dx-toolkit-$(git describe).tar.gz"
+echo "Package in {DNANEXUS_HOME}/dx-toolkit-$(git describe).tar.gz"
