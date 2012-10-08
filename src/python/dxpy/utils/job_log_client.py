5733fcc2bc48fe0dc0fc2b2fc6af5eeb7ca6fe12
Andrey Kislyuk <kislyuk@gmail.com>
2012-10-07 23:46:33 -0700

    improve robustness of dx watch wrt socket.io handshake bug (https://github.com/LearnBoost/socket.io/issues/438)

--- src/python/dxpy/utils/job_log_client.py
+++ src/python/dxpy/utils/job_log_client.py
@@ -1,13 +1,20 @@
 import os, sys, json, re
-from ws4py.client.threadedclient import WebSocketClient
-from dxpy.utils.describe import get_find_jobs_string
+
+#from ws4py.client.threadedclient import WebSocketClient
+from ws4py.client import WebSocketBaseClient
+from ws4py.exc import HandshakeError
 
 import dxpy
+from dxpy.utils.describe import get_find_jobs_string
 
-class DXJobLogStreamClient(WebSocketClient):
+class DXJobLogStreamClient(WebSocketBaseClient):
+    def handshake_ok(self):
+        self.run()
+
     def set_dx_streaming_id(self, dx_streaming_id):
         self.dx_streaming_id = dx_streaming_id
         self.seen_jobs = set()
+        self.num_received_messages = 0
 
     def opened(self):
         self.send('5:1::' + json.dumps({"name": "streamingId", "args": [self.dx_streaming_id]}))
@@ -18,15 +25,20 @@ class DXJobLogStreamClient(WebSocketClient):
             print get_find_jobs_string(dxpy.describe(job_id), has_children=False)
 
     def received_message(self, message):
-        if (str(message) == '2::'):
+        message_string = str(message)
+        if message_string == '7:::1+0':
+            raise HandshakeError("Server suggests reconnect")
+        elif message_string == '2::':
+            # Ping/Echo
             self.send('2::')
             return
 
-        if not re.match("^([^:]+):([^:]*):([^:]*):(.+)", str(message)):
+        if not re.match("^([^:]+):([^:]*):([^:]*):(.+)", message_string):
             return
 
-        msg_type, msg_id, msg_endpoint, msg_data = re.match("^([^:]+):([^:]*):([^:]*):(.+)", str(message)).groups()
+        msg_type, msg_id, msg_endpoint, msg_data = re.match("^([^:]+):([^:]*):([^:]*):(.+)", message_string).groups()
         if msg_type == '5':
+            self.num_received_messages += 1
             try:
                 msg_content = json.loads(msg_data)
                 if (msg_content["name"] == "log"):
