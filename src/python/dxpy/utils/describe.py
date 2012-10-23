f1f06de2512755698067017066a7be5378f0cd72
Katherine Lai <klai@dnanexus.com>
2012-10-23 11:09:40 -0700

    Renaming "Dependencies" to "Depends on" in describe output as with API change

--- src/python/dxpy/utils/describe.py
+++ src/python/dxpy/utils/describe.py
@@ -307,7 +307,7 @@ def print_data_obj_desc(desc):
                 print_json_field(field, desc[field])
 
 def print_job_desc(desc):
-    recognized_fields = ['id', 'class', 'project', 'workspace', 'program', 'app', 'state', 'parentJob', 'originJob', 'function', 'runInput', 'originalInput', 'input', 'output', 'folder', 'launchedBy', 'created', 'modified', 'failureReason', 'failureMessage', 'stdout', 'stderr', 'waitingOnChildren', 'dependencies', 'projectWorkspace', 'globalWorkspace', 'resources', 'projectCache', 'applet', 'name', 'instanceType', 'systemRequirements', 'executableName', 'failureFrom']
+    recognized_fields = ['id', 'class', 'project', 'workspace', 'program', 'app', 'state', 'parentJob', 'originJob', 'function', 'runInput', 'originalInput', 'input', 'output', 'folder', 'launchedBy', 'created', 'modified', 'failureReason', 'failureMessage', 'stdout', 'stderr', 'waitingOnChildren', 'dependsOn', 'projectWorkspace', 'globalWorkspace', 'resources', 'projectCache', 'applet', 'name', 'instanceType', 'systemRequirements', 'executableName', 'failureFrom']
 
     print_field("ID", desc["id"])
     print_field("Class", desc["class"])
@@ -352,8 +352,8 @@ def print_job_desc(desc):
     print_field("Last modified", datetime.datetime.fromtimestamp(desc['modified']/1000).ctime())
     if 'waitingOnChildren' in desc:
         print_list_field('Pending subjobs', desc['waitingOnChildren'])
-    if 'dependencies' in desc:
-        print_list_field('Dependencies', desc['dependencies'])
+    if 'dependsOn' in desc:
+        print_list_field('Depends on', desc['dependsOn'])
     if "failureReason" in desc:
         print_field("Failure reason", desc["failureReason"])
     if "failureMessage" in desc:
