db8bc94730b387c060445982b611631a37d8b897
Vince Ramey <vramey@dnanexus.com>
2012-11-19 14:33:07 -0800

    refactor of dx_spans_to_bed to handle complex tables

--- src/python/dxpy/scripts/dx_spans_to_bed.py
+++ src/python/dxpy/scripts/dx_spans_to_bed.py
@@ -22,12 +22,95 @@ bed_col = {"chr":0,
            "block_sizes":10,
            "block_starts":11}
 
+default_bed_line = ["-", "0", "0", "-", "0", ".", "0", "0", "0,0,0", "0", "0", "0"]
+
+class gene:
+    def __init__(self, founder):
+        self.parent_ids = [founder['span_id']]
+        self.gene = None
+        self.trans = []
+        self.add_data(founder)
+
+    def add_data(self, data ):
+        if data['type'] == "gene":
+            self.gene = data
+        elif data['type'] == "transcript":
+            self.trans.append(transcript(data))
+            self.parent_ids.append(data['span_id'])
+        else:
+            for t in self.trans:
+                if data['parent_id'] == t.data['span_id']:
+                    t.add_data(data)
+
+    def check_and_write_data(self, current_lo, bed_file):
+        if self.gene == None:
+            return self.trans[0].check_and_write_data(current_lo, bed_file)
+        # just write the gene if we're a lone gene and it's passed
+        elif len(self.trans) == 0 and current_lo > self.gene['hi']:
+            output_row = default_bed_line[:]
+            output_row[bed_col['chr']] = self.gene['chr']
+            output_row[bed_col['chr']] = self.gene['lo']
+            output_row[bed_col['chr']] = self.gene['hi']
+            output_row[bed_col['chr']] = self.gene['name']
+            output_row[bed_col['chr']] = self.gene['strand']
+            if "thick_start" in self.gene:
+                output_row[bed_col['thick_start']] = str(self.gene['thick_start'])
+            if "thick_end" in self.gene:
+                output_row[bed_col['thick_end']] = str(self.gene['thick_end'])
+            return True
+        elif current_lo > self.gene['hi']:
+            for t in self.trans:
+                if t.check_and_write_data(current_lo, bed_file) != True:
+                    raise dxpy.AppError("found end of gene but not end of transcript: " + str(self.gene))
+            return True
+        else:
+            return False
+
+class transcript:
+    def __init__(self, t):
+        self.data = t
+        self.exons = []
+
+    def add_data( self, e ):
+        self.exons.append(e)
+
+    def check_and_write_data(self, current_lo, bed_file):
+        if self.data['hi'] >= current_lo:
+            return False
+        else:
+            output_row = default_bed_line[:]
+            output_row[bed_col['chr']] = self.data['chr']
+            output_row[bed_col['lo']] = str(self.data['lo'])
+            output_row[bed_col['hi']] = str(self.data['hi'])
+            output_row[bed_col['name']] = self.data['name']
+            output_row[bed_col['strand']] = self.data['strand']
+            if "thick_start" in self.data:
+                output_row[bed_col['thick_start']] = str(self.data['thick_start'])
+            if "thick_end" in self.data:
+                output_row[bed_col['thick_end']] = str(self.data['thick_end'])
+
+        block_sizes = []
+        block_starts = []
+
+        output_row[bed_col['block_count']] = str(len(self.exons))
+        for i in range(len(self.exons)):
+            block_sizes.append(str(self.exons[i]['hi'] - self.exons[i]['lo']))
+            block_starts.append(str(self.exons[i]['lo'] - self.data['lo']))
+
+        if output_row[bed_col['block_count']] != 0:
+            output_row[bed_col["block_sizes"]] = ",".join(block_sizes)
+            output_row[bed_col["block_starts"]] = ",".join(block_starts)
+
+        bed_file.write("\t".join(output_row) + "\n")
+        #print "\t".join(output_row)
+        return True
+
 
 def main(**kwargs):
     if len(kwargs) == 0:
         kwargs = vars(arg_parser.parse_args(sys.argv[1:]))
 
-    print kwargs
+    #print kwargs
 
     try:
         spans = dxpy.DXGTable(kwargs['Spans'])
@@ -46,7 +129,6 @@ def main(**kwargs):
 def export_genes(spans, out_name):
     global bed_col
     # setup default 
-    default_bed_line = ["-", "0", "0", "-", "0", ".", "0", "0", "0,0,0", "0", "0", "0"]
 
     span_cols = spans.get_col_names()
 
@@ -56,6 +138,7 @@ def export_genes(spans, out_name):
         output_row = default_bed_line[:]
 
         incomplete_buffer = []
+        gene_model = []
 
         generator = spans.iterate_rows(want_dict=True)
 
@@ -67,13 +150,18 @@ def export_genes(spans, out_name):
             except StopIteration:
                 entry = None
 
-
             #print "current entry"
             #print entry
             if entry != None:
-                incomplete_buffer.append(entry)
+                # take founding members (those with no parents) place in gene model
+                if entry['parent_id'] == -1:
+                    gene_model.append(gene(entry))
+                # otherwise they are to be added later
+                else:
+                    incomplete_buffer.append(entry)
                 current_chr = entry['chr']
                 current_lo = entry['lo']
+                #print "Current lo: " + str(current_lo)
             else:
                 # switch these to different values to flush final genes out to file
                 current_chr = ""
@@ -81,23 +169,69 @@ def export_genes(spans, out_name):
             parent_to_write = {}
             buff_to_keep = []
 
+            to_keep_exons = []
+
+            # if we've found your parent, add you in the same model
+            for orphan in incomplete_buffer:
+                added = False
+                for g in gene_model:
+                    if orphan['parent_id'] in g.parent_ids:
+                        g.add_data(orphan)
+                        added = True
+                if not added:
+                    to_keep_exons.append(orphan)
+
+            incomplete_buffer = to_keep_exons[:]
+
+            to_keep_genes = []
+
+            for g in gene_model:
+                if not g.check_and_write_data(current_lo, bed_file):
+                    to_keep_genes.append(g)
+                    
+            gene_model = to_keep_genes[:]
+
+            if entry == None:
+                return
+
+##########################################
+'''
             for buff in incomplete_buffer:
                 # if the current element is past your hi and you're a parent element then
                 # write yourself and your children down together as an element
-                if buff['parent_id'] == -1 and (buff['hi'] < current_lo or buff['chr'] != current_chr):
-                    parent_to_write[buff['span_id']] = [buff]
+                if (buff['hi'] < current_lo or buff['chr'] != current_chr):
+                    # if we have a gene hierarchy
+                    if buff['type'] == "gene":
+                        parent_to_write[buff['span_id']] = {"gene":buff, "trans":{} }
+                    # else have lone transcript
+                    elif buff['type'] == "transcript" and buff['parent_id'] == -1:
+                        parent_to_write[buff['span_id']] = {"gene":None, "trans":{buff['span_id']:{"exons":[], "trans_info":buff}}}
                 # if we're a parent but not done yet, stay around until next entry
-                elif buff['parent_id'] == -1:
+                elif (buff['type'] == "gene" or buff['type'] == "transcript"):
                     buff_to_keep.append(buff)
 
-            # now write that parent and all children
+            print "*"*20
+            for r in incomplete_buffer:
+                print " ".join([r['chr'], str(r['lo']), str(r['hi'])])
+            print "*"*20
+
+            # insert all transcripts into genes 
             for buff in incomplete_buffer:
-                # if we're writing out your parent, put you in the bucket
-                if buff['parent_id'] in parent_to_write:
-                    parent_to_write[buff['parent_id']].append(buff)
-                # else, keep you around
-                elif buff['parent_id'] != -1:
-                    buff_to_keep.append(buff)
+                if buff['type'] == "transcript" and buff['parent_id'] in parent_to_write:
+                    parent_to_write[buff['parent_id']]['trans'][buff['span_id']] = {"trans_info":buff, "exons":[]}
+
+            # now write that parent and all children #
+            ##########################################
+
+            for buff in incomplete_buffer:
+                for current_parent in parent_to_write:
+                    # if we're writing out your parent, put you in the bucket
+                    # unless you're a transcript, then don't write
+                    if buff['parent_id'] in parent_to_write[current_parent]['trans']:
+                        parent_to_write[current_parent]['trans'][buff['parent_id']]['exons'].append(buff)
+                    # else, keep you around
+                    elif buff['parent_id'] != -1 and buff['type'] != "transcript":
+                        buff_to_keep.append(buff)
 
             #print "buff_to_keep"
             #print buff_to_keep
@@ -105,48 +239,22 @@ def export_genes(spans, out_name):
             incomplete_buffer = buff_to_keep[:]
                 
             for gene_obj in parent_to_write:
-                parent = parent_to_write[gene_obj][0]
-                output_row[bed_col['chr']] = parent['chr']
-                output_row[bed_col['lo']] = str(parent['lo'])
-                output_row[bed_col['hi']] = str(parent['hi'])
-                output_row[bed_col['name']] = parent['name']
-                output_row[bed_col['strand']] = parent['strand']
-                if "thick_start" in parent:
-                    output_row[bed_col['thick_start']] = str(parent['thick_start'])
-                if "thick_end" in parent:
-                    output_row[bed_col['thick_end']] = str(parent['thick_end'])
-
-                block_sizes = []
-                block_starts = []
-
-                output_row[bed_col['block_count']] = str(len(parent_to_write[gene_obj]) - 1)
-                for i in range(1, len(parent_to_write[gene_obj])):
-                    block_sizes.append(str(parent_to_write[gene_obj][i]['hi'] - parent_to_write[gene_obj][i]['lo']))
-                    block_starts.append(str(parent_to_write[gene_obj][i]['lo'] - parent['lo']))
-
-                if output_row[bed_col['block_count']] != 0:
-                    output_row[bed_col["block_sizes"]] = ",".join(block_sizes)
-                    output_row[bed_col["block_starts"]] = ",".join(block_starts)
+                for trans_to_write in parent_to_write[gene_obj]['trans']:
                     
-                bed_file.write("\t".join(output_row) + "\n")
-                output_row = default_bed_line[:]
+                    #print "incomplete_buffer"
+                    #print incomplete_buffer
+                    #print "buff_to_keep"
+                    #print buff_to_keep
+                    #print "parent_to_write"
+                    #print parent_to_write
+                    #return
+'''
 
-                #print "incomplete_buffer"
-                #print incomplete_buffer
-                #print "buff_to_keep"
-                #print buff_to_keep
-                #print "parent_to_write"
-                #print parent_to_write
-                #return
-
-            if entry == None:
-                return
 
 # this function exports any gri, Spans, or NamedSpans object but not a Genes object
 def export_generic_bed(spans, out_name):
 
     global bed_col
-    default_bed_line = ["-", "0", "0", "-", "0", ".", "0", "0", "0,0,0", "0", "0", "0"]
 
     spans_columns = spans.get_col_names()
 
