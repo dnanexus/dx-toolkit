5cf449245da9f1d71417bfee502cd8619b2a76bb
Phil Sung <psung@dnanexus.com>
2012-12-06 13:29:53 -0800

    Fix permissions on generated packages.

--- build/package.sh
+++ build/package.sh
@@ -14,4 +14,5 @@ mv build/Prebuilt-Readme.md Readme.md
 temp_archive=$(mktemp --suffix .tar.gz)
 (cd "${DNANEXUS_HOME}/.."; tar --exclude-vcs -czf $temp_archive dx-toolkit)
 mv $temp_archive "${DNANEXUS_HOME}/dx-toolkit-$(git describe).tar.gz"
+chmod 664 "${DNANEXUS_HOME}/dx-toolkit-$(git describe).tar.gz"
 echo "Package in {DNANEXUS_HOME}/dx-toolkit-$(git describe).tar.gz"
