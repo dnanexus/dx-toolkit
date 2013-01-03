3f7c1c615680df917eaa1a7633d8135975dca1a2
andrew carroll <acarroll@dnanexus.com>
2013-01-03 15:40:44 -0800

    Adjusting coordinates of gtf import/export to account for the fact that gtf/gff coordinates are inclusive on hi

--- src/python/dxpy/scripts/dx_gff_to_genes.py
+++ src/python/dxpy/scripts/dx_gff_to_genes.py
@@ -193,7 +193,7 @@ def parseLine(line):
         raise dxpy.AppError("One of the start values was could not be translated to an integer. " + "\nOffending line: " + line + "\nOffending value: " + tabSplit[3])
     
     try:
-        hi = int(tabSplit[4])-1
+        hi = int(tabSplit[4])
     except ValueError:
         raise dxpy.AppError("One of the start values was could not be translated to an integer. " + "\nOffending line: " + line + "\nOffending value: " + tabSplit[4])
 
