c2857948ec402d5a8ae8b243a4e61becabbf8807
Phil Sung <psung@dnanexus.com>
2013-01-24 13:59:16 -0800

    Add license to repo and license header to all files.

--- src/javascript/oldJSLib/run_tests.js
+++ src/javascript/oldJSLib/run_tests.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env v8cgi
 
+// Copyright (C) 2013 DNAnexus, Inc.
+//
+// This file is part of dx-toolkit (DNAnexus platform client libraries).
+//
+//   Licensed under the Apache License, Version 2.0 (the "License"); you may
+//   not use this file except in compliance with the License. You may obtain a
+//   copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//   License for the specific language governing permissions and limitations
+//   under the License.
+
 // This is a sample tests file for JS client-side bindings,
 // The initial code (above main() is taken from template for seed_apps used by EM)
 require.paths.push(".");
