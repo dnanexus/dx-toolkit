4d45f496b35f222f7212d013184e0b53d5a69234
Katherine Lai <klai@dnanexus.com>
2013-01-23 16:57:00 -0800

    Updating copyright years

--- src/python/doc/conf.py
+++ src/python/doc/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DNAnexus Python Bindings'
-copyright = u'2012, DNAnexus'
+copyright = u'2012-2013, DNAnexus'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
