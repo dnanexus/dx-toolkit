b4aa32483c4cc8bbfb150d8a8e2356f236ef891e
Katherine Lai <klai@dnanexus.com>
2013-01-28 04:05:07 -0800

    Got rid of a stacktrace when a workflow is out of order

--- src/python/dxpy/cli/exec_io.py
+++ src/python/dxpy/cli/exec_io.py
@@ -29,7 +29,12 @@ def stage_to_job_refs(x, launched_jobs):
     ''' Used by run() to parse stage inputs bound to other stages when executing a workflow '''
     if isinstance(x, collections.Mapping):
         if "connectedTo" in x:
-            return {'job': launched_jobs[x['connectedTo']['stage']].get_id(), "field": x['connectedTo']['output']}
+            if x['connectedTo']['stage'] in launched_jobs and launched_jobs[x['connectedTo']['stage']] is not None:
+                return {'job': launched_jobs[x['connectedTo']['stage']].get_id(), "field": x['connectedTo']['output']}
+            else:
+                # TODO: Make this better
+                sys.stderr.write(fill("Error: An input is connected to a stage that has not yet been launched.  You will need to reorder the stages before they can be run.") + "\n")
+                exit(1)
         for key, value in x.iteritems():
             x[key] = stage_to_job_refs(value, launched_jobs)
     elif isinstance(x, list):
