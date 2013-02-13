f3d52c343297194271d786c9d1a082df207e4a15
Katherine Lai <klai@dnanexus.com>
2013-02-13 00:15:34 -0800

    R bindings: now using roxygen2 for saner documentation

---
+++ src/R/dxR-build-templates/dxR-package.R
@@ -0,0 +1,22 @@
+##' DNAnexus R Client Library
+##' 
+##' dxR is an R extension containing API wrapper functions for interacting with
+##' the new DNAnexus platform.
+##' 
+##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
+##' VERSION\cr Date: \tab DATE\cr License: \tab Apache License (== 2.0)\cr
+##' }
+##' 
+##' @name dxR-package
+##' @aliases dxR-package dxR
+##' @docType package
+##' @author Katherine Lai
+##' 
+##' Maintainer: Katherine Lai <klai@@dnanexus.com>
+# @examples
+# TODO: fill in examples here 
+# 
+NULL
+
+
+
--- src/R/install_dxR_deps.R
+++ src/R/install_dxR_deps.R
@@ -4,3 +4,6 @@ if("RCurl" %in% rownames(installed.packages()) == FALSE) {
 if("RJSONIO" %in% rownames(installed.packages()) == FALSE) {
   install.packages("RJSONIO", lib=.Library.site[1], dependencies=TRUE, repos="http://cran.us.r-project.org")
 }
+if("roxygen2" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("roxygen2", lib=.Library.site[1], dependencies=TRUE, repos="http://cran.us.r-project.org")
+}
