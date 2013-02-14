0d9d0300f2ec8859c9383582c2a60c710c6fc484
Katherine Lai <klai@dnanexus.com>
2013-02-14 12:40:16 -0800

    R: removing date from auto-generated DESCRIPTION, etc.

--- src/R/dxR-build-templates/dxR-package.R
+++ src/R/dxR-build-templates/dxR-package.R
@@ -4,7 +4,7 @@
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' VERSION\cr Date: \tab DATE\cr License: \tab Apache License (== 2.0)\cr
+##' VERSION\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
