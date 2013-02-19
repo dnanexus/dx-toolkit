419a775176669408f7a24eac6980a0ac6f26442d
Katherine Lai <klai@dnanexus.com>
2013-02-19 11:19:33 -0800

    R: Removing version # from build process; modifying by hand now

--- src/R/dxR/R/dxR-package.R
+++ src/R/dxR/R/dxR-package.R
@@ -1,14 +1,10 @@
-# WARNING: Do not edit the copy of this file directly in the package
-# directory; edit the copy in the dxR-build-templates directory which
-# will then be propagated in the R make target.
-#
 ##' DNAnexus R Client Library
 ##' 
 ##' dxR is an R extension containing API wrapper functions for interacting with
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' 0.37.0\cr License: \tab Apache License (== 2.0)\cr
+##' 0.37.1\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
