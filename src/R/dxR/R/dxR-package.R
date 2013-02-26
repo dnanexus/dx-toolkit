8c649b0daacdd62a95a6c4af5cb3cf21f18138ed
Katherine Lai <klai@dnanexus.com>
2013-02-26 13:33:39 -0800

    Bumped R bindings version to 0.38.1

--- src/R/dxR/R/dxR-package.R
+++ src/R/dxR/R/dxR-package.R
@@ -4,7 +4,7 @@
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' 0.37.1\cr License: \tab Apache License (== 2.0)\cr
+##' 0.38.1\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
