842d19603a4333bdf19baeaa6a2c8871d0c34bcb
Anurag Biyani <abiyani@dnanexus.com>
2013-03-05 16:50:27 -0800

    PTFM-6479 Read value of env variables

--- src/ruby/test/dxhttptest.rb
+++ src/ruby/test/dxhttptest.rb
@@ -1,25 +1,25 @@
-require "../dxhttp"
+require "../lib/dxhttp"
 require "test/unit"
  
 class TestDXHTTP < Test::Unit::TestCase
   def test_negative
-    #printresp(DXHTTPRequest("/system/findProjects", {}, true, JSON.parse('{"max_retries": 2}')))
-    assert_raise(RuntimeError) { DXHTTPRequest("/system/IDoNotExist", {}, false) }
-    assert_raise(RuntimeError) { DXHTTPRequest("/system/greet", {}, true, JSON.parse('{"method": "PUT"}')) } # We don't support PUT method
-    assert_raise(RuntimeError) { DXHTTPRequest("/system/findProjects", "{MalformedJson}", true) } # should fail because of malformed json
+    #printresp(DXRuby.DXHTTPRequest("/system/findProjects", {}, true, JSON.parse('{"max_retries": 2}')))
+    assert_raise(RuntimeError) { DXRuby.DXHTTPRequest("/system/IDoNotExist", {}, false) }
+    assert_raise(RuntimeError) { DXRuby.DXHTTPRequest("/system/greet", {}, true, JSON.parse('{"method": "PUT"}')) } # We don't support PUT method
+    assert_raise(RuntimeError) { DXRuby.DXHTTPRequest("/system/findProjects", "{MalformedJson}", true) } # should fail because of malformed json
   end
 
   def test_get
-    resp = DXHTTPRequest("http://www.google.com/", nil, true, JSON.parse('{"method": "GET", "prepend_srv": false, "auth": false}'))
+    resp = DXRuby.DXHTTPRequest("http://www.google.com/", nil, true, JSON.parse('{"method": "GET", "prepend_srv": false, "auth": false}'))
     assert_match(/Search the world's information/i, resp, "Google's webpage should contain this string")
   end
   
   def test_apiserver_requests
-    resp1 = DXHTTPRequest("/system/findProjects", {}, true)
+    resp1 = DXRuby.DXHTTPRequest("/system/findProjects", {}, true)
     assert_instance_of(Hash, resp1)
     assert_instance_of(Array, resp1["results"])
 
-    resp2 = DXHTTPRequest("/system/findProjects", "{}", true) # Passing {} or "{}" should both behave correctly
+    resp2 = DXRuby.DXHTTPRequest("/system/findProjects", "{}", true) # Passing {} or "{}" should both behave correctly
     assert_equal(resp1, resp2)
   end
 
@@ -27,7 +27,7 @@ end
 
 ##
 # Useful for manual debugging
-# Prints the response returned by DXHTTPRequest
+# Prints the response returned by DXRuby.DXHTTPRequest
 def printresp(resp)
   if (!resp.is_a?(String))
     puts JSON.pretty_generate(resp)
@@ -35,3 +35,5 @@ def printresp(resp)
     puts resp
   end
 end
+#resp = DXRuby.DXHTTPRequest("/system/findProjects", {}, true)
+#printresp(resp)
