5b2e79a33458730a93e6f2ae0dc5ce6988dfcfea
Chris Meyer <cygnil+github@hex-machina.com>
2013-04-16 11:13:39 -0700

    Handling names and getting rid of somewhat useless ImageBaker class

--- src/python/dxpy/scripts/dx_build_report_html.py
+++ src/python/dxpy/scripts/dx_build_report_html.py
@@ -17,7 +17,7 @@
 #   under the License.
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARN)
 
 import argparse
 import base64
@@ -29,52 +29,27 @@ import re
 import urllib2
 
 parser = argparse.ArgumentParser(description="Constructs and uploads an HTML report from HTML and attached images")
-parser.add_argument("src", help="Source image or HTML file")
-parser.add_argument("-p", "--project", help="Destination project ID. Can also be a full path in the form PROJECT:/PATH/TO/FOLDER")
+parser.add_argument("src", help="Source image or HTML file", nargs="+")
+parser.add_argument("-n", "--name", help="Name to give the report on the remote DNAnexus system")
 parser.add_argument("-o", "--output", help="Local file to save baked HTML to", default=None)
+parser.add_argument("-p", "--project", help="Destination project ID. Can also be a full path in the form PROJECT:/PATH/TO/FOLDER")
 
 
-class ImageBaker():
+def _image_to_data(img):
     """
-    Takes an HTML and parses it for image links, then fetches those images and re-encodes the src
-    attribute to include the image data encoded in Base64.
+    Does the work of encoding an image into Base64
     """
-    def __init__(self, src):
-        src = os.path.realpath(src)
-        self.path = os.path.dirname(src)
-        self.file = os.path.basename(src)
-        self.html = "".join(_load_file(src).readlines())
-        if imghdr.what("", self.html):
-            self.html = "<html><body><img src='{}'/></body></html>".format(self.file)
-
-    def _image_to_data(self, img):
-        """
-        Does the work of encoding an image into Base64
-        """
-        # If the image is already encoded in Base64, we have nothing to do here
-        if not img["src"] or re.match("data:", img["src"]):
-            return
-        if re.match("http[s]://", img["src"]):
-            img_data = _load_url(img["src"]).read()
-        else:
-            img_data = _load_file(img["src"]).read()
-        img_type = imghdr.what("", img_data)
-        img_b64 = base64.b64encode(img_data)
-        src_data = "data:image/{};base64,{}".format(img_type, img_b64)
-        img["src"] = src_data
-
-    def bake(self):
-        """
-        Runs the encoder on the file given when this class was instantiated
-        """
-        cwd = os.getcwd()
-        os.chdir(self.path)
-        bs_html = bs4.BeautifulSoup(self.html)
-        images = bs_html.find_all("img")
-        for image in images:
-            self._image_to_data(image)
-        os.chdir(cwd)
-        return bs_html
+    # If the image is already encoded in Base64, we have nothing to do here
+    if not img["src"] or re.match("data:", img["src"]):
+        return
+    elif re.match("http[s]://", img["src"]):
+        img_data = _load_url(img["src"]).read()
+    else:
+        img_data = _load_file(img["src"]).read()
+    img_type = imghdr.what("", img_data)
+    img_b64 = base64.b64encode(img_data)
+    src_data = "data:image/{};base64,{}".format(img_type, img_b64)
+    img["src"] = src_data
 
 
 def _load_file(path, mode="r"):
@@ -101,13 +76,42 @@ def _load_url(url):
         parser.error("{} could not be loaded remotely!".format(url))
 
 
-def _upload_html(project, html, path="/"):
+def bake(src):
+    """
+    Runs the encoder on the given source file
+    """
+    src = os.path.realpath(src)
+    path = os.path.dirname(src)
+    filename = os.path.basename(src)
+    html = "".join(_load_file(src).readlines())
+    if imghdr.what("", html):
+        html = "<html><body><img src='{}'/></body></html>".format(filename)
+
+    # Change to the file's directory so image files with relative paths can be loaded correctly
+    cwd = os.getcwd()
+    os.chdir(path)
+    bs_html = bs4.BeautifulSoup(html)
+    images = bs_html.find_all("img")
+    for image in images:
+        _image_to_data(image)
+    os.chdir(cwd)
+    return bs_html
+
+
+def upload_html(project, html, path="/", name=""):
     """
     Uploads the HTML to a file on the server
     """
     try:
         dxfile = dxpy.bindings.dxfile_functions
-        file_id = dxpy.api.file_new({"project": project, "folder": path, "hidden": True})["id"]
+        args = {
+            "project": project,
+            "folder": path,
+            "hidden": True
+        }
+        if name and len(name) > 0:
+            args["name"] = name
+        file_id = dxpy.api.file_new(args)["id"]
         html_file = dxfile.DXFile(file_id, project, "w")
         html_file.write(html)
         html_file.close()
@@ -116,10 +120,10 @@ def _upload_html(project, html, path="/"):
         parser.error("Could not upload HTML report to DNAnexus server!")
 
 
-def _create_record(project, file_ids, path="/"):
+def create_record(project, file_ids, path="/", name=""):
     """
-    Creates a master record for the HTML report; this doesn't contain contain the actual HTML, but
-    reports are required to be records rather than files
+    Creates a master record for the HTML report; this doesn't contain contain the actual HTML, but reports
+    are required to be records rather than files and we can link more than one HTML file to a report
     """
     files = []
     for file_id in file_ids:
@@ -130,27 +134,14 @@ def _create_record(project, file_ids, path="/"):
         "types": ["Report", "HTMLReport"],
         "details": {"files": files}
     }
+    if name and len(name) > 0:
+        args["name"] = name
     try:
         record_id = dxpy.api.record_new(args)["id"]
         dxpy.api.record_close(record_id)
         return record_id
     except:
         parser.error("Could not create an HTML report record on DNAnexus servers!")
-
-
-def upload(project, html):
-    """
-    Uploads all HTML report components to DNAnexus and creates a record to encompass them all
-    """
-    if not re.match("project-", project):
-        project = "project-" + project
-    path = "/"
-    if re.match("project-[\d\w]{24}:/", project):
-        path = re.sub("^project-[\d\w]{24}:", "", project)
-        project = re.sub("^(project-[\d\w]{24}):/.*", "\\1", project)
-    file_id = _upload_html(project, html, path)
-    record_id = _create_record(project, [file_id], path)
-    return record_id
 
 
 def save(filename, html):
@@ -170,15 +161,39 @@ def main(**kwargs):
         args = parser.parse_args()
     else:
         args = parser.parse_args(**kwargs)
-    image_baker = ImageBaker(args.src)
-    html = image_baker.bake()
-    # Adjust all links in the HTML Report to address the top bar so they're still useful
-    for link in html("a"):
-        link["target"] = "_top"
     if args.project:
-        print(upload(args.project, str(html)))
-    if args.output:
-        save(args.output, str(html))
+        if not re.match("project-", args.project):
+            args.project = "project-" + args.project
+        path = "/"
+        if re.match("project-[\d\w]{24}:/", args.project):
+            path = re.sub("^project-[\d\w]{24}:", "", args.project)
+            args.project = re.sub("^(project-[\d\w]{24}):/.*", "\\1", args.project)
+    remote_file_ids = []
+    for i, source in enumerate(args.src):
+        html = bake(source)
+        # Adjust all links in the HTML Report to address the top bar so they're still useful
+        for link in html("a"):
+            link["target"] = "_top"
+
+        # If we're supposed to upload the report to the server, upload the individual HTML file
+        if args.project:
+            remote_file_ids.append(upload_html(args.project, str(html), path, os.path.basename(source)))
+
+        # If we're supposed to save locally, do that
+        if args.output:
+            filename = args.output
+            # We may have to wrangle the filename a little if there are multiple output files
+            if len(args.src) > 1:
+                index_str = "." + str(i)
+            else:
+                index_str = ""
+            if re.search("\.html?$", filename):
+                filename = re.sub("(\.html?)$", index_str + "\\1", filename)
+            else:
+                filename += index_str + ".html"
+            save(filename, str(html))
+    if len(remote_file_ids) > 0:
+        print(create_record(args.project, remote_file_ids, path, args.name or args.src[0]))
 
 
 if __name__ == "__main__":
