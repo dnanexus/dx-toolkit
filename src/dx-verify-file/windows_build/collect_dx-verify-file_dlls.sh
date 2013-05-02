003c0c7f2ca5b612d403e14fa88be2efbebac822
Anurag Biyani <abiyani@dnanexus.com>
2013-05-01 17:47:47 -0700

    dx-verify-file changes
    - Compile properly on windows
    - Reads file > 2GB correctly

---
+++ src/dx-verify-file/windows_build/collect_dx-verify-file_dlls.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -ex
+#
+# Copyright (C) 2013 DNAnexus, Inc.
+#
+# This file is part of dx-toolkit (DNAnexus platform client libraries).
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#   use this file except in compliance with the License. You may obtain a copy
+#   of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+pwd
+cd $1
+pwd
+SWPATH=${HOME}/sw/local
+cp $SWPATH/curl/bin/libcurl-4.dll .
+cp $SWPATH/zlib-1.2.3-bin/bin/zlib1.dll .
+cp $SWPATH/regex-2.7-bin/bin/regex2.dll .
+cp /bin/msys-1.0.dll .
+cp /bin/msys-crypto-1.0.0.dll .
