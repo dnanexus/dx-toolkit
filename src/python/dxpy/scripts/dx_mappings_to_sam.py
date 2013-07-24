ccebd3cb6f4b32f1c67830734d403eb15c9a5373
Phil Sung <psung@dnanexus.com>
2013-07-24 13:55:50 -0700

    Error on invalid sequence is converted to an AppError.

--- src/python/dxpy/scripts/dx_mappings_to_sam.py
+++ src/python/dxpy/scripts/dx_mappings_to_sam.py
@@ -271,7 +271,7 @@ def writeRow(row, col, defaultCol, outputFile, idAsName, idPrepend, writeRowId, 
         try:
             seq = reverseComplement(seq)
         except ValueError as e:
-            raise ValueError("Error converting row %d: %s" % (row["__id__"], e.message))
+            raise dxpy.AppError("Error converting row %d: %s" % (row["__id__"], e.message))
         qual = qual[::-1]
     
     if values["mate_id"] == -1 or values["chr"] != values["chr2"] or values["chr"] == '' or values["chr"] == '*':
