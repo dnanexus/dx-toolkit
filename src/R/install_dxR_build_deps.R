81eb06666af201c0a406c64ade7f2e94dbd2bc6f
Phil Sung <psung@dnanexus.com>
2013-12-10 16:48:18 -0800

    R build fix: install roxygen2 from archive.
    * Update outdated R documentation pages.

--- src/R/install_dxR_build_deps.R
+++ src/R/install_dxR_build_deps.R
@@ -4,9 +4,23 @@ if("RCurl" %in% rownames(installed.packages()) == FALSE) {
 if("RJSONIO" %in% rownames(installed.packages()) == FALSE) {
   install.packages("RJSONIO", lib=.Library.site[1], repos="http://cran.us.r-project.org")
 }
-if("roxygen2" %in% rownames(installed.packages()) == FALSE) {
-  install.packages("roxygen2", lib=.Library.site[1], repos="http://cran.us.r-project.org")
-}
 if("testthat" %in% rownames(installed.packages()) == FALSE) {
   install.packages("testthat", lib=.Library.site[1], repos="http://cran.us.r-project.org")
+}
+
+if("digest" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("digest", lib=.Library.site[1], repos="http://cran.us.r-project.org")
+}
+if("stringr" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("stringr", lib=.Library.site[1], repos="http://cran.us.r-project.org")
+}
+if("brew" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("brew", lib=.Library.site[1], repos="http://cran.us.r-project.org")
+}
+download.file(
+  "http://cran.r-project.org/src/contrib/Archive/roxygen2/roxygen2_2.2.2.tar.gz",
+  "roxygen2_2.2.2"
+)
+if("roxygen2" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("roxygen2_2.2.2", lib=.Library.site[1], repos=NULL, type="source")
 }
