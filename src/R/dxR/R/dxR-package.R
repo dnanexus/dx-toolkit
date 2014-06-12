b0db51faa6e5de261ff63455d6ac3801e49a563e
Andrey Kislyuk <akislyuk@dnanexus.com>
2014-06-12 17:34:49 +0000

    Changelog and dxR version update for v0.101.2

--- src/R/dxR/R/dxR-package.R
+++ src/R/dxR/R/dxR-package.R
@@ -4,7 +4,7 @@
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' 0.101.1\cr License: \tab Apache License (== 2.0)\cr
+##' 0.101.2\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
