7aaaf5842f3d02807716b52a9d79e2c28a629793
Daniel Killebrew <dkillebrew@dnanexus.com>
2014-09-02 12:51:35 -0700

    Reduce file closing timeout to 1 day

--- src/python/dxpy/bindings/__init__.py
+++ src/python/dxpy/bindings/__init__.py
@@ -601,7 +601,7 @@ class DXDataObject(DXObject):
 
         return self.describe(**kwargs)["state"]
 
-    def _wait_on_close(self, timeout=3600*24*7, **kwargs):
+    def _wait_on_close(self, timeout=3600*24*1, **kwargs):
         elapsed = 0
         while True:
             state = self._get_state(**kwargs)
