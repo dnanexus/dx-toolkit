f6d60d557f65cf5a237d7f3286baa92d9af53c95
Ric Medlinger <rmedlinger@dnanexus.com>
2014-10-29 19:11:26 +0000

    Changelog and version updates for v0.122.0

--- src/R/dxR/R/dxR-package.R
+++ src/R/dxR/R/dxR-package.R
@@ -4,7 +4,7 @@
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' 0.121.0\cr License: \tab Apache License (== 2.0)\cr
+##' 0.122.0\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
