a6562a68791abd2eeaed1e1ffc2ce9262f97c59e
Ohad Rodeh <orodeh@dnanexus.com>
2014-11-14 15:45:19 -0800

    PTFM-13730: expanding the bash path variable
    Summary: Expanding the bash path variable, and uplifting the tests accordingly.
    
    Test Plan: Improved the file_load/vars test.
    
    Reviewers: klai
    
    Reviewed By: klai
    
    Differential Revision: https://phabricator.dnanexus.com/D155

--- src/python/test/file_load/vars/run.sh
+++ src/python/test/file_load/vars/run.sh
@@ -5,48 +5,60 @@ main() {
     dx-print-bash-vars --dbg-compare-old > BASH_VAR_ERRS
 
     ## Checking file type
-    check_var_defined "$seq1"
+    check_var_is_defined "$seq1"
 
-    check_var_defined "$seq2"
+    check_var_is_defined "$seq2"
 
     ## Checking array:file type
-    check_array_var_defined "$genes"
+    check_var_is_defined "$genes"
 
-    check_var_defined "$seq1_name" "A.txt"
-    check_var_defined "$seq1_path" "\$HOME/in/seq1/A.txt"
-    check_var_defined "$seq1_prefix" "A"
-    check_var_defined "$seq2_name" "A.txt"
-    check_var_defined "$seq2_path" "\$HOME/in/seq2/A.txt"
-    check_var_defined "$seq2_prefix" "A"
+    check_var "seq1_name" "$seq1_name" "A.txt"
+    check_var "seq1_prefix" "$seq1_prefix" "A"
+    check_var "seq2_name" "$seq2_name" "A.txt"
+    check_var "seq2_prefix" "$seq2_prefix" "A"
     check_array_var_defined "$genes_name" "( A.txt A.txt )"
-    check_array_var_defined "$genes_path" "( \$HOME/in/genes/A.txt \$HOME/in/genes/A.txt )"
     check_array_var_defined "$genes_prefix" "( A A )"
+
+    check_var "seq1_path" "$seq1_path" "$HOME/in/seq1/A.txt"
+    check_var "seq2_path" "$seq2_path" "$HOME/in/seq2/A.txt"
+    check_array_var_defined "$genes_path" "( $HOME/in/genes/A.txt $HOME/in/genes/A.txt )"
+
+    # checking that the path variable really works
+    dx download "$seq1" -o seq1
+    diff "$seq1_path" seq1
+    dx download "$seq2" -o seq2
+    diff "$seq2_path" seq2
 
     dx-upload-all-outputs
 }
 
 # Check if an environment variable is defined, and if it has the correct value
-check_var_defined() {
+check_var_is_defined() {
     if [[ -z $1 ]];
     then
         echo "Error: expecting environment variable $1 to be defined"
         dx-jobutil-report-error "Error: expecting environment variable $1 to be defined" "AppError"
         exit 1
     fi
+}
 
-    if [ $# -ne 3 ];
+
+# Check if an environment variable is defined, and if it has the correct value
+check_var() {
+    if [[ $# -ne 3 ]];
     then
-        return
+        echo "Error: check_var expects three inputs, but got $#"
+        dx-jobutil-report-error "check_var expects three inputs, but got $#" "AppError"
+        exit 1
     fi
 
-    if [[ ! "$1" == "$2" ]];
+    if [[ ! "$2" == "$3" ]];
     then
-        echo "Error: expecting environment variable $1 to equal $2"
-        dx-jobutil-report-error "Error: expecting environment variable $1 to equal $2" "AppError"
+        echo "Error: expecting $2 to equal $3 (var=$1)"
+        dx-jobutil-report-error "expecting $2 to equal $3 (var=$1)" "AppError"
         exit 1
     fi
 }
-
 
 # The same, for a variable that is supposed to take on an array value
 check_array_var_defined() {
