773a84130c6259a02c8c2cebcb7f3df914f9fe62
Andrey Kislyuk <kislyuk@gmail.com>
2014-11-20 12:42:50 -0800

    code review

--- src/python/dxpy/utils/__init__.py
+++ src/python/dxpy/utils/__init__.py
@@ -313,4 +313,5 @@ def json_loads_raise_on_duplicates(*args, **kwargs):
 def warn(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+# Moved to the bottom due to circular imports
 from .exec_utils import run, convert_handlers_to_dxlinks, parse_args_as_job_input, entry_point, DXJSONEncoder
