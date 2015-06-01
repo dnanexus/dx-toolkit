9eea992aa20945d6311915c1c855309c7287322f
Phil Sung <psung@dnanexus.com>
2015-06-01 09:54:38 -0700

    Example of running a DXWorkflow in Java
    Summary:
    add a test that runs a workflow (in part to document how to run
    workflows in Java)
    
    Test Plan: none
    
    Reviewers: pnguyen
    
    Reviewed By: pnguyen
    
    Differential Revision: https://dnanexus.phacility.com/D360

--- src/java/src/test/java/com/dnanexus/DXWorkflowTest.java
+++ src/java/src/test/java/com/dnanexus/DXWorkflowTest.java
@@ -24,16 +24,50 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.dnanexus.DXDataObject.DescribeOptions;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableList;
 
 /**
  * Tests for creating workflows.
  */
 public class DXWorkflowTest {
 
+    @JsonInclude(Include.NON_NULL)
+    private static class WorkflowAddStageInput {
+        @JsonProperty
+        public int editVersion;
+
+        @JsonProperty
+        public String executable;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class WorkflowAddStageOutput {
+        @JsonProperty
+        public int editVersion;
+
+        @JsonProperty
+        public String stage;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class WorkflowRunOutput {
+        @JsonProperty
+        public String id;
+    }
+
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private DXProject testProject;
+
+    private ObjectNode makeDXLink(DXDataObject dataObject) {
+        return DXJSON.getObjectBuilder().put("$dnanexus_link", dataObject.getId()).build();
+    }
 
     @Before
     public void setUp() {
@@ -120,6 +154,61 @@ public class DXWorkflowTest {
                         return DXWorkflow.newWorkflow();
                     }
                 });
+    }
+
+    @Test
+    public void testRunWorkflow() {
+        DXWorkflow workflow = DXWorkflow.newWorkflow().setProject(testProject).build();
+
+        // Create an applet to be added to the workflow and some inputs to be supplied to it
+        InputParameter inputString = InputParameter.newInputParameter("input_string",
+                IOClass.STRING).build();
+        InputParameter inputRecord = InputParameter.newInputParameter("input_record",
+                IOClass.RECORD).build();
+        OutputParameter outputRecord = OutputParameter.newOutputParameter("output_record",
+                IOClass.RECORD).build();
+
+        DXRecord myRecord = DXRecord.newRecord().setProject(testProject).setName("myRecord")
+                .build().close();
+
+        String code = "dx-jobutil-add-output output_record `dx-jobutil-parse-link \"$input_record\"` --class=record\n";
+
+        DXApplet applet = DXApplet.newApplet().setProject(testProject)
+                .setName("applet_for_java_test")
+                .setRunSpecification(RunSpecification.newRunSpec("bash", code).build())
+                .setInputSpecification(ImmutableList.of(inputString, inputRecord))
+                .setOutputSpecification(ImmutableList.of(outputRecord)).build();
+
+        // ---
+        // Here is some low-level code for building up the workflow. Eventually there will be
+        // high-level bindings to this functionality.
+        WorkflowAddStageInput addStage0Input = new WorkflowAddStageInput();
+        addStage0Input.editVersion = 0;
+        addStage0Input.executable = applet.getId();
+
+        WorkflowAddStageOutput addStage0Output = DXAPI.workflowAddStage(workflow.getId(),
+                addStage0Input, WorkflowAddStageOutput.class);
+
+        WorkflowAddStageInput addStage1Input = new WorkflowAddStageInput();
+        addStage1Input.editVersion = addStage0Output.editVersion;
+        addStage1Input.executable = applet.getId();
+
+        WorkflowAddStageOutput addStage1Output = DXAPI.workflowAddStage(workflow.getId(),
+                addStage1Input, WorkflowAddStageOutput.class);
+        // End low-level code
+        // ---
+
+        // Supply workflow inputs in the format STAGE.INPUTNAME
+        ObjectNode runInput = DXJSON.getObjectBuilder()
+                .put(addStage0Output.stage + ".input_string", "foo")
+                .put(addStage0Output.stage + ".input_record", makeDXLink(myRecord))
+                .put(addStage1Output.stage + ".input_string", "bar")
+                .put(addStage1Output.stage + ".input_record", makeDXLink(myRecord)).build();
+
+        // We run a workflow here, but do not wait for its result, so it's fine that this test
+        // doesn't check for ConfigOption.RUN_JOBS.
+        DXAnalysis analysis = workflow.newRun().setInput(runInput).setProject(testProject).run();
+        analysis.terminate();
     }
 
     // Internal tests
