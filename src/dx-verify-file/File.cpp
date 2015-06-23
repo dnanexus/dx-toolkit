a42b02bf0706a5fbc8aeb481b6ecd72da88f7cef
Daniel Killebrew <dkillebrew@dnanexus.com>
2015-06-23 11:31:32 -0700

    verify file expects files to be closed
    Summary: so that it can check reported file size. This is for compatibility with direct upload to S3 (which changed this).
    
    Test Plan: run upload agent tests
    
    Reviewers: psung
    
    Reviewed By: psung
    
    Differential Revision: https://dnanexus.phacility.com/D400

--- src/dx-verify-file/File.cpp
+++ src/dx-verify-file/File.cpp
@@ -56,10 +56,10 @@ File::File(const std::string &localFile_, const std::string &remoteFile_, const 
 void File::init() {
   using namespace dx;
   testLocalFileExists(localFile);
-  matchStatus = Status::MATCH_SUCCESSFUL_OR_IN_PROGRESS; 
+  matchStatus = Status::MATCH_SUCCESSFUL_OR_IN_PROGRESS;
   fs::path p(localFile);
   size = fs::file_size(p);
-  
+
   //Call file describe on remote file to get "parts" size
   JSON inp(JSON_HASH);
   inp["parts"] = true;
@@ -75,18 +75,22 @@ void File::init() {
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Now do a quick sanity check
-  //  - Remote file size must match local file size (if file is in "closed" or "closing" state)
-  //  - All parts must be in "completed" state, and sum of total size of all parts == local file size (irrespective of file state)
+  //  - Remote file size must match local file size (if file is "closed")
+  //  - All parts must be in "completed" state, and sum of total size of all parts == local file size
   // If any of these check fail, mark the file as a non-match directly
   assert(out.has("state") && out["state"].type() == JSON_STRING);
-  if (out["state"].get<string>() != "open") {
+  if (out["state"].get<string>() == "closed") {
     assert(out.has("size") && out["size"].type() == JSON_INTEGER);
     if (out["size"].get<int64_t>() != size) {
       LOG << "Size of local file '" << localFile << "' & remote file '" << remoteFile << "' differ. Marking it as a non-match" << endl;
       matchStatus = Status::FAILED_TO_MATCH_REMOTE_FILE;
       return;
     }
+  } else {
+      throw runtime_error("Remote file ('" + remoteFile + "') is not in 'closed' state.\n"
+                          "This program should only be used for 'closed' files.");
   }
+
   int64_t totalPartSize = 0;
   for (JSON::object_iterator it = out["parts"].object_begin(); it != out["parts"].object_end(); ++it) {
     // Assert the structure of each value in "parts" hash
@@ -114,7 +118,7 @@ unsigned int File::createChunks(BlockingQueue<Chunk *> &queue) {
   // For creating chunks:
   //  1) We sort the part IDs in increasing order (by pushing them all in a std::map after converting to int)
   //  2) We start creating file chunks
-  
+
   if (matchStatus == Status::FAILED_TO_MATCH_REMOTE_FILE) {
     return 0; // we have already marked the file as a non-match, no need to create chunks
   }
