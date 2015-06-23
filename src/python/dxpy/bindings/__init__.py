dnanexus/dx-toolkit

commit d10b98e825a3d76504e85285acd6687d3638970a

PTFM-15103 Add Python bindings for dataobject describe fields API
Summary:
Add high-level Python bindings for the new API so clients calling the describe
method can use the functionality.

Make two commands take advantage of the new bindings: the "wait on close"
methods for files and gtables, and "dx ls" which can use a minimal set of
fields in the default and --brief cases.

Test Plan: unit tests, and hand-examined diagnostic output from _DX_DEBUG=1

Reviewers: atseng

Reviewed By: atseng

Subscribers: pnguyen

Differential Revision: https://dnanexus.phacility.com/D401

src/python/dxpy/bindings/__init__.py
@@ -295,25 +295,41 @@ class DXDataObject(DXObject):
 
         return self._proj
 
-    def describe(self, incl_properties=False, incl_details=False, **kwargs):
+    def describe(self, fields=None, default_fields=None, incl_properties=False, incl_details=False, **kwargs):
         """
-        :param incl_properties: If true, includes the properties of the object in the output
-        :type incl_properties: boolean
-        :param incl_details: If true, includes the details of the object in the output
-        :type incl_details: boolean
+        :param fields: set of fields to include in the output, for
+            example ``{'name', 'modified'}``. The field ``id`` is always
+            implicitly included. If ``fields`` is specified, the default
+            fields are not included (that is, only the fields specified
+            here, and ``id``, are included) unless ``default_fields`` is
+            additionally set to True.
+        :type fields: set or sequence of str
+        :param default_fields: if True, include the default fields in
+            addition to fields requested in ``fields``, if any; if
+            False, only the fields specified in ``fields``, if any, are
+            returned (defaults to False if ``fields`` is specified, True
+            otherwise)
+        :type default_fields: bool
+        :param incl_properties: if true, includes the properties of the
+            object in the output (deprecated; use
+            ``fields={'properties'}, default_fields=True`` instead)
+        :type incl_properties: bool
+        :param incl_details: if true, includes the details of the object
+            in the output (deprecated; use ``fields={'details'},
+            default_fields=True`` instead)
+        :type incl_details: bool
         :returns: Description of the remote object
         :rtype: dict
 
-        Returns a dict with a description of the remote data object. The
-        result includes the key-value pairs as specified in the API
-        documentation for the ``/describe`` method of each data object
-        class. At a minimum, "id", "class", etc. should be available,
-        but different classes of objects may have additional fields.
+        Return a dict with a description of the remote data object.
 
-        If *incl_properties* is set, the output contains an additional
-        key-value pair with key "properties". If *incl_details* is set,
-        the output contains an additional key-value pair with key
-        "details".
+        The result includes the key-value pairs as specified in the API
+        documentation for the ``/describe`` method of each data object
+        class. The API defines some default set of fields that will be
+        included (at a minimum, "id", "class", etc. should be available,
+        and there may be additional fields that vary based on the
+        class); the set of fields may be customized using ``fields`` and
+        ``default_fields``.
 
         Any project-specific metadata fields (name, properties, and
         tags) are obtained from the copy of the object in the project
@@ -327,7 +343,18 @@ class DXDataObject(DXObject):
                 _class=self._class)
             )
 
-        describe_input = dict(properties=incl_properties, details=incl_details)
+        if (incl_properties or incl_details) and (fields is not None or default_fields is not None):
+            raise ValueError('Cannot specify properties or details in conjunction with fields or default_fields')
+
+        if incl_properties or incl_details:
+            describe_input = dict(properties=incl_properties, details=incl_details)
+        else:
+            describe_input = {}
+            if default_fields is not None:
+                describe_input['defaultFields'] = default_fields
+            if fields is not None:
+                describe_input['fields'] = {field_name: True for field_name in fields}
+
         if self._proj is not None:
             describe_input["project"] = self._proj
 
@@ -599,7 +626,7 @@ class DXDataObject(DXObject):
 
         '''
 
-        return self.describe(**kwargs)["state"]
+        return self.describe(fields={'state'}, **kwargs)["state"]
 
     def _wait_on_close(self, timeout=3600*24*1, **kwargs):
         elapsed = 0

src/python/dxpy/bindings/dxfile.py
@@ -392,7 +392,7 @@ class DXFile(DXDataObject):
         be in either the "open" or "closing" states.
         '''
 
-        return self.describe(**kwargs)["state"] == "closed"
+        return self.describe(fields={'state'}, **kwargs)["state"] == "closed"
 
     def close(self, block=False, **kwargs):
         '''

src/python/dxpy/bindings/dxproject.py
@@ -118,6 +118,11 @@ class DXContainer(DXObject):
         input hash to be supplied to each ``/describe`` call.
 
         """
+        # TODO: it would be nice if we could supply describe
+        # fields/defaultFields in a similar way to what we pass to the
+        # high-level describe method, rather than having to construct
+        # the literal API input
+
         api_method = dxpy.api.container_list_folder
         if isinstance(self, DXProject):
             api_method = dxpy.api.project_list_folder

src/python/dxpy/scripts/dx.py
@@ -728,8 +728,15 @@ def ls(args):
     resp = None
     if entity_results is None:
         try:
+            # Request the minimal set of describe fields possible
+            if args.brief:
+                describe_input = dict(fields={'id': True, 'name': True})
+            elif args.verbose:
+                describe_input = True
+            else:
+                describe_input = dict(fields={'id': True, 'class': True, 'name': True})
             resp = dxproj.list_folder(folder=folderpath,
-                                      describe={},
+                                      describe=describe_input,
                                       only=only,
                                       includeHidden=args.all)
 
@@ -754,7 +761,8 @@ def ls(args):
                         print(BOLD() + 'State' + DELIMITER('\t') + 'Last modified' + DELIMITER('       ') + 'Size' + DELIMITER('     ') + 'Name' + DELIMITER(' (') + 'ID' + DELIMITER(')') + ENDC())
                     else:
                         print("No data objects found in the folder")
-                name_counts = collections.Counter(obj['describe']['name'] for obj in resp['objects'])
+                if not args.brief and not args.verbose:
+                    name_counts = collections.Counter(obj['describe']['name'] for obj in resp['objects'])
                 for obj in resp['objects']:
                     if args.brief:
                         print(obj['id'])

src/python/test/test_dxpy.py
@@ -1039,6 +1039,19 @@ class TestDXRecord(unittest.TestCase):
         with self.assertRaises(TypeError):
             dxrecord = dxpy.new_dxrecord(foo=1)
 
+    def test_custom_describe_fields(self):
+        dxrecord = dxpy.new_dxrecord(name="recordname", tags=["tag"], details={}, folder="/")
+        self.assertEqual(dxrecord.describe(fields={"name", "tags"}),
+                         {"id": dxrecord.get_id(), "name": "recordname", "tags": ["tag"]})
+        self.assertEqual(dxrecord.describe(fields={"name", "tags"}, default_fields=False),
+                         {"id": dxrecord.get_id(), "name": "recordname", "tags": ["tag"]})
+        describe_with_custom_fields = dxrecord.describe(fields={"name", "properties"}, default_fields=True)
+        self.assertIn('name', describe_with_custom_fields)
+        self.assertIn('modified', describe_with_custom_fields)
+        self.assertIn('properties', describe_with_custom_fields)
+        self.assertNotIn('details', describe_with_custom_fields)
+
+
 @unittest.skipUnless(testutil.TEST_RUN_JOBS, 'skipping test that would run a job')
 class TestDXAppletJob(unittest.TestCase):
     def setUp(self):

commit efe53e8cf3359a52fd917599a4fb61938cfcfa2f

d10b98e8 broke callers that supply args positionally

src/python/dxpy/bindings/__init__.py
@@ -295,7 +295,7 @@ class DXDataObject(DXObject):
 
         return self._proj
 
-    def describe(self, fields=None, default_fields=None, incl_properties=False, incl_details=False, **kwargs):
+    def describe(self, incl_properties=False, incl_details=False, fields=None, default_fields=None, **kwargs):
         """
         :param fields: set of fields to include in the output, for
             example ``{'name', 'modified'}``. The field ``id`` is always