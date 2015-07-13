362469415700d56d72f87669b4ca6202ceb5f9cc
Bill Agee <billagee@gmail.com>
2015-07-13 15:06:09 -0700

    PTFM-15658 Fix --tests cmdline option
    Summary:
    Now that the test files are using absolute_import, it's
    necessary to omit the "test" dir from test names passed
    via the --tests arg - and the working dir when running
    tests needs to be src/python/test/
    
    Also, cleaned up formatting of the usage docstring
    printed with --help.
    
    Test Plan:
    Ran tests using the usage examples:
    * run_python_integration_tests.py (without --tests)
    * "" --tests test_dx_bash_helpers.TestDXBashHelpers.test_basic
    * "" --tests test_dx_bash_helpers.TestDXBashHelpers
    * "" --tests test_dx_bash_helpers
    
    Reviewers: psung
    
    Reviewed By: psung
    
    Differential Revision: https://dnanexus.phacility.com/D437

--- build/run_python_integration_tests.py
+++ build/run_python_integration_tests.py
@@ -3,24 +3,20 @@
 '''
 Runs Python integration tests and merges the resulting test coverage files.
 
-To run a single test method:
+If no arguments are given, all tests in src/python/test/ are run.
 
-   # Runs the method "test_basic" in class TestDXBashHelpers, in file
-   # src/python/test/test_dx_bash_helpers.py
-   build/run_python_integration_tests.py --tests \
-           test.test_dx_bash_helpers.TestDXBashHelpers.test_basic
+To run a single test method, e.g. test_basic in class TestDXBashHelpers,
+in file src/python/test/test_dx_bash_helpers.py:
+
+    $ build/run_python_integration_tests.py --tests test_dx_bash_helpers.TestDXBashHelpers.test_basic
 
 To run an entire test class:
 
-    build/run_python_integration_tests.py --tests \
-            test.test_dx_bash_helpers.TestDXBashHelper
+    $ build/run_python_integration_tests.py --tests test_dx_bash_helpers.TestDXBashHelpers
 
 To run all tests in a file:
 
-    build/run_python_integration_tests.py --tests \
-            test.test_dx_bash_helpers
-
-If no arguments are given, all tests in src/python/test/ are run.
+    $ build/run_python_integration_tests.py --tests test_dx_bash_helpers
 '''
 
 from __future__ import print_function, unicode_literals
@@ -29,16 +25,17 @@ import argparse
 import os
 import subprocess
 
-parser = argparse.ArgumentParser(description=__doc__)
+parser = argparse.ArgumentParser(usage=__doc__)
 parser.add_argument(
     '--tests',
     help='Specify a specific test to run.',
-    metavar='test.test_module.TestCase.test_method',
+    metavar='test_module.TestCase.test_method',
     nargs='*'
 )
 
 TOOLKIT_ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 PYTHON_DIR = os.path.join(TOOLKIT_ROOT_DIR, 'src', 'python')
+PYTHON_TEST_DIR = os.path.join(PYTHON_DIR, 'test')
 
 os.environ['DNANEXUS_INSTALL_PYTHON_TEST_DEPS'] = 'yes'
 
@@ -57,7 +54,7 @@ def run():
         if args.tests:
             cmd += ['-v'] + args.tests
         else:
-            cmd += ['discover', '--start-directory', 'test', '--verbose']
+            cmd += ['discover', '--start-directory', '.', '--verbose']
 
         # Setting COVERAGE_PROCESS_START is required to collect coverage for
         # subprocess calls to dx.py and friends:
@@ -65,7 +62,7 @@ def run():
                 os.environ,
                 COVERAGE_PROCESS_START = os.path.join(PYTHON_DIR, '.coveragerc'),
                 COVERAGE_FILE = os.path.join(PYTHON_DIR, '.coverage'))
-        subprocess.check_call(cmd, cwd=PYTHON_DIR, env=subproc_env)
+        subprocess.check_call(cmd, cwd=PYTHON_TEST_DIR, env=subproc_env)
 
         subprocess.check_call(["coverage", "combine"], cwd=PYTHON_DIR)
     finally:
