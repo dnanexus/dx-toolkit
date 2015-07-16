c254e977a1d126633a5f29aacb7a1de8e1aaf11e
Evan Worley <evanworley@gmail.com>
2015-07-15 17:22:28 -0700

    Attempting to fix integration tests due to error message change

--- src/python/test/test_dxclient.py
+++ src/python/test/test_dxclient.py
@@ -4745,8 +4745,8 @@ class TestDXCp(DXTestCase):
 
         # The file {p1}:/{f} exists, however, {p1}/{f} does not. We
         # want to see an error message that reflects this.
-        expected_err_msg = "ResolutionError: The folder could not be found in {p2}".format(
-            p2=self.project)
+        expected_err_msg = "ResolutionError: The folder /{f} could not be found in {p2}".format(
+            p2=self.project, f=fname1)
         with self.assertSubprocessFailure(stderr_regexp=expected_err_msg, exit_code=3):
             run("dx cp {p1}/{f} {p2}:/".format(p1=self.proj_id1, f=fname1, p2=self.proj_id2))
 
