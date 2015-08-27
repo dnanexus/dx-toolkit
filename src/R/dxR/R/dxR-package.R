7bd69e69c76596ec1204e00c41485b2e21697127
Phil Sung <psung@dnanexus.com>
2015-08-27 01:07:34 +0000

    Changelog and version updates for v0.159.0

--- src/R/dxR/R/dxR-package.R
+++ src/R/dxR/R/dxR-package.R
@@ -4,7 +4,7 @@
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' 0.158.0\cr License: \tab Apache License (== 2.0)\cr
+##' 0.159.0\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
