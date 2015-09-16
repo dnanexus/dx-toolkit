2e7649228905fb0ae60abf7a09674e4ad9a926c7
Kaveh Nafissi <knafissi@dnanexus.com>
2015-09-16 20:15:17 +0000

    Changelog and version updates for v0.162.0, UA 1.5.11

--- src/R/dxR/R/dxR-package.R
+++ src/R/dxR/R/dxR-package.R
@@ -4,7 +4,7 @@
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' 0.161.0\cr License: \tab Apache License (== 2.0)\cr
+##' 0.162.0\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
