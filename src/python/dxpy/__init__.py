dnanexus/dx-toolkit

commit 384e8ef40fbc298f87b103960d87e3e834d2d915

PTFM-13671 dx download improvements
Summary:
- dxpy.DXHTTPRequest uses urllib3 directly instead of via requests. This is faster in many cases.

- dxpy.bindings.dxfile_functions.download_dxfile uses a plain concurrent.futures.ThreadPoolExecutor instead of our custom version, which is much slower.

- download_dxfile downloads along part boundaries.

- download_dxfile retrieves part metadata from file description, validates any data already downloaded, resumes on the next incomplete part, and validates each downloaded part against its MD5 checksum.

- download_dxfile avoids using DXFile read buffers, eliminating unneded buffer copies which were observed to be slow as well.

- download_dxfile avoids setting the Range header for single-part files, allowing nginx to enable gzip compression.

Test Plan: dxpy integration tests

Reviewers: eworley, psung

Reviewed By: psung

Subscribers: psung

Differential Revision: https://dnanexus.phacility.com/D513

src/python/dxpy/__init__.py
@@ -125,13 +125,16 @@ environment variables:
 
 from __future__ import (print_function, unicode_literals)
 
-import os, sys, json, time, logging, platform, collections, ssl, traceback
+import os, sys, json, time, logging, platform, ssl, traceback
 import errno
 import requests
 import socket
+from collections import namedtuple
 
-from requests.exceptions import ConnectionError, HTTPError, Timeout
+from requests.exceptions import HTTPError
 from requests.auth import AuthBase
+from requests.packages import urllib3
+from requests.packages.urllib3.packages.ssl_match_hostname import match_hostname
 from .compat import USING_PYTHON2, expanduser
 
 logger = logging.getLogger(__name__)
@@ -139,9 +142,6 @@ logger.addHandler(logging.NullHandler())
 
 
 def configure_urllib3():
-    from requests.packages import urllib3
-    from requests.packages.urllib3.packages.ssl_match_hostname import match_hostname
-
     # Disable verbose urllib3 warnings and log messages
     urllib3.disable_warnings(category=urllib3.exceptions.InsecurePlatformWarning)
     logging.getLogger('dxpy.packages.requests.packages.urllib3.connectionpool').setLevel(logging.ERROR)
@@ -172,8 +172,6 @@ APISERVER_PROTOCOL = DEFAULT_APISERVER_PROTOCOL
 APISERVER_HOST = DEFAULT_APISERVER_HOST
 APISERVER_PORT = DEFAULT_APISERVER_PORT
 
-SESSION_HANDLERS = collections.defaultdict(requests.session)
-
 DEFAULT_RETRIES = 6
 DEFAULT_TIMEOUT = 600
 DEFAULT_RETRY_AFTER_503_INTERVAL = 60
@@ -184,7 +182,16 @@ _UPGRADE_NOTIFY = True
 USER_AGENT = "{name}/{version} ({platform})".format(name=__name__,
                                                     version=TOOLKIT_VERSION,
                                                     platform=platform.platform())
-
+_default_headers = requests.utils.default_headers()
+_default_headers['DNAnexus-API'] = API_VERSION
+_default_headers['User-Agent'] = USER_AGENT
+_pool_manager = urllib3.PoolManager(maxsize=32,
+                                    cert_reqs=ssl.CERT_REQUIRED,
+                                    ca_certs=requests.certs.where(),
+                                    headers=_default_headers,
+                                    timeout=urllib3.util.timeout.Timeout(connect=DEFAULT_TIMEOUT,
+                                                                         read=DEFAULT_TIMEOUT))
+_RequestForAuth = namedtuple('_RequestForAuth', 'method url headers')
 _expected_exceptions = exceptions.network_exceptions + (exceptions.DXAPIError, )
 
 def _process_method_url_headers(method, url, headers):
@@ -225,19 +232,13 @@ def _is_retryable_exception(e):
     have been established, we return False.
 
     """
-    try:
-        if isinstance(e, ConnectionError):
-            # Unfortunately requests doesn't seem to provide a sensible
-            # API to retrieve the cause
-            cause = e.args[0].args[1]
-            if isinstance(cause, (socket.gaierror, socket.herror)):
-                return True
-            if isinstance(cause, socket.error) and cause.errno in _RETRYABLE_SOCKET_ERRORS:
-                return True
-        return False
-    except (AttributeError, TypeError, IndexError):
-        return False
-
+    if isinstance(e, urllib3.exceptions.MaxRetryError):
+        e = e.reason
+    if isinstance(e, (socket.gaierror, socket.herror)):
+        return True
+    if isinstance(e, socket.error) and e.errno in _RETRYABLE_SOCKET_ERRORS:
+        return True
+    return False
 
 def _extract_msg_from_last_exception():
     ''' Extract a useful error message from the last thrown exception '''
@@ -294,6 +295,7 @@ def DXHTTPRequest(resource, data, method='POST', headers=None, auth=True,
     :type decode_response_body: boolean
     :param prepend_srv: If True, prepends the API server location to the URL
     :type prepend_srv: boolean
+    :param session_handler: Deprecated.
     :param max_retries: Maximum number of retries to perform for a request. A "failed" request is retried if any of the following is true:
 
                         - A response is received from the server, and the content length received does not match the "Content-Length" header.
@@ -320,8 +322,6 @@ def DXHTTPRequest(resource, data, method='POST', headers=None, auth=True,
        through to :func:`DXHTTPRequest`.
 
     '''
-    if session_handler is None:
-        session_handler = SESSION_HANDLERS[os.getpid()]
     if headers is None:
         headers = {}
 
@@ -340,20 +340,19 @@ def DXHTTPRequest(resource, data, method='POST', headers=None, auth=True,
     if auth is True:
         auth = AUTH_HELPER
 
+    if auth is not None:
+        auth(_RequestForAuth(method, url, headers))
+
     if 'verify' not in kwargs and 'DX_CA_CERT' in os.environ:
         kwargs['verify'] = os.environ['DX_CA_CERT']
         if os.environ['DX_CA_CERT'] == 'NOVERIFY':
             kwargs['verify'] = False
-            from requests.packages import urllib3
             urllib3.disable_warnings()
 
     if jsonify_data:
         data = json.dumps(data)
         if 'Content-Type' not in headers and method == 'POST':
             headers['Content-Type'] = 'application/json'
-
-    headers['DNAnexus-API'] = API_VERSION
-    headers['User-Agent'] = USER_AGENT
 
     # If the input is a buffer, its data gets consumed by
     # requests.request (moving the read position). Record the initial
@@ -365,12 +364,14 @@ def DXHTTPRequest(resource, data, method='POST', headers=None, auth=True,
 
     try_index = 0
     while True:
-        success, streaming_response_truncated = True, False
+        success, streaming_response_truncated, time_started = True, False, None
         response = None
         try:
+            if _DEBUG > 0:
+                time_started = time.time()
             _method, _url, _headers = _process_method_url_headers(method, url, headers)
-            response = session_handler.request(_method, _url, headers=_headers, data=data,
-                                               timeout=timeout, auth=auth, **kwargs)
+            response = _pool_manager.request(_method, _url, headers=_headers, body=data,
+                                             timeout=timeout, **kwargs)
 
             if _UPGRADE_NOTIFY and response.headers.get('x-upgrade-info', '').startswith('A recommended update is available') and not os.environ.has_key('_ARGCOMPLETE'):
                 logger.info(response.headers['x-upgrade-info'])
@@ -383,45 +384,42 @@ def DXHTTPRequest(resource, data, method='POST', headers=None, auth=True,
 
             # If an HTTP code that is not in the 200 series is received and the content is JSON, parse it and throw the
             # appropriate error.  Otherwise, raise the usual exception.
-            if response.status_code // 100 != 2:
+            if response.status // 100 != 2:
                 # response.headers key lookup is case-insensitive
                 if response.headers.get('content-type', '').startswith('application/json'):
-                    content = json.loads(response.content.decode('utf-8'))
-                    try:
-                        error_class = getattr(exceptions, content["error"]["type"], exceptions.DXAPIError)
-                    except Exception:
-                        logger.error("Error while parsing content['error']['type']...")
-                        logger.error(content)
-                    raise error_class(content, response.status_code)
-                response.raise_for_status()
+                    content = json.loads(response.data.decode('utf-8'))
+                    error_class = getattr(exceptions, content["error"]["type"], exceptions.DXAPIError)
+                    raise error_class(content, response.status)
+                raise HTTPError("{} {}".format(response.status, response.reason))
 
             if want_full_response:
                 return response
             else:
                 if 'content-length' in response.headers:
-                    if int(response.headers['content-length']) != len(response.content):
+                    if int(response.headers['content-length']) != len(response.data):
                         range_str = (' (%s)' % (headers['Range'],)) if 'Range' in headers else ''
                         raise exceptions.ContentLengthError(
                             "Received response with content-length header set to %s but content length is %d%s" %
-                            (response.headers['content-length'], len(response.content), range_str)
+                            (response.headers['content-length'], len(response.data), range_str)
                         )
 
-                content = response.content
+                content = response.data
 
                 if decode_response_body:
                     content = content.decode('utf-8')
                     if response.headers.get('content-type', '').startswith('application/json'):
                         try:
                             content = json.loads(content)
-                            t = int(response.elapsed.total_seconds() * 1000)
+                            if _DEBUG > 0:
+                                t = int((time.time() - time_started) * 1000)
                             if _DEBUG >= 3:
-                                print(method, url, "<=", response.status_code, "(%dms)" % t,
+                                print(method, url, "<=", response.status, "(%dms)" % t,
                                       "\n" + json.dumps(content, indent=2), file=sys.stderr)
                             elif _DEBUG == 2:
-                                print(method, url, "<=", response.status_code, "(%dms)" % t, json.dumps(content),
+                                print(method, url, "<=", response.status, "(%dms)" % t, json.dumps(content),
                                       file=sys.stderr)
                             elif _DEBUG > 0:
-                                print(method, url, "<=", response.status_code, "(%dms)" % t, Repr().repr(content),
+                                print(method, url, "<=", response.status, "(%dms)" % t, Repr().repr(content),
                                       file=sys.stderr)
                         except ValueError:
                             # If a streaming API call (no content-length
@@ -438,7 +436,7 @@ def DXHTTPRequest(resource, data, method='POST', headers=None, auth=True,
             success = False
             exception_msg = _extract_msg_from_last_exception()
             if isinstance(e, _expected_exceptions):
-                if response is not None and response.status_code == 503:
+                if response is not None and response.status == 503:
                     seconds_to_wait = _extract_retry_after_timeout(response)
                     logger.warn("%s %s: %s. Waiting %d seconds due to server unavailability...",
                                 method, url, exception_msg, seconds_to_wait)
@@ -462,7 +460,7 @@ def DXHTTPRequest(resource, data, method='POST', headers=None, auth=True,
                        streaming_response_truncated:
                         ok_to_retry = always_retry or (method == 'GET') or _is_retryable_exception(e)
                     else:
-                        ok_to_retry = 500 <= response.status_code < 600
+                        ok_to_retry = 500 <= response.status < 600
 
                 if ok_to_retry:
                     if rewind_input_buffer_offset is not None:

src/python/dxpy/bindings/dxfile_functions.py
@@ -22,12 +22,21 @@ The following helper functions are useful shortcuts for interacting with File ob
 
 '''
 
-from __future__ import (print_function, unicode_literals)
+from __future__ import print_function, unicode_literals, division, absolute_import
 
 import os, sys, math, mmap, stat
+import hashlib
+import traceback
+from collections import defaultdict
+from multiprocessing import cpu_count
+from concurrent.futures import ThreadPoolExecutor
 
 import dxpy
+from .. import logger, DXHTTPRequest
 from . import dxfile, DXFile
+from .dxfile import FILE_REQUEST_TIMEOUT
+from ..compat import open
+from ..exceptions import DXFileError, DXPartLengthMismatchError, DXChecksumMismatchError
 
 def open_dxfile(dxid, project=None, read_buffer_size=dxfile.DEFAULT_BUFFER_SIZE):
     '''
@@ -79,18 +88,25 @@ def new_dxfile(mode=None, write_buffer_size=dxfile.DEFAULT_BUFFER_SIZE, **kwargs
     dx_file.new(**kwargs)
     return dx_file
 
-def download_dxfile(dxid, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append=False, show_progress=False,
+_download_retry_counter = defaultdict(lambda: 3)
+_executor = None
+def _get_executor():
+    global _executor
+    if _executor is None:
+        _executor = ThreadPoolExecutor(max_workers=cpu_count())
+    return _executor
+
+def download_dxfile(dxfile_or_id, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append=False, show_progress=False,
                     project=None, **kwargs):
     '''
-    :param dxid: Remote file ID
-    :type dxid: string
+    :param dxfile_or_id: Remote file handler or ID
+    :type dxfile_or_id: DXFile or string
     :param filename: Local filename
     :type filename: string
     :param append: If True, appends to the local file (default is to truncate local file if it exists)
     :type append: boolean
 
-    Downloads the remote file with object ID *dxid* and saves it to
-    *filename*.
+    Downloads the remote file referenced by *dxfile_or_id* and saves it to *filename*.
 
     Example::
 
@@ -98,7 +114,7 @@ def download_dxfile(dxid, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append
 
     '''
 
-    def print_progress(bytes_downloaded, file_size):
+    def print_progress(bytes_downloaded, file_size, action="Downloaded"):
         num_ticks = 60
 
         effective_file_size = file_size or 1
@@ -108,39 +124,145 @@ def download_dxfile(dxid, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append
         ticks = int(round((bytes_downloaded / float(effective_file_size)) * num_ticks))
         percent = int(round((bytes_downloaded / float(effective_file_size)) * 100))
 
-        fmt = "[{done}{pending}] Downloaded {done_bytes:,}{remaining} bytes ({percent}%) {name}"
-        sys.stderr.write(fmt.format(done=('=' * (ticks - 1) + '>') if ticks > 0 else '',
-                                    pending=' ' * (num_ticks - ticks),
+        fmt = "[{done}{pending}] {action} {done_bytes:,}{remaining} bytes ({percent}%) {name}"
+        sys.stderr.write(fmt.format(action=action,
+                                    done=("=" * (ticks - 1) + ">") if ticks > 0 else "",
+                                    pending=" " * (num_ticks - ticks),
                                     done_bytes=bytes_downloaded,
-                                    remaining=' of {size:,}'.format(size=file_size) if file_size else "",
+                                    remaining=" of {size:,}".format(size=file_size) if file_size else "",
                                     percent=percent,
                                     name=filename))
         sys.stderr.flush()
         sys.stderr.write("\r")
         sys.stderr.flush()
 
-    file_size = None
     _bytes = 0
 
-    mode = 'ab' if append else 'wb'
-    with DXFile(dxid, mode='r', project=project, read_buffer_size=chunksize) as dxfile, open(filename, mode) as fd:
-        if show_progress:
-            print_progress(0, None)
-        while True:
-            file_content = dxfile.read(chunksize, **kwargs)
-            if file_size is None:
-                file_size = dxfile._file_length
+    if isinstance(dxfile_or_id, DXFile):
+        dxfile = dxfile_or_id
+    else:
+        dxfile = DXFile(dxfile_or_id, mode="r", project=project)
 
+    dxfile_desc = dxfile.describe(fields={"parts"}, default_fields=True, **kwargs)
+    parts = dxfile_desc["parts"]
+    parts_to_get = sorted(parts, key=int)
+    file_size = dxfile_desc.get("size")
+    chunks_to_get = []
+
+    # Warm up the download URL cache in the file handler, to avoid all worker threads trying to fetch it simultaneously
+    dxfile.get_download_url(**kwargs)
+
+    offset = 0
+    for part_id in parts_to_get:
+        parts[part_id]["start"] = offset
+        offset += parts[part_id]["size"]
+
+    def get_chunk(chunk_info):
+        url, headers = dxfile.get_download_url(**kwargs)
+        # If we're fetching the whole object in one shot, avoid setting the Range header to take advantage of gzip
+        # transfer compression
+        if len(parts) > 1 or len(chunks_to_get) > 1:
+            headers["Range"] = "bytes={}-{}".format(chunk_info["start"], chunk_info["end"])
+        chunk_info["data"] = DXHTTPRequest(url, b"", method="GET", headers=headers, auth=None, jsonify_data=False,
+                                           prepend_srv=False, always_retry=True, timeout=FILE_REQUEST_TIMEOUT,
+                                           decode_response_body=False)
+        return chunk_info
+
+    if append:
+        fh = open(filename, "ab")
+    else:
+        try:
+            fh = open(filename, "rb+")
+        except IOError:
+            fh = open(filename, "wb")
+
+    if show_progress:
+        print_progress(0, None)
+
+    if fh.mode == "rb+":
+        last_verified_part, last_verified_pos, max_verify_chunk_size = None, 0, 1024*1024
+        try:
+            for part_id in parts_to_get:
+                part_info = parts[part_id]
+                bytes_to_read = part_info["size"]
+                hasher = hashlib.md5()
+                while bytes_to_read > 0:
+                    chunk = fh.read(min(max_verify_chunk_size, bytes_to_read))
+                    if len(chunk) < min(max_verify_chunk_size, bytes_to_read):
+                        raise DXFileError("Local data for part {} is truncated".format(part_id))
+                    hasher.update(chunk)
+                    bytes_to_read -= max_verify_chunk_size
+                if hasher.hexdigest() != part_info["md5"]:
+                    raise DXFileError("Checksum mismatch when verifying downloaded part {}".format(part_id))
+                else:
+                    last_verified_part = part_id
+                    last_verified_pos = fh.tell()
+                    if show_progress:
+                        _bytes += part_info["size"]
+                        print_progress(_bytes, file_size, action="Verified")
+        except (IOError, DXFileError) as e:
+            logger.debug(e)
+        fh.seek(last_verified_pos)
+        fh.truncate()
+        if last_verified_part is not None:
+            del parts_to_get[:parts_to_get.index(last_verified_part)+1]
+        if show_progress and len(parts_to_get) < len(parts):
+            print_progress(last_verified_pos, file_size, action="Resuming at")
+        logger.debug("Verified %s/%d downloaded parts", last_verified_part, len(parts_to_get))
+
+    for part_id in parts_to_get:
+        part_info = parts[part_id]
+        for chunk_start in range(part_info["start"], part_info["start"] + part_info["size"], chunksize):
+            chunk_end = min(chunk_start + chunksize, part_info["start"] + part_info["size"]) - 1
+            chunks_to_get.append(dict(part=part_id, start=chunk_start, end=chunk_end))
+
+    def verify_part(part_id, got_bytes, hasher):
+        if got_bytes is not None and got_bytes != parts[part_id]["size"]:
+            msg = "Unexpected part data size in {} part {} (expected {}, got {})"
+            msg = msg.format(dxfile.get_id(), part_id, got_bytes, parts[part_id]["size"])
+            raise DXPartLengthMismatchError(msg)
+        if hasher is not None and hasher.hexdigest() != parts[part_id]["md5"]:
+            msg = "Checksum mismatch in {} part {} (expected {}, got {})"
+            msg = msg.format(dxfile.get_id(), part_id, hasher.hexdigest(), parts[part_id]["md5"])
+            raise DXChecksumMismatchError(msg)
+
+    try:
+        cur_part, got_bytes, hasher = None, None, None
+        # Timeout is required for non-blocking join that can be interrupted by SIGINT (Ctrl+C)
+        for chunk in _get_executor().map(get_chunk, chunks_to_get, timeout=sys.maxint):
+            if chunk["part"] != cur_part:
+                verify_part(cur_part, got_bytes, hasher)
+                cur_part, got_bytes, hasher = chunk["part"], 0, hashlib.md5()
+            got_bytes += len(chunk["data"])
+            hasher.update(chunk["data"])
+            fh.write(chunk["data"])
             if show_progress:
-                _bytes += len(file_content)
+                _bytes += len(chunk["data"])
                 print_progress(_bytes, file_size)
+        verify_part(cur_part, got_bytes, hasher)
+        if show_progress:
+            print_progress(_bytes, file_size, action="Completed")
+    except DXFileError:
+        part_gid = dxfile.get_id() + str(cur_part)
+        print(traceback.format_exc(), file=sys.stderr)
+        _download_retry_counter[part_gid] -= 1
+        if _download_retry_counter[part_gid] > 0:
+            print("Retrying {} ({} tries remain)".format(dxfile.get_id(), _download_retry_counter[part_gid]))
+            return download_dxfile(dxfile_or_id, filename, chunksize=chunksize, append=append,
+                                   show_progress=show_progress, project=project, **kwargs)
+        raise
+    except KeyboardInterrupt:
+        # Call os._exit() in case of KeyboardInterrupt. Otherwise, the atexit registered handler in
+        # concurrent.futures.thread will run, and issue blocking join() on all worker threads, requiring us to
+        # listen to events in worker threads in order to enable timely exit in response to Ctrl-C.
+        print("", file=sys.stderr)
+        os._exit(os.EX_IOERR)
 
-            if len(file_content) == 0:
-                if show_progress:
-                    sys.stderr.write("\n")
-                break
+    if show_progress:
+        sys.stderr.write("\n")
 
-            fd.write(file_content)
+    fh.close()
+
 
 def _get_buffer_size_for_file(file_size, file_is_mmapd=False):
     """Returns an upload buffer size that is appropriate to use for a file

src/python/dxpy/exceptions.py
@@ -20,7 +20,7 @@ Exceptions for the :mod:`dxpy` package.
 
 from __future__ import (print_function, unicode_literals)
 
-import os, sys, json, traceback, errno
+import sys, json, traceback, errno, socket
 import requests
 
 import dxpy
@@ -107,6 +107,12 @@ class ServiceUnavailable(DXAPIError):
 class DXFileError(DXError):
     '''Exception for :class:`dxpy.bindings.dxfile.DXFile`.'''
     pass
+
+class DXPartLengthMismatchError(DXFileError):
+    '''Exception raised by :class:`dxpy.bindings.dxfile.DXFile` on part length mismatch.'''
+
+class DXChecksumMismatchError(DXFileError):
+    '''Exception raised by :class:`dxpy.bindings.dxfile.DXFile` on checksum mismatch.'''
 
 class DXGTableError(DXError):
     '''Exception for :class:`dxpy.bindings.dxgtable.DXGTable`.'''
@@ -202,11 +208,11 @@ def exit_with_exc_info(code=1, message='', print_tb=False, exception=None):
         sys.stderr.write('\n')
     sys.exit(code)
 
-network_exceptions = (requests.ConnectionError,
-                      requests.exceptions.ChunkedEncodingError,
-                      requests.exceptions.ContentDecodingError,
-                      requests.HTTPError,
-                      requests.Timeout,
+network_exceptions = (requests.packages.urllib3.exceptions.ProtocolError,
+                      requests.packages.urllib3.exceptions.DecodeError,
+                      requests.packages.urllib3.exceptions.ReadTimeoutError,
+                      requests.packages.urllib3.exceptions.MaxRetryError,
+                      socket.error,
                       requests.packages.urllib3.connectionpool.HTTPException)
 
 default_expected_exceptions = network_exceptions + (DXAPIError,

src/python/test/test_dxclient.py
@@ -21,6 +21,7 @@ from __future__ import print_function, unicode_literals, division, absolute_impo
 
 import os, sys, unittest, json, tempfile, subprocess, csv, shutil, re, base64, random, time
 import pipes
+import hashlib
 from contextlib import contextmanager
 import pexpect
 import requests
@@ -1250,6 +1251,34 @@ dxpy.run()
             gen_file("X.txt", proj_id)
             buf = run("dx download -o - X.txt")
             self.assertEqual(buf, data)
+
+    def test_dx_download_resume_and_checksum(self):
+        def assert_md5_checksum(filename, hasher):
+            with open(filename) as fh:
+                self.assertEqual(hashlib.md5(fh.read()).hexdigest(), hasher.hexdigest())
+
+        # Manually upload 2 parts
+        part1, part2 = b"0123456789ABCDEF"*1024*64*5, b"0"
+        dxfile = dxpy.new_dxfile(name="test")
+        dxfile.upload_part(part1, index=1)
+        dxfile.upload_part(part2, index=2)
+        dxfile.close(block=True)
+
+        wd = tempfile.mkdtemp()
+        run("cd {wd}; dx download test; ls -la".format(wd=wd))
+        assert_md5_checksum(os.path.join(wd, "test"), hashlib.md5(part1 + part2))
+        run("cd {wd}; truncate -s $((1024*1024*5)) test".format(wd=wd))
+        run("cd {wd}; dx download -f test".format(wd=wd))
+        assert_md5_checksum(os.path.join(wd, "test"), hashlib.md5(part1 + part2))
+        run("cd {wd}; truncate -s $((1024*1024*5 - 1)) test".format(wd=wd))
+        run("cd {wd}; dx download -f test".format(wd=wd))
+        assert_md5_checksum(os.path.join(wd, "test"), hashlib.md5(part1 + part2))
+        run("cd {wd}; truncate -s 1 test".format(wd=wd))
+        run("cd {wd}; dx download -f test".format(wd=wd))
+        assert_md5_checksum(os.path.join(wd, "test"), hashlib.md5(part1 + part2))
+        run("cd {wd}; rm test; touch test".format(wd=wd))
+        run("cd {wd}; dx download -f test".format(wd=wd))
+        assert_md5_checksum(os.path.join(wd, "test"), hashlib.md5(part1 + part2))
 
 
 class TestDXClientDescribe(DXTestCase):

commit c6b6e5af7bb0d6b08e1695ad4ee390c85ef3ef49

Fix connection state issue introduced by 384e8ef
With 384e8ef, dxpy.DXHTTPRequest(..., **kwargs) now passes
unrecognized kwargs to urllib3, not to requests. This causes
unrecognized kwargs to propagate all the way down to httplib, exposing
a way for httplib to end up in an inconsistent state (if it crashes
due to the unrecognized kwarg or for other reasons) and fail to serve
a subsequent request that wants to reuse the same connection.

This change abandons all the cached connections to eliminate this
possbility. It may be too heavy-handed, but the overhead of opening a
new connection is relatively low.

src/python/dxpy/__init__.py
@@ -442,6 +442,10 @@ def DXHTTPRequest(resource, data, method='POST', headers=None, auth=True,
                 return content
             raise AssertionError('Should never reach this line: expected a result to have been returned by now')
         except Exception as e:
+            # Avoid reusing connections in the pool, since they may be
+            # in an inconsistent state (observed as "ResponseNotReady"
+            # errors).
+            _get_pool_manager(kwargs).clear()
             success = False
             exception_msg = _extract_msg_from_last_exception()
             if isinstance(e, _expected_exceptions):