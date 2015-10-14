125d2c449b8988bf95444104fc3b46b884c9c86f
Andrey Kislyuk <akislyuk@dnanexus.com>
2015-10-14 13:02:23 -0700

    Simplify rate-limited concurrent executor; use it in dx download
    Summary:
    384e8ef contains a design flaw: it does not rate-limit the execution
    of futures by the consumption of their results. This lack of
    backpressure causes dx download to run out of memory whenever local
    storage I/O can't keep up with network I/O and the memory gets
    exhausted. Manifestations of this issue include both soft (part
    truncation and retry) and hard (OOM kill) failures.
    
    We already have a rate-limited executor in
    dxpy.utils.response_iterator. In addition to plugging dx download into
    this standard DXFile executor, this change removes two unused pieces
    of functionality from the executor, and refactors it slightly to
    restrict the scope of several variables to aid garbage collection.
    
    Luckily, the performance gains are observed even with the rate-limited
    executor (although we may want to adjust its rate headroom later).
    
    Test Plan:
    existing tests and manual testing on c3.large series
    instances with 10GB files and 96MB parts (DX_JOB_ID environment
    variable set at upload time) using "/usr/bin/time -v dx download ...".
    
    Reviewers: pnguyen
    
    Reviewed By: pnguyen
    
    Subscribers: psung, eworley
    
    Differential Revision: https://dnanexus.phacility.com/D571

--- src/python/dxpy/bindings/dxfile.py
+++ src/python/dxpy/bindings/dxfile.py
@@ -25,6 +25,7 @@ from __future__ import (print_function, unicode_literals)
 
 import os, sys, logging, traceback, hashlib, copy, time
 import concurrent.futures
+from multiprocessing import cpu_count
 
 import dxpy
 from . import DXDataObject
@@ -32,8 +33,7 @@ from ..exceptions import DXFileError
 from ..utils import warn
 from ..compat import BytesIO
 
-# TODO: adaptive buffer size
-DXFILE_HTTP_THREADS = 8
+DXFILE_HTTP_THREADS = cpu_count()
 DEFAULT_BUFFER_SIZE = 1024*1024*16
 if dxpy.JOB_ID:
     # Increase HTTP request buffer size when we are running within the
@@ -577,9 +577,7 @@ class DXFile(DXDataObject):
         if self._response_iterator is None:
             self._response_iterator = dxpy.utils.response_iterator(
                 self._request_iterator,
-                self._http_threadpool,
-                max_active_tasks=self._http_threadpool_size,
-                queue_id=id(self)
+                self._http_threadpool
             )
         return next(self._response_iterator)
 
