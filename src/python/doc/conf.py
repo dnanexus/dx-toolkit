cefe7a65f6634e0589fa9016028380524ba38366
Phil Sung <psung@dnanexus.com>
2015-12-16 16:14:33 -0800

    Fix copyright date in Sphinx docs

--- src/python/doc/conf.py
+++ src/python/doc/conf.py
@@ -59,7 +59,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DNAnexus Python Bindings'
-copyright = u'2012-2013, DNAnexus'
+copyright = u'2012-2015, DNAnexus'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
