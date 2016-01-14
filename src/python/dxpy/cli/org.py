dnanexus/dx-toolkit

commit 98b67c747f149478a997d860f52c6b117cbb8405

PTFM-17107 Modify I/Os of "dx {add,remove,update} member".
Summary:
"dx {add,remove,update} member" now parses user ids as well as usernames. This
will improve UX because "dx new user" and "dx find org_members" return user
ids.

Test Plan:
-> pnguyen@pnguyen:~/src/nucleus$ bin/run_integration_tests.sh \
   --client_tests_only --test test_dxclient.TestDXClientMembership

Reviewers: psung

Reviewed By: psung

Differential Revision: https://dnanexus.phacility.com/D714

src/python/dxpy/cli/org.py
@@ -29,16 +29,27 @@ from dxpy.utils.printing import (fill, DELIMITER, format_find_results)
 import json
 
 
+def get_user_id(user_id_or_username):
+    """Gets the user ID based on the value `user_id_or_username` specified on
+    the command-line, being extra lenient and lowercasing the value in all
+    cases.
+    """
+    user_id_or_username = user_id_or_username.lower()
+    if not user_id_or_username.startswith("user-"):
+        user_id = "user-" + user_id_or_username.lower()
+    else:
+        user_id = user_id_or_username
+    return user_id
+
+
 def get_org_invite_args(args):
     """
     PRECONDITION:
         - If /org-x/invite is being called in conjunction with /user/new, then
           `_validate_new_user_input()` has been called on `args`; otherwise,
           the parser must perform all the basic input validation.
-        - `args.username` is well-formed and valid (e.g. it does not start with
-          "user-").
     """
-    org_invite_args = {"invitee": "user-" + args.username.lower()}
+    org_invite_args = {"invitee": get_user_id(args.username_or_user_id)}
     org_invite_args["level"] = args.level
     if "set_bill_to" in args and args.set_bill_to is True:
         # /org-x/invite is called in conjunction with /user/new.
@@ -52,8 +63,10 @@ def get_org_invite_args(args):
 
 
 def add_membership(args):
+    user_id = get_user_id(args.username_or_user_id)
+
     try:
-        dxpy.api.org_find_members(args.org_id, {"id": ["user-" + args.username.lower()]})["results"][0]
+        dxpy.api.org_find_members(args.org_id, {"id": [user_id]})["results"][0]
     except:
         pass
     else:
@@ -64,29 +77,30 @@ def add_membership(args):
     if args.brief:
         print("org-" + args.org_id)
     else:
-        print(fill("Invited user-{u} to {o}".format(u=args.username.lower(),
-                                                    o=args.org_id)))
+        print(fill("Invited {u} to {o}".format(u=user_id, o=args.org_id)))
 
 
 def _get_org_remove_member_args(args):
     remove_member_args = {
-        "user": "user-" + args.username.lower(),
+        "user": get_user_id(args.username_or_user_id),
         "revokeProjectPermissions": args.revoke_project_permissions,
         "revokeAppPermissions": args.revoke_app_permissions}
     return remove_member_args
 
 
 def remove_membership(args):
+    user_id = get_user_id(args.username_or_user_id)
+
     try:
-        dxpy.api.org_find_members(args.org_id, {"id": ["user-" + args.username.lower()]})["results"][0]
+        dxpy.api.org_find_members(args.org_id, {"id": [user_id]})["results"][0]
     except IndexError:
         raise DXCLIError("Cannot remove a user who is not a member of the org")
 
     confirmed = not args.confirm
     if not confirmed:
         # Request interactive confirmation.
-        print(fill("WARNING: About to remove user-{u} from {o}; project permissions will{rpp} be removed and app permissions will{rap} be removed".format(
-            u=args.username.lower(), o=args.org_id,
+        print(fill("WARNING: About to remove {u} from {o}; project permissions will{rpp} be removed and app permissions will{rap} be removed".format(
+            u=user_id, o=args.org_id,
             rpp="" if args.revoke_project_permissions else " not",
             rap="" if args.revoke_app_permissions else " not")))
 
@@ -99,29 +113,25 @@ def remove_membership(args):
         if args.brief:
             print(result["id"])
         else:
-            print(fill("Removed user-{u} from {o}".format(u=args.username.lower(),
-                                                          o=args.org_id)))
-            print(fill("Removed user-{u} from the following projects:".format(
-                u=args.username.lower())))
+            print(fill("Removed {u} from {o}".format(u=user_id, o=args.org_id)))
+            print(fill("Removed {u} from the following projects:".format(u=user_id)))
             if len(result["projects"].keys()) != 0:
                 for project_id in result["projects"].keys():
                     print("\t{p}".format(p=project_id))
             else:
                 print("\tNone")
-            print(fill("Removed user-{u} from the following apps:".format(
-                u=args.username.lower())))
+            print(fill("Removed {u} from the following apps:".format(u=user_id)))
             if len(result["apps"].keys()) != 0:
                 for app_id in result["apps"].keys():
                     print("\t{a}".format(a=app_id))
             else:
                 print("\tNone")
     else:
-        print(fill("Aborting removal of user-{u} from {o}".format(
-            u=args.username.lower(), o=args.org_id)))
+        print(fill("Aborting removal of {u} from {o}".format(u=user_id, o=args.org_id)))
 
 
 def _get_org_set_member_access_args(args, current_level):
-    user_id = "user-" + args.username.lower()
+    user_id = get_user_id(args.username_or_user_id)
     org_set_member_access_input = {user_id: {}}
 
     if args.level is not None:
@@ -150,8 +160,10 @@ def _get_org_set_member_access_args(args, current_level):
 
 
 def update_membership(args):
+    user_id = get_user_id(args.username_or_user_id)
+
     try:
-        member_access = dxpy.api.org_find_members(args.org_id, {"id": ["user-" + args.username.lower()]})["results"][0]
+        member_access = dxpy.api.org_find_members(args.org_id, {"id": [user_id]})["results"][0]
     except IndexError:
         raise DXCLIError("Cannot update a user who is not a member of the org")
 
@@ -163,8 +175,7 @@ def update_membership(args):
     if args.brief:
         print(result["id"])
     else:
-        print(fill("Updated membership of user-{u} in {o}".format(
-            u=args.username.lower(), o=args.org_id)))
+        print(fill("Updated membership of {u} in {o}".format(u=user_id, o=args.org_id)))
 
 
 def _get_find_orgs_args(args):

src/python/dxpy/scripts/dx.py
@@ -3790,7 +3790,7 @@ register_subparser(parser_add_stage, subparsers_action=subparsers_add, categorie
 
 parser_add_member = subparsers_add.add_parser("member", help="Grant a user membership to an org", description="Grant a user membership to an org", prog="dx add member", parents=[stdout_args, env_args])
 parser_add_member.add_argument("org_id", help="ID of the org")
-parser_add_member.add_argument("username", help="Username")
+parser_add_member.add_argument("username_or_user_id", help="Username or ID of user")
 parser_add_member.add_argument("--level", required=True, choices=["ADMIN", "MEMBER"], help="Org membership level that will be granted to the specified user")
 parser_add_member.add_argument("--allow-billable-activities", default=False, action="store_true", help='Grant the specified user "allowBillableActivities" in the org')
 parser_add_member.add_argument("--no-app-access", default=True, action="store_false", dest="app_access", help='Disable "appAccess" for the specified user in the org')
@@ -3865,7 +3865,7 @@ register_subparser(parser_remove_stage, subparsers_action=subparsers_remove, cat
 
 parser_remove_member = subparsers_remove.add_parser("member", help="Revoke the org membership of a user", description="Revoke the org membership of a user", prog="dx remove member", parents=[stdout_args, env_args])
 parser_remove_member.add_argument("org_id", help="ID of the org")
-parser_remove_member.add_argument("username", help="Username")
+parser_remove_member.add_argument("username_or_user_id", help="Username or ID of user")
 parser_remove_member.add_argument("--keep-explicit-project-permissions", default=True, action="store_false", dest="revoke_project_permissions", help="Disable revocation of explicit project permissions of the specified user to projects billed to the org; implicit project permissions (i.e. those granted to the specified user via his membership in this org) will always be revoked")
 parser_remove_member.add_argument("--keep-explicit-app-permissions", default=True, action="store_false", dest="revoke_app_permissions", help="Disable revocation of explicit app developer and user permissions of the specified user to apps billed to the org; implicit app permissions (i.e. those granted to the specified user via his membership in this org) will always be revoked")
 parser_remove_member.add_argument("-y", "--yes", action="store_false", dest="confirm", help="Do not ask for confirmation")
@@ -3936,7 +3936,7 @@ register_subparser(parser_update_stage, subparsers_action=subparsers_update, cat
 
 parser_update_member = subparsers_update.add_parser("member", help="Update the membership of a user in an org", description="Update the membership of a user in an org", prog="dx update member", parents=[stdout_args, env_args])
 parser_update_member.add_argument("org_id", help="ID of the org")
-parser_update_member.add_argument("username", help="Username")
+parser_update_member.add_argument("username_or_user_id", help="Username or ID of user")
 parser_update_member.add_argument("--level", choices=["ADMIN", "MEMBER"], help="The new org membership level of the specified user")
 parser_update_member.add_argument("--allow-billable-activities", choices=["true", "false"], help='The new "allowBillableActivities" membership permission of the specified user in the org; default false if demoting the specified user from ADMIN to MEMBER')
 parser_update_member.add_argument("--app-access", choices=["true", "false"], help='The new "appAccess" membership permission of the specified user in the org; default true if demoting the specified user from ADMIN to MEMBER')

src/python/test/test_dxclient.py
@@ -5049,12 +5049,7 @@ class TestDXClientMembership(DXTestCase):
 
         cmd = "dx update member {o} {u} --level MEMBER --allow-billable-activities true"
         run(cmd.format(o=self.org_id, u=self.username))
-        exp_membership = {"id": self.user_id,
-                          "level": "MEMBER",
-                          "allowBillableActivities": True,
-                          "createProjectsAndApps": True,
-                          "appAccess": True,
-                          "projectAccess": "UPLOAD"}
+        exp_membership.update(allowBillableActivities=True, createProjectsAndApps=True)
         membership = self._org_find_members(self.user_id)
         self.assertEqual(membership, exp_membership)
 
@@ -5071,17 +5066,56 @@ class TestDXClientMembership(DXTestCase):
 
         cmd = "dx update member {o} {u} --level MEMBER --allow-billable-activities true --project-access CONTRIBUTE --app-access false"
         run(cmd.format(o=self.org_id, u=self.username))
-        exp_membership = {"id": self.user_id,
-                          "level": "MEMBER",
-                          "allowBillableActivities": True,
-                          "createProjectsAndApps": True,
-                          "appAccess": False,
-                          "projectAccess": "CONTRIBUTE"}
+        exp_membership.update(level="MEMBER", projectAccess="CONTRIBUTE", appAccess=False)
         membership = self._org_find_members(self.user_id)
         self.assertEqual(membership, exp_membership)
 
         cmd = "dx remove member {o} {u} -y"
         run(cmd.format(o=self.org_id, u=self.username))
+
+        with self.assertRaises(IndexError):
+            self._org_find_members(self.user_id)
+
+    def test_add_update_remove_membership_with_user_id(self):
+        # This is similar to `test_add_update_remove_membership()` above, but
+        # it specifies user id instead of username as arg to `dx` command.
+
+        cmd = "dx add member {o} {u} --level {l} --project-access UPLOAD"
+        run(cmd.format(o=self.org_id, u=self.user_id, l="MEMBER"))
+        exp_membership = {"id": self.user_id,
+                          "level": "MEMBER",
+                          "allowBillableActivities": False,
+                          "createProjectsAndApps": False,
+                          "appAccess": True,
+                          "projectAccess": "UPLOAD"}
+        membership = self._org_find_members(self.user_id)
+        self.assertEqual(membership, exp_membership)
+
+        cmd = "dx update member {o} {u} --level MEMBER --allow-billable-activities true"
+        run(cmd.format(o=self.org_id, u=self.user_id))
+        exp_membership.update(allowBillableActivities=True, createProjectsAndApps=True)
+        membership = self._org_find_members(self.user_id)
+        self.assertEqual(membership, exp_membership)
+
+        cmd = "dx update member {o} {u} --level ADMIN"
+        run(cmd.format(o=self.org_id, u=self.user_id))
+        exp_membership = {"id": self.user_id,
+                          "level": "ADMIN",
+                          "allowBillableActivities": True,
+                          "createProjectsAndApps": True,
+                          "appAccess": True,
+                          "projectAccess": "ADMINISTER"}
+        membership = self._org_find_members(self.user_id)
+        self.assertEqual(membership, exp_membership)
+
+        cmd = "dx update member {o} {u} --level MEMBER --allow-billable-activities true --project-access CONTRIBUTE --app-access false"
+        run(cmd.format(o=self.org_id, u=self.user_id))
+        exp_membership.update(level="MEMBER", projectAccess="CONTRIBUTE", appAccess=False)
+        membership = self._org_find_members(self.user_id)
+        self.assertEqual(membership, exp_membership)
+
+        cmd = "dx remove member {o} {u} -y"
+        run(cmd.format(o=self.org_id, u=self.user_id))
 
         with self.assertRaises(IndexError):
             self._org_find_members(self.user_id)

commit b73ca4464f9ed637f0f32ac6d7901f709f57ed2e

Fix bug introduced by 98b67c7.

src/python/dxpy/cli/org.py
@@ -42,14 +42,18 @@ def get_user_id(user_id_or_username):
     return user_id
 
 
-def get_org_invite_args(args):
+def get_org_invite_args(user_id, args):
     """
+    Used by:
+        - `dx new user`
+        - `dx add member`
+
     PRECONDITION:
         - If /org-x/invite is being called in conjunction with /user/new, then
           `_validate_new_user_input()` has been called on `args`; otherwise,
           the parser must perform all the basic input validation.
     """
-    org_invite_args = {"invitee": get_user_id(args.username_or_user_id)}
+    org_invite_args = {"invitee": user_id}
     org_invite_args["level"] = args.level
     if "set_bill_to" in args and args.set_bill_to is True:
         # /org-x/invite is called in conjunction with /user/new.
@@ -72,7 +76,7 @@ def add_membership(args):
     else:
         raise DXCLIError("Cannot add a user who is already a member of the org")
 
-    dxpy.api.org_invite(args.org_id, get_org_invite_args(args))
+    dxpy.api.org_invite(args.org_id, get_org_invite_args(user_id, args))
 
     if args.brief:
         print("org-" + args.org_id)

src/python/dxpy/scripts/dx.py
@@ -1296,7 +1296,7 @@ def new_user(args):
     user_id = "user-" + args.username.lower()
     if args.org is not None:
         # Invite new user to org.
-        dxpy.api.org_invite(args.org, get_org_invite_args(args))
+        dxpy.api.org_invite(args.org, get_org_invite_args(user_id, args))
 
     if args.brief:
         print(user_id)