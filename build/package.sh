87d604049e32fa97e0c41e6aa67bae49c1e938bd
Andrey Kislyuk <kislyuk@gmail.com>
2016-01-21 17:13:39 -0800

    PTFM-17184 refactor make python
    Summary:
    Eliminate complex, brittle, and unnecessary logic from the
    Makefile that can (now that pip supports this use case) be replaced
    with a simple "cd python; pip install .".
    
    This has the side effect of no longer distributing Python packaging tools (easy_install, pip, pkg_resources, setuptools, wheel) with dx-toolkit. That prevents situations where our copies of these packaging tools interfere with other copies on the host.
    
    Test Plan: Tested manually on 12.04, 14.04, and Mac.
    
    Reviewers: psung
    
    Reviewed By: psung
    
    Differential Revision: https://dnanexus.phacility.com/D735

--- build/package.sh
+++ build/package.sh
@@ -32,7 +32,7 @@ cd "${DNANEXUS_HOME}"
 make clean
 make
 rm Makefile
-rm -rf debian src/{java,javascript,perl,R,ruby,ua,python/build,{dx-verify-file,dx-contigset-to-fasta,dx-wig-to-wiggle}/build} build/py27_env share/dnanexus/lib/javascript
+rm -rf debian src/{java,javascript,perl,R,ruby,ua,python/build,{dx-verify-file,dx-contigset-to-fasta,dx-wig-to-wiggle}/build} build/py_env share/dnanexus/lib/javascript
 mv build/Prebuilt-Readme.md Readme.md
 
 "$(dirname $0)/fix_shebang_lines.sh" bin "/usr/bin/env python2.7"
