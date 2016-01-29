2ae24550a2d2b9e9c57598ee2b76e10fa9875003
Andrey Kislyuk <kislyuk@gmail.com>
2016-01-29 13:25:41 -0800

    dxpy: Eliminate possibility of header sharing among requests

--- src/python/dxpy/bindings/dxfile_functions.py
+++ src/python/dxpy/bindings/dxfile_functions.py
@@ -202,6 +202,7 @@ def download_dxfile(dxid, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append
 
     def get_chunk(part_id_to_get, start, end):
         url, headers = dxfile.get_download_url(project=project, **kwargs)
+        headers = dict(headers)
         # If we're fetching the whole object in one shot, avoid setting the Range header to take advantage of gzip
         # transfer compression
         if len(parts) > 1 or end - start + 1 < parts[part_id_to_get]["size"]:
