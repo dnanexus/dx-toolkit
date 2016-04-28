889c1207d8ae83f565d0d5dd5677a649f4fea249
Igor Voloshanenko <ivoloshanenko@dnanexus.com>
2016-04-28 23:29:35 +0300

    https://dnanexus.jira.com/browse/PTFM-17818 Allow user to specify all --debug-on options at once in dx run

--- src/python/test/test_dxclient.py
+++ src/python/test/test_dxclient.py
@@ -878,6 +878,33 @@ class TestDXClient(DXTestCase):
             dx.setwinsize(20, 90)
             dx.expect("dnanexus@", timeout=1200)
 
+    @unittest.skipUnless(testutil.TEST_RUN_JOBS, "Skipping test that would run jobs")
+    def test_dx_run_debug_on_all(self):
+        with self.configure_ssh() as wd:
+            crash_applet = dxpy.api.applet_new(dict(name="crash",
+                                                    runSpec={"code": "exit 5", "interpreter": "bash",
+                                                             "execDepends": [{"name": "dx-toolkit"}]},
+                                                    inputSpec=[], outputSpec=[],
+                                                    dxapi="1.0.0", version="1.0.0",
+                                                    project=self.project))["id"]
+
+            job_id = run("dx run {} --yes --brief --debug-on All".format(crash_applet),
+                         env=override_environment(HOME=wd)).strip()
+            elapsed = 0
+            while True:
+                job_desc = dxpy.describe(job_id)
+                if job_desc["state"] == "debug_hold":
+                    break
+                time.sleep(1)
+                elapsed += 1
+                if elapsed > 1200:
+                    raise Exception("Timeout while waiting for job to enter debug hold")
+
+            dx = pexpect.spawn("dx ssh " + job_id, env=override_environment(HOME=wd))
+            dx.logfile = sys.stdout
+            dx.setwinsize(20, 90)
+            dx.expect("dnanexus@", timeout=1200)
+
     @unittest.skipUnless(testutil.TEST_DX_LOGIN,
                          'This test requires authserver to run, requires dx login to select the right authserver, ' +
                          'and may result in temporary account lockout. TODO: update test instrumentation to allow ' +
