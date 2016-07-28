ac1c9778c33984528ebbb4a8dcce8f8a59665103
Nicolas Bock <nicbock@gmail.com>
2016-07-28 16:26:00 -0700

    Added a nonce generator.

---
+++ src/java/src/main/java/com/dnanexus/Nonce.java
@@ -0,0 +1,68 @@
+// Copyright (C) 2013-2016 DNAnexus, Inc.
+//
+// This file is part of dx-toolkit (DNAnexus platform client libraries).
+//
+//   Licensed under the Apache License, Version 2.0 (the "License"); you may
+//   not use this file except in compliance with the License. You may obtain a
+//   copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//   License for the specific language governing permissions and limitations
+//   under the License.
+
+package com.dnanexus;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.security.SecureRandom;
+
+/*import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.fasterxml.jackson.databind.node.DoubleNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.LongNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.google.common.collect.Lists;
+*/
+
+/**
+ * Utility class for generating nonces.
+ */
+public class Nonce {
+
+	public static final SecureRandom random = new SecureRandom();
+    final protected static char[] hexArray = "0123456789abcdef".toCharArray();
+	public static int counter = 0;
+
+	// From http://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
+	public static String bytesToHex(byte[] bytes) {
+    	char[] hexChars = new char[bytes.length * 2];
+    
+	    for ( int j = 0; j < bytes.length; j++ ) {
+    	    int v = bytes[j] & 0xFF;
+        	hexChars[j * 2] = hexArray[v >>> 4];
+        	hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+    	}
+    	return new String(hexChars);
+	}
+    
+    public static String nonce() {
+        byte bytes[] = new byte[32];
+        random.nextBytes(bytes);
+        String nonce = bytesToHex(bytes);
+        nonce += String.valueOf(System.currentTimeMillis()) + String.valueOf(counter++);
+        return nonce;
+    }
+
+}
+
