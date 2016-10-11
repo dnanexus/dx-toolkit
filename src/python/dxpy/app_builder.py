c11fb6961309795cc7f4e25717c25cae3a88906e
pnguyen <pnguyen@dnanexus.com>
2016-10-11 12:47:14 -0700

    Add small fixes to `create_app` function.

--- src/python/dxpy/app_builder.py
+++ src/python/dxpy/app_builder.py
@@ -684,12 +684,19 @@ def _update_version(app_name, version, app_spec, try_update=True):
 def create_app(applet_id, applet_name, src_dir, publish=False, set_default=False, billTo=None, try_versions=None,
                try_update=True, confirm=True, regional_options=None):
     """
+    :param regional_options: The regional configurations that will be used to
+    create this app. The caller is responsible for ensuring that the dict, if
+    specified, is well-formed.
+    :type regional_options: dict.
+
     Creates a new app object from the specified applet.
     """
     app_spec = _get_app_spec(src_dir)
     logger.info("Will create app with spec: %s" % (app_spec,))
 
     if regional_options is None:
+        # TODO: Specify the applet in a multi-region-enabled manner. Will need
+        # to somehow fetch the region of the applet.
         app_spec["applet"] = applet_id
     else:
         app_spec["regionalOptions"] = regional_options
