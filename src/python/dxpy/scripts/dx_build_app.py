646745a946223a3324c20e522d5426f2e91c1913
pnguyen <pnguyen@dnanexus.com>
2016-11-12 17:23:28 -0800

    Generalize further.

--- src/python/dxpy/scripts/dx_build_app.py
+++ src/python/dxpy/scripts/dx_build_app.py
@@ -812,20 +812,20 @@ def build_and_upload_locally(src_dir, mode, overwrite=False, archive=False, publ
         projects_by_region = {}
         if enabled_regions is not None:
             # Create temporary projects in each enabled region.
-            for region in enabled_regions:
-                try:
+            try:
+                for region in enabled_regions:
                     working_project = dxpy.api.project_new({"name": "Temporary build project for dx-build-app",
                                                             "region": region})["id"]
                     projects_by_region[region] = working_project
                     logger.debug("Created temporary project %s to build in" % (working_project,))
-                except Exception as e:
-                    # A /project/new request may fail if the requesting user is
-                    # not authorized to create projects in a certain region.
-                    delete_temporary_projects(projects_by_region.values())
-                    if isinstance(e, DXAPIError):
-                        err_exit()
-                    else:
-                        raise e
+            except Exception as e:
+                # A /project/new request may fail if the requesting user is
+                # not authorized to create projects in a certain region.
+                delete_temporary_projects(projects_by_region.values())
+                if isinstance(e, DXAPIError):
+                    err_exit()
+                else:
+                    raise e
         else:
             # Create a temp project
             try:
