5e6d5a8b9085f4d5002d28ce22588a7d862e12b5
Geet Duggal <gduggal@dnanexus.com>
2017-01-19 22:08:41 +0000

    Changelog and version updates for v0.206.3

--- src/R/dxR/R/dxR-package.R
+++ src/R/dxR/R/dxR-package.R
@@ -4,7 +4,7 @@
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' 0.206.2\cr License: \tab Apache License (== 2.0)\cr
+##' 0.206.3\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
