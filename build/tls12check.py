5c1b1668136e94797548758b831c591fb1bd156d
Bill Agee <billagee@gmail.com>
2017-04-08 10:30:11 -0700

    More msg verbosity

--- build/tls12check.py
+++ build/tls12check.py
@@ -5,12 +5,33 @@
 import ssl
 import sys
 
-# See https://github.com/boto/botocore/blob/1.5.29/botocore/handlers.py#L636
+# From https://github.com/boto/botocore/blob/1.5.29/botocore/handlers.py#L636
 openssl_version_tuple = ssl.OPENSSL_VERSION_INFO
+
+upgrade_required_msg = """
+PYTHON UPGRADE REQUIRED
+
+Your Python implementation does not support TLS 1.2.
+
+You must upgrade Python before using the DNAnexus CLI.
+
+Next steps:
+    * Install Homebrew using the guide at:  https://brew.sh/
+    * Then run the command:  brew install python
+
+Your OpenSSL version: '{0}'
+""".format(ssl.OPENSSL_VERSION)
+
+upgrade_not_required_msg = """
+NO UPGRADE REQUIRED
+
+Your Python supports TLS 1.2, and is ready to use with the DNAnexus CLI.
+
+Your OpenSSL version: '{0}'
+""".format(ssl.OPENSSL_VERSION)
+
 if openssl_version_tuple < (1, 0, 1, 0, 0):
-    print("TLS 1.2 is not supported by the OpenSSL version ({0}) used by your Python implementation.".format(
-        ssl.OPENSSL_VERSION))
+    print(upgrade_required_msg)
     sys.exit(1)
 
-print("TLS 1.2 is supported by the OpenSSL version ({0}) used by your Python implementation.".format(
-    ssl.OPENSSL_VERSION))
+print(upgrade_not_required_msg)
