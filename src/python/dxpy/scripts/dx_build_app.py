2d79ff6268eb993477071b6a0b83b5914283deda
commandlinegirl <commandlinegirl@users.noreply.github.com>
2017-10-19 16:01:08 -0700

    DEVEX-362: Warn when old pricing policy scheme is used in dxapp.json (#256)

--- src/python/dxpy/scripts/dx_build_app.py
+++ src/python/dxpy/scripts/dx_build_app.py
@@ -356,6 +356,11 @@ def _verify_app_source_dir_impl(src_dir, temp_dir, mode, enforce=True):
             if thing != 'PUBLIC' and (not isinstance(thing, basestring) or not re.match("^(org-|user-)", thing)):
                 raise dxpy.app_builder.AppBuilderException('authorizedUsers field contains an entry which is not either the string "PUBLIC" or a user or org ID')
 
+    if "pricingPolicy" in manifest:
+        warn_message = "\"pricingPolicy\" at the top level is not accepted. It must be specified "
+        warn_message += "under the \"regionalOptions\" field in all enabled regions of the app"
+        logger.warn(warn_message)
+
     # Check all other files that are going to be in the resources tree.
     # For these we detect the language based on the filename extension.
     # Obviously this check can have false positives, since the app can
