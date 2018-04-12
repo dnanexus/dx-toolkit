b1208b7cb2cee0239e6af626cf138144030675de
Geet Duggal <geetduggal@gmail.com>
2018-04-12 11:34:18 -0700

    Update dxfile_functions.py

--- src/python/dxpy/bindings/dxfile_functions.py
+++ src/python/dxpy/bindings/dxfile_functions.py
@@ -182,7 +182,7 @@ def _download_symbolic_link(dxid, md5digest, project, dest_filename):
         print("Downloading symbolic link with wget")
         subprocess.check_call(cmd)
     except subprocess.CalledProcessError as e:
-        err_exit("Failed to call wget: {cmd}\n{msg}\n".format(cmd=str(cmd), msg=e.output.strip())
+        err_exit("Failed to call wget: {cmd}\n{msg}\n".format(cmd=str(cmd), msg=e.output.strip()))
 
     if md5digest is not None:
         _verify(dest_filename, md5digest)
