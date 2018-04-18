17cdfcdcea83c4de3c23b6a97487b4513ceb55e9
Aleksandra Zalcman <commandlinegirl@users.noreply.github.com>
2018-04-17 22:45:39 -0700

    Update test_dxclient.py

--- src/python/test/test_dxclient.py
+++ src/python/test/test_dxclient.py
@@ -3935,7 +3935,7 @@ class TestDXClientGlobalWorkflow(DXTestCaseBuildWorkflows):
         self.assertTrue(analysis_id.startswith('analysis-'))
         analysis_desc = run("dx describe " + analysis_id)
         self.assertIn('stage_0.number = 32', analysis_desc)
-        self.assertIn('Global workflow', analysis_desc)
+        self.assertIn('globalworkflow-', analysis_desc)
         self.assertIn(gwf_name, analysis_desc)
 
         analysis_desc = json.loads(run("dx describe " + analysis_id + " --json"))
