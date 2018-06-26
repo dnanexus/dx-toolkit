916584d3fe2f7f704acadd4a3518961dfed03c8b
Bill Agee <billagee@gmail.com>
2018-06-25 23:22:23 -0700

    Add comment

--- src/python/test/test_dxclient.py
+++ src/python/test/test_dxclient.py
@@ -10156,6 +10156,8 @@ class TestDXRunBatch(DXTestCase):
 
 
 class TestDXCompile(DXTestCase):
+    # TODO - find out why this test fails in Jenkins, at
+    # https://jenkins-vstg.internal.dnanexus.com/view/Traceability/
     #@pytest.mark.TRACEABILITY_MATRIX
     #@testutil.update_traceability_matrix(["DNA_CLI_COMPILE_PORTABLE_WORKFLOW"])
     def test_compile(self):
