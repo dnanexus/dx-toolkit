2996e1f6754b92999b387d3275cf6a09238f1e84
yhwang <yhwang@dnanexus.com>
2019-01-22 17:15:15 -0800

    Update testing to test for  file:array variable

--- src/python/test/file_load/basic_except/run.sh
+++ src/python/test/file_load/basic_except/run.sh
@@ -1,5 +1,5 @@
 main() {
-    dx-download-all-inputs --except seq1 --except seq2
+    dx-download-all-inputs --except seq1 --except seq2 --except ref
 
     # verify that seq1 and seq2 have not been downloaded
     if [ -e "in/seq1" ]
@@ -13,6 +13,13 @@ main() {
         exit 1
     fi
 
+    # verify that ref has not been downloaded
+    if [ -e "in/ref" ]
+    then
+        echo "Error: file:array ref has been downloaded, in spite of the --except flag"
+        exit 1
+    fi
+
     # Check file content
     dx download "$seq1" -o seq1
     dx download "$seq2" -o seq2
@@ -20,8 +27,13 @@ main() {
     mkdir -p out/result
     echo "hello world, $seq1, $seq2" > out/result/report.txt
 
+    # Check ref content
+    for i in ${!ref[@]}; do
+      dx download "${ref[$i]}" -o "${ref_path[$i]}"
+    done
     mkdir -p out/genes
     echo 'ls in/$ref' > out/genes/refs_ls.txt
+
     echo 'ls in/$reads' > out/genes/reads_ls.txt
 
     dx-upload-all-outputs --except genes
