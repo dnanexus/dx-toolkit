84b29f69562d55656b8de7dd56068bb365906695
Kurt Jensen <kjensen@dnanexus.com>
2019-02-25 10:42:13 -0800

    Add --saml-idp test

--- src/python/test/test_dxclient.py
+++ src/python/test/test_dxclient.py
@@ -5750,6 +5750,15 @@ class TestDXClientOrg(DXTestCase):
         self.assertEqual(new_org_name, cur_org_name)
         self.assertEqual(new_org_policies, exp_org_policies)
 
+        # --saml-idp
+        proposed_idp = "samlprovider"
+        exp_org_policies = dict(cur_org_policies, restrictProjectTransfer=proposed_idp)
+        res = run('dx update org {o} --saml-idp {p} --brief'.format(o=self.org_id,
+                                                                    p=proposed_idp)).strip()
+        self.assertEqual(res, self.org_id)
+        new_idp = dxpy.api.org_describe(org_id)["samlIdP"]
+        self.assertEqual(new_idp, proposed_idp)
+
         # All args.
         cur_org_name, cur_org_policies = new_org_name, new_org_policies
         proposed_org_name = "bar"
