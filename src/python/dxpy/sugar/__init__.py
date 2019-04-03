61891dddc9fe14b421e3a94d4d1e1b8c8861e390
Maria Simbirsky <msimbirsky@dnanexus.com>
2019-04-03 14:34:46 -0400

    Add requires_worker_context decorator for functions that run inside jobs

---
+++ src/python/dxpy/sugar/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2013-2019 DNAnexus, Inc.
+#
+# This file is part of dx-toolkit (DNAnexus platform client libraries).
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#   use this file except in compliance with the License. You may obtain a copy
+#   of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+import dxpy
+from functools import wraps
+
+def requires_worker_context(func):
+    """This decorator checks that a given function is running within a DNAnexus job context"""
+    @wraps(func)
+    def check_job_id(*args, **kwargs):
+        if dxpy.JOB_ID is None:
+            raise dxpy.DXError("Illegal function call, must be called from within DNAnexus job context.")
+        else:
+            return func(*args, **kwargs)
+
+    return check_job_id
