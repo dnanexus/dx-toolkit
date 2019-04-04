9e0267ba8ac5a3ebe9b1ccdb49bd991a6be6f371
Ohad Rodeh <orodeh@dnanexus.com>
2019-04-04 09:52:20 -0700

    Splitting out batch tests from the main dxclient test file

--- src/python/test/test_dxclient.py
+++ src/python/test/test_dxclient.py
@@ -19,7 +19,7 @@
 
 from __future__ import print_function, unicode_literals, division, absolute_import
 
-import os, sys, unittest, json, tempfile, subprocess, csv, shutil, re, base64, random, time
+import os, sys, unittest, json, tempfile, subprocess, shutil, re, base64, random, time
 import filecmp
 import pipes
 import stat
@@ -10299,113 +10299,6 @@ class TestDXUpdateApp(DXTestCaseBuildApps):
         self.assertEqual(app_2['name'], app_spec_2['name'])
         self.assertEqual(app_2['version'], "0.0.2")
 
-
-@unittest.skipUnless(testutil.TEST_RUN_JOBS,
-                     'skipping test that would run jobs')
-class TestDXRunBatch(DXTestCase):
-    def test_basic(self):
-        # write python code into code.py file
-        tmp_path = tempfile.mkdtemp()
-        code_path = os.path.join(tmp_path, 'code.py')
-        with open(code_path, 'wb') as f:
-            f.write("@dxpy.entry_point('main')\n")
-            f.write("def main(**job_inputs):\n")
-            f.write("\toutput = {}\n")
-            f.write("\toutput['thresholds'] = job_inputs['thresholds']\n")
-            f.write("\toutput['pie'] = job_inputs['pie'] + 1\n")
-            f.write("\toutput['misc'] = {'n': 'non', 'y': 'oui'}\n")
-            f.write("\treturn output\n")
-            f.write("\n")
-            f.write("dxpy.run()\n")
-        code = open(code_path, 'r').read()
-
-        # write arguments table
-        arg_table = os.path.join(tmp_path, 'table.csv')
-        with open(arg_table, 'wb') as csvfile:
-            writer = csv.writer(csvfile, delimiter=(u"\t").encode('utf-8'))
-            header = ["batch ID", "thresholds", "pie", "misc"]
-            writer.writerow(header)
-            writer.writerow(["SRR_1", "[10,81]", "3.12", "{}"])
-
-        applet = dxpy.api.applet_new({
-            "name": "copy_all",
-            "project": self.project,
-            "dxapi": "1.0.0",
-            "inputSpec": [ { "name": "thresholds", "class": "array:int"},
-                           { "name": "pie", "class": "float" },
-                           { "name": "misc", "class": "hash" } ],
-            "outputSpec": [ { "name": "thresholds", "class": "array:int" },
-                            { "name": "pie", "class": "float" },
-                            { "name": "misc", "class": "hash" } ],
-            "runSpec": { "interpreter": "python2.7",
-                         "code": code,
-                         "distribution": "Ubuntu",
-                         "release": "14.04" }
-        })
-
-        # run in batch mode
-        job_id = run("dx run {} --batch-tsv={} --yes --brief"
-                     .format(applet["id"], arg_table)).strip()
-        job_desc = dxpy.api.job_describe(job_id)
-        self.assertEqual(job_desc["executableName"], 'copy_all')
-        self.assertEqual(job_desc["input"], { "thresholds": [10,81],
-                                               "misc": {},
-                                               "pie": 3.12 })
-
-        # run in batch mode with --batch-folders
-        job_id = run("dx run {} --batch-tsv={} --batch-folders --yes --brief"
-                     .format(applet["id"], arg_table)).strip()
-        job_desc = dxpy.api.job_describe(job_id)
-        self.assertEqual(job_desc["folder"], "/SRR_1")
-
-        # run in batch mode with --batch-folders and --destination
-        job_id = run("dx run {} --batch-tsv={} --batch-folders --destination={}:/run_01 --yes --brief"
-                     .format(applet["id"], arg_table, self.project)).strip()
-        job_desc = dxpy.api.job_describe(job_id)
-        self.assertEqual(job_desc["folder"], "/run_01/SRR_1")
-
-    def test_files(self):
-        # Create file with junk content
-        dxfile = dxpy.upload_string("xxyyzz", project=self.project,
-                                    wait_on_close=True, name="bubbles")
-
-        # write python code into code.py file
-        tmp_path = tempfile.mkdtemp()
-        code_path = os.path.join(tmp_path, 'code.py')
-        with open(code_path, 'wb') as f:
-            f.write("@dxpy.entry_point('main')\n")
-            f.write("def main(**job_inputs):\n")
-            f.write("\toutput = {}\n")
-            f.write("\toutput['plant'] = job_inputs['plant']\n")
-            f.write("\treturn output\n")
-            f.write("\n")
-            f.write("dxpy.run()\n")
-        code = open(code_path, 'r').read()
-
-        # write arguments table
-        arg_table = os.path.join(tmp_path, 'table.csv')
-        with open(arg_table, 'wb') as csvfile:
-            writer = csv.writer(csvfile, delimiter=(u"\t").encode('utf-8'))
-            header = ["batch ID", "plant", "plant ID"]
-            writer.writerow(header)
-            writer.writerow(["SRR_1", "bubbles", dxfile.get_id()])
-
-        applet = dxpy.api.applet_new({
-            "name": "copy_file",
-            "project": self.project,
-            "dxapi": "1.0.0",
-            "inputSpec": [ { "name": "plant", "class": "file" } ],
-            "outputSpec": [ { "name": "plant", "class": "file" } ],
-            "runSpec": { "interpreter": "python2.7",
-                         "code": code,
-                         "distribution": "Ubuntu",
-                         "release": "14.04" }
-        })
-        job_id = run("dx run {} --batch-tsv={} --yes --brief"
-                     .format(applet["id"], arg_table)).strip()
-        job_desc = dxpy.api.job_describe(job_id)
-        self.assertEqual(job_desc["executableName"], 'copy_file')
-        self.assertEqual(job_desc["input"], { "plant": {"$dnanexus_link": dxfile.get_id() }})
 
 if __name__ == '__main__':
     if 'DXTEST_FULL' not in os.environ:
