5ebe965447c4ee209fd16336d78ec5ac9f2dcfe1
Geet Duggal <geetduggal@gmail.com>
2019-05-28 16:23:43 -0700

    DEVEX 923 - Approved tools CLI (#509)

--- src/python/dxpy/cli/parsers.py
+++ src/python/dxpy/cli/parsers.py
@@ -345,6 +345,10 @@ def get_update_project_args(args):
         input_params["containsPHI"] = True if args.containsPHI == 'true' else False
     if args.bill_to is not None:
         input_params["billTo"] = args.bill_to
+    if args.allowed_executables is not None:
+        input_params['allowedExecutables'] = args.allowed_executables
+    if args.unset_allowed_executables:
+        input_params['allowedExecutables'] = None
     return input_params
 
 
