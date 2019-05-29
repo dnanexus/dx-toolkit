5f0c7909e7753d4af5cb2d87a56dfa7fc5a889fb
Kurt Jensen <kjensen@dnanexus.com>
2019-05-29 14:38:35 -0700

    Provide project ID for make_download_url

--- src/python/dxpy/scripts/dx.py
+++ src/python/dxpy/scripts/dx.py
@@ -1713,6 +1713,8 @@ def make_download_url(args):
     # TODO: how to do data egress billing for make_download_url?
     try:
         dxfile = dxpy.DXFile(entity_result['id'], project=project)
+        # Only provide project ID, not job workspace container ID
+        project = project if re.match(r"^project-[a-zA-Z0-9]{24}$", project) else dxpy.DXFile.NO_PROJECT_HINT
         url, _headers = dxfile.get_download_url(preauthenticated=True,
                                                 duration=normalize_timedelta(args.duration)//1000 if args.duration else 24*3600,
                                                 filename=args.filename,
