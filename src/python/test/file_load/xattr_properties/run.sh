5996c7bcc77051deabafeafdcd89e748dfbc999a
Kurt Jensen <kjensen@dnanexus.com>
2019-06-15 13:54:48 -0700

    Test

---
+++ src/python/test/file_load/xattr_properties/run.sh
@@ -0,0 +1,78 @@
+main() {
+    # parallel download
+    dx-download-all-inputs --parallel
+
+    # creating some output results
+    echo "ABCD" > dummy_data.txt
+    mkdir -p out
+    dirs="result1 result2 result3 result4"
+    keys="key1 "
+
+    for d in $dirs
+    do
+        mkdir -p out/$d
+        cp dummy_data.txt out/$d/$d
+        attr -s "key0" -V "val0" out/$d/$d
+        attr -s "key1" -V "val1" out/$d/$d
+    done
+
+    # sequential upload with metadata as properties
+    dx-upload-all-outputs --sequential --wait-on-close --xattr-properties
+    compare_xattr_to_properties
+    remove_uploaded_files
+
+    # parallel upload with metadata as properties
+    dx-upload-all-outputs --clearJSON=true --parallel --wait-on-close --xattr-properties
+    compare_xattr_to_properties
+}
+
+function compare_xattr_to_properties()
+{
+    files=$(find "out" -type f)
+    for f in $files
+    do
+        echo "f=$f"
+        basename=${f##*/}
+        properties=$(dx describe --json $basename | jq -r .properties)
+        [[ "val0" == $(echo $properties | jq -r .key0) ]]
+        [[ "val1" == $(echo $properties | jq -r .key1) ]]
+    done
+
+# download files uploaded from the "out" directory, and compare
+# against the originals
+function compare_upload_to_outdir()
+{
+    files=$(find "out" -type f)
+
+    mkdir -p tmp
+    for f in $files
+    do
+        basename=${f##*/}
+
+        # ensure the file is closed before downloading it
+        dx wait "$basename"
+        dx download "$basename" -o tmp/"$basename"
+        DIFF=$(diff out/$basename/$basename tmp/$basename)
+        if [ "$DIFF" != "" ]
+        then
+            echo "Error in upload, file $basename is incorrect"
+            exit 1
+        fi
+    done
+
+    rm -rf tmp
+}
+
+
+function remove_uploaded_files()
+{
+    files=$(find "out" -type f)
+
+    for f in $files
+    do
+        echo "f=$f"
+        basename=${f##*/}
+
+        dx rm $basename
+    done
+}
