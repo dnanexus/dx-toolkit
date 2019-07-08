9cc20a939c9f732094e616955ed8c284a8b2aa03
John Didion <github@didion.net>
2019-07-08 14:29:04 -0400

    Sci 1328 available memory (#533)
    * Placeholder for dxpy.sugar package
    
    * Add requires_worker_context decorator for functions that run inside jobs
    
    * SCI-1323, SCI-1324: Add sugar transfer and processing modules (#511)
    
    * Move files to new branch; implement changes suggested by Steve
    
    * Remove unused code
    
    * Set self._returncode to the returncode of the last process
    
    * Set self._returncode to the returncode of the last process that failed with an error
    
    * Implement suggested changes
    
    * Remove bool options from chdir parameter
    
    * Cleanup
    
    * Add initial functions from dx_utils.py
    
    * Add general group files function
    
    * Rename built-in variables
    
    * Rename subjobs module to chunking
    
    * Update documentation to reflect chunks are in GB
    
    * Code clean-up
    
    * add tests
    
    * fix tests
    
    * Code clean-up
    
    * fix tests
    
    * Use MemAvailable instead of MemFree; clean up comment

---
+++ src/python/dxpy/sugar/__init__.py
@@ -0,0 +1,89 @@
+# Copyright (C) 2013-2019 DNAnexus, Inc.
+#
+# This file is part of dx-toolkit (DNAnexus platform client libraries).
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#   use this file except in compliance with the License. You may obtain a copy
+#   of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+from __future__ import print_function, unicode_literals, division, absolute_import
+from functools import wraps
+import re
+
+import dxpy
+
+from dxpy.sugar.processing import run_cmd, chain_cmds
+from dxpy.sugar.transfers import (
+    Uploader,
+    Downloader,
+    upload_file,
+    tar_and_upload_files,
+    download_file,
+)
+
+
+MEM_RE = re.compile(r"^MemAvailable:[\s]*([0-9]*) kB")
+MEM_CONVERSIONS = {
+    "K": 1,
+    "M": 1 << 10,
+    "G": 1 << 20
+}
+
+
+def requires_worker_context(func):
+    """This decorator checks that a given function is running within a DNAnexus
+    job context.
+    """
+    @wraps(func)
+    def check_job_id(*args, **kwargs):
+        if dxpy.JOB_ID is None:
+            raise dxpy.DXError(
+                "Illegal function call, must be called from within DNAnexus job "
+                "context."
+            )
+        else:
+            return func(*args, **kwargs)
+
+    return check_job_id
+
+
+@requires_worker_context
+def available_memory(suffix="M", meminfo_path="/proc/meminfo"):
+    """Queries a worker's /proc/meminfo for available memory and returns a float
+    of the specified suffix size.
+
+    Note that this function doesn't necessarily require to be run on a DNAnexus worker,
+    but depends on /proc/meminfo, which only exists on Linux systems.
+
+    Args:
+        suffix (str): One of 'M', 'K' or 'G' to return memory in Mib, KiB or
+            GiB, respectively.
+        meminfo_path: Path to /proc/meminfo, or a file that contains compatible
+            output.
+
+    Returns:
+        float: total_memory read from meminfo in KiB, MiB, or GiB
+            depending on specified suffix.
+
+    Raises:
+        dxpy.DXError is raised if suffix is not recognized or system memory
+        cannot be read.
+    """
+    suffix = suffix.upper()
+    if suffix not in MEM_CONVERSIONS:
+        raise ValueError(
+            "Unknown memory suffix {0}. Please choose from K, M, or G.".format(suffix)
+        )
+
+    total_mem = MEM_RE.findall(open(meminfo_path).read())
+    if len(total_mem) != 1:
+        raise dxpy.DXError("Problem reading system memory from {}".format(meminfo_path))
+
+    return float(total_mem[0]) / MEM_CONVERSIONS[suffix]
---
+++ src/python/dxpy/sugar/chunking.py
@@ -0,0 +1,131 @@
+# Copyright (C) 2013-2019 DNAnexus, Inc.
+#
+# This file is part of dx-toolkit (DNAnexus platform client libraries).
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#   use this file except in compliance with the License. You may obtain a copy
+#   of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+from __future__ import print_function, unicode_literals, division, absolute_import
+import math
+
+import dxpy
+
+
+BYTES_PER_GB = 1024 * 1024 * 1024
+
+
+def divide_dxfiles_into_chunks(dxfiles, target_size_gb):
+    """
+    This is a general function to divide input files into a set of chunks based on
+    file size.
+
+    Args:
+        dxfiles: List of dx files to split
+        target_size_gb: Target size (in gigabytes) of each bin
+
+    Returns:
+        Groups of files with each group having roughly target_size of data
+    """
+    if not dxfiles:
+        return []
+    if target_size_gb <= 0:
+        raise ValueError("Target size must be > 0")
+
+    filesizes = get_dxlink_filesizes(dxfiles)
+    total_size_bytes = sum(filesizes)
+
+    # Now, get the splits.  We'll target each set of bam files to be a total
+    # of `target_size` bytes.
+    num_bins = max(
+        1, int(math.ceil(total_size_bytes / (target_size_gb * BYTES_PER_GB)))
+    )
+
+    # It's conceivable that some of the splits could be empty.  We'll remove
+    # those from our list.
+    return list(filter(None, schedule_lpt(list(zip(dxfiles, filesizes)), num_bins)))
+
+
+def get_dxlink_filesizes(dxfiles):
+    """Run dx describe on a list of DNAnexus dxlink inputs to get the
+    corresponding file sizes.
+
+    Args:
+        dxfiles (list): DXFile objects, dxlinks, or file IDs
+
+    Returns:
+        list: corresponding filesizes in bytes, output of 'dx describe'
+        command
+    """
+    ids = [as_dxfile_id(f) for f in dxfiles]
+    descriptions = dxpy.api.system_describe_data_objects(
+        {"objects": ids}, always_retry=True
+    )
+    sizes = dict(
+        (d["describe"]["id"], d["describe"]["size"])
+        for d in descriptions["results"]
+    )
+    return [sizes[i] for i in ids]
+
+
+def as_dxfile_id(dxfile):
+    if isinstance(dxfile, dxpy.DXFile):
+        return dxfile.get_id()
+    elif dxpy.is_dxlink(dxfile):
+        return dxpy.get_dxlink_ids(dxfile)
+    else:
+        return dxfile
+
+
+import pysnooper
+@pysnooper.snoop()
+def schedule_lpt(jobs, num_bins):
+    """This function implements the Longest Processing Time algorithm to get
+    a good division of labor for the multiprocessor scheduling problem.
+
+    Args:
+        jobs (dict or list): A dictionary with string 'key' specifying job
+            name and float 'value' specifying job weight (how long job should
+            run compared to other jobs). A list of tuples may also be passed
+            if it is equivalent to dict.items()
+        num_bins (int): Number of groups to split jobs into.
+
+    Returns:
+        List of lists: Each group (list) in list is a group of jobs that should
+            be run together on a single worker or instance and consists of
+            tuples as provided in jobs input.
+
+    Examples:
+        # it's assumed that there's an app-specific way to generate a filelist
+        # of filenames and sizes
+        fl = filenames_and_sizes(files)
+        fl_groups = schedule_lpt(fl, num_jobs)
+        for group in fl_groups:
+            print group
+            job = dxpy.new_dxjob({'files': group}, 'subjob_name')
+            output['output_files'].append(job.get_output_ref('output_files'))
+    """
+    # We expect a list of tuples, with the first value the name of the
+    # job and the second value the weight.  If we are given a dict
+    # then convert keys to job names and values to weights.
+    num_bins = min(num_bins, len(jobs))
+    indexes = range(num_bins)
+    groups = [[] for _ in indexes]
+    sizes = [0] * num_bins
+
+    if isinstance(jobs, dict):
+        jobs = jobs.items()
+
+    for job in sorted(jobs, key=lambda j: j[1], reverse=True):
+        idx = min(indexes, key=sizes.__getitem__)
+        groups[idx].append(job[0])
+        sizes[idx] += job[1]
+
+    return groups
