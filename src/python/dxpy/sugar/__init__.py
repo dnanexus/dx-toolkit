c44a6a73c6fef13bfed16269508e9781f1c86e4c
jdidion <github@didion.net>
2019-07-17 17:21:27 -0400

    Readd get_available_memory() and associated tests

--- src/python/dxpy/sugar/__init__.py
+++ src/python/dxpy/sugar/__init__.py
@@ -15,6 +15,15 @@
 #   under the License.
 import dxpy
 from functools import wraps
+import re
+
+
+MEM_RE = re.compile(r"^MemAvailable:[\s]*([0-9]*) kB")
+MEM_CONVERSIONS = {
+    "K": 1,
+    "M": 1 << 10,
+    "G": 1 << 20
+}
 
 
 def in_worker_context():
@@ -36,3 +45,38 @@ def requires_worker_context(func):
             )
 
     return check_job_id
+
+
+@requires_worker_context
+def available_memory(suffix="M", meminfo_path="/proc/meminfo"):
+    """Queries a worker's /proc/meminfo for available memory and returns a float
+    of the specified suffix size.
+
+    Note that this function doesn't necessarily require to be run on a DNAnexus worker,
+    but depends on /proc/meminfo, which only exists on Linux systems.
+
+    Args:
+        suffix (str): One of 'M', 'K' or 'G' to return memory in Mib, KiB or
+            GiB, respectively.
+        meminfo_path: Path to /proc/meminfo, or a file that contains compatible
+            output.
+
+    Returns:
+        float: total_memory read from meminfo in KiB, MiB, or GiB
+            depending on specified suffix.
+
+    Raises:
+        dxpy.DXError is raised if suffix is not recognized or system memory
+        cannot be read.
+    """
+    suffix = suffix.upper()
+    if suffix not in MEM_CONVERSIONS:
+        raise ValueError(
+            "Unknown memory suffix {0}. Please choose from K, M, or G.".format(suffix)
+        )
+
+    total_mem = MEM_RE.findall(open(meminfo_path).read())
+    if len(total_mem) != 1:
+        raise dxpy.DXError("Problem reading system memory from {}".format(meminfo_path))
+
+    return float(total_mem[0]) / MEM_CONVERSIONS[suffix]
