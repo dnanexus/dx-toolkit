97da89e534f560092dafc1118476e90725645433
Kurt Jensen <kjensen@dnanexus.com>
2020-01-16 13:15:14 -0800

    Trim log line bytes in python 3

--- src/python/dxpy/dxlog.py
+++ src/python/dxpy/dxlog.py
@@ -21,6 +21,7 @@ import socket, json, time, os, logging
 from logging.handlers import SysLogHandler
 
 from dxpy.exceptions import DXError
+from dxpy.compat import USING_PYTHON2
 
 class DXLogHandler(SysLogHandler):
     '''
@@ -68,6 +69,19 @@ class DXLogHandler(SysLogHandler):
         # See logging.handlers.SysLogHandler for an explanation of this.
         return self.priority_names[self.priority_map.get(record.levelname, "warning")]
 
+def truncate_message(self, message):
+    if USING_PYTHON2:
+        if len(message) > 8015:
+            message = message[:8000] + "... [truncated]"
+    else:
+        # Trim bytes
+        encoded = message.encode('utf-8')
+        if len(encoded) > 8015:
+            # Ignore UnicodeDecodeError chars that could have been messed up by truncating
+            message = encoded[:8015].decode('utf-8', 'ignore') + "... [truncated]"
+    return message
+
+
     def emit(self, record):
         level = self.encodePriority(record)
         message = record.getMessage()
@@ -78,11 +92,10 @@ class DXLogHandler(SysLogHandler):
         # Note: we use Python 2 semantics here (byte strings). This
         # script is not Python 3 ready. If *line* was a unicode string
         # with wide chars, its byte length would exceed the limit.
-        if len(message) > 8015:
-            message = message[:8000] + "... [truncated]"
+        message = truncate_message
 
         data = json.dumps({"source": self.source, "timestamp": int(round(time.time() * 1000)),
-                           "level": level, "msg": message})
+                           "level": level, "msg": message}).encode('utf-8')
 
         levelno = int(record.levelno)
         if levelno >= logging.CRITICAL or (levelno == logging.INFO and message.startswith(b"CPU: ")):
