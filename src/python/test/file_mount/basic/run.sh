1246e70306fe024c6dc52f474ab28061be968f85
wkoetke <wkoetke@dnanexus.com>
2020-04-03 10:29:27 -0700

    test only

--- src/python/test/file_mount/basic/run.sh
+++ src/python/test/file_mount/basic/run.sh
@@ -3,6 +3,9 @@ main() {
     ls .
     ls /usr/bin
 
+    curl -O https://github.com/dnanexus/dxfuse/releases/download/v0.21/dxfuse-linux
+    mv dxfuse-linux dxfuse
+    
     dx-mount-all-inputs
 
     find /home/dnanexus/in -name \*
