c838bb5ddc2207c39815e2d64d7f6bb879c11fea
Kurt Jensen <kjensen@dnanexus.com>
2020-09-15 16:08:24 -0700

    16.04 v1 remote builders. 20.04 asset builder (#642)
    * Add 20.04 asset builder
    
    * Remote builders for 16.04 version '1'

--- src/python/dxpy/asset_builder.py
+++ src/python/dxpy/asset_builder.py
@@ -34,6 +34,8 @@ ASSET_BUILDER_PRECISE = "app-create_asset_precise"
 ASSET_BUILDER_TRUSTY = "app-create_asset_trusty"
 ASSET_BUILDER_XENIAL = "app-create_asset_xenial"
 ASSET_BUILDER_XENIAL_V1 = "app-create_asset_xenial_v1"
+ASSET_BUILDER_FOCAL = "app-create_asset_focal"
+
 
 
 class AssetBuilderException(Exception):
@@ -234,6 +236,8 @@ def build_asset(args):
             app_run_result = dxpy.api.app_run(ASSET_BUILDER_XENIAL_V1, input_params=builder_run_options)
         elif asset_conf['release'] == "16.04":
             app_run_result = dxpy.api.app_run(ASSET_BUILDER_XENIAL, input_params=builder_run_options)
+        elif asset_conf['release'] == "20.04":
+            app_run_result = dxpy.api.app_run(ASSET_BUILDER_FOCAL, input_params=builder_run_options)
 
         job_id = app_run_result["id"]
 
