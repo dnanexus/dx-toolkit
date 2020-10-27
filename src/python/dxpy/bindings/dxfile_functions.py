5a8f4cc286cda507bb759a15b21f1ce611af3b33
Kurt Jensen <kjensen@dnanexus.com>
2020-10-27 13:12:48 -0700

    Increase wget retries to 20

--- src/python/dxpy/bindings/dxfile_functions.py
+++ src/python/dxpy/bindings/dxfile_functions.py
@@ -190,7 +190,7 @@ def _download_symbolic_link(dxid, md5digest, project, dest_filename):
         if wget_exe is None:
             err_exit("wget is not installed on this system")
 
-        cmd = ["wget", "--tries=5", "--quiet"]
+        cmd = ["wget", "--tries=20", "--quiet"]
         if os.path.isfile(dxid):
             # file already exists, resume upload.
             cmd += ["--continue"]
