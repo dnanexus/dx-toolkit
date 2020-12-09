025eef25011829cf1919de02cddd058d5fbdbc95
Marek Hrvol <mhrvol@seznam.cz>
2020-12-09 13:55:54 +0100

    minor

--- src/python/dxpy/scripts/dx.py
+++ src/python/dxpy/scripts/dx.py
@@ -3382,6 +3382,7 @@ def run(args):
     print(os.environ.get("DX_RUN_DETACH"))
     if not args.detach:
         print("Imhere")
+        print(os.environ.get("DX_RUN_DETACH") == 1)
         args.detach = os.environ.get("DX_RUN_DETACH") == 1
     print(args.detach)
     print("HEEEEERE")
