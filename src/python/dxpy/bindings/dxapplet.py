55d63b4815fd93202c34889d81a69a4cb2cdbb63
Marek Hrvol <mhrvol@seznam.cz>
2020-12-09 15:55:27 +0100

    minor

--- src/python/dxpy/bindings/dxapplet.py
+++ src/python/dxpy/bindings/dxapplet.py
@@ -158,7 +158,7 @@ class DXExecutable:
     def run(self, executable_input, project=None, folder=None, name=None, tags=None, properties=None, details=None,
             instance_type=None, stage_instance_types=None, stage_folders=None, rerun_stages=None, cluster_spec=None,
             depends_on=None, allow_ssh=None, debug=None, delay_workspace_destruction=None, priority=None,
-            ignore_reuse=None, ignore_reuse_stages=None, extra_args=None, **kwargs):
+            ignore_reuse=None, ignore_reuse_stages=None, extra_args=None, detach=None, **kwargs):
         '''
         :param executable_input: Hash of the executable's input arguments
         :type executable_input: dict
@@ -192,6 +192,8 @@ class DXExecutable:
         :type ignore_reuse_stages: list
         :param extra_args: If provided, a hash of options that will be merged into the underlying JSON given for the API call
         :type extra_args: dict
+        :param detach: If provided, job will not start as subjob if run inside of a different job.
+        :type detach: boolean
         :returns: Object handler of the newly created job
         :rtype: :class:`~dxpy.bindings.dxjob.DXJob`
 
@@ -222,7 +224,8 @@ class DXExecutable:
                                         debug=debug,
                                         delay_workspace_destruction=delay_workspace_destruction,
                                         priority=priority,
-                                        extra_args=extra_args)
+                                        extra_args=extra_args,
+                                        detach=detach)
 
         return self._run_impl(run_input, **kwargs)
 
