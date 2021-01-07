96a5ea7bc9cf0ed6f154203170401cf6189571e1
Marek Hrvol <mhrvol@seznam.cz>
2021-01-07 17:14:58 +0100

    minor

--- src/python/dxpy/workflow_builder.py
+++ src/python/dxpy/workflow_builder.py
@@ -378,7 +378,7 @@ def _build_regular_workflow(json_spec, keep_open=False):
     Precondition: json_spec must be validated
     """
     workflow_id = dxpy.api.workflow_new(json_spec)["id"]
-    if keep_open:
+    if not keep_open:
         print("I aint closing this.")
         dxpy.api.workflow_close(workflow_id)
     return workflow_id
