0e24749efac1bb675a604840054d668554af66c3
Marek Hrvol <mhrvol-cf@dnanexus.com>
2021-07-15 16:47:37 +0200

    minor

--- src/python/dxpy/bindings/dxfile_functions.py
+++ src/python/dxpy/bindings/dxfile_functions.py
@@ -380,8 +380,10 @@ def _download_dxfile(dxid, filename, part_retry_counter,
             get_first_chunk_sequentially = (file_size > 128 * 1024 and last_verified_pos == 0 and dxpy.JOB_ID)
             cur_part, got_bytes, hasher = None, None, None
             from random import random
+            from requests import exceptions
+
             if random() > 0.5:
-                raise Exception("emptyyyy")
+                raise exceptions.HTTPError("ermpty")
             for chunk_part, chunk_data in response_iterator(chunk_requests(),
                                                             dxfile._http_threadpool,
                                                             do_first_task_sequentially=get_first_chunk_sequentially):
