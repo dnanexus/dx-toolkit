bfcaad6c1a7776fd05508897bd0c39f1ce07cef1
Kurt Jensen <kjensen@dnanexus.com>
2022-01-31 13:48:54 -0800

    DEVEX-2046 Fix KeyError for required inputs in dxapp.json for `dx-app-wizard --json` (#762)

--- src/python/dxpy/scripts/dx_app_wizard.py
+++ src/python/dxpy/scripts/dx_app_wizard.py
@@ -272,7 +272,7 @@ array:boolean  array:int      boolean        hash           string''')
 
     if 'inputSpec' in app_json:
         for param in app_json['inputSpec']:
-            may_be_missing = param['optional'] and "default" not in param
+            may_be_missing = param.get('optional') and "default" not in param
             if param['class'] == 'file':
                 param_list = optional_file_input_names if may_be_missing else required_file_input_names
             elif param['class'] == 'array:file':
