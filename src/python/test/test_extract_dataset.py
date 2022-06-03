5b02b9a334fb2689018347843ea9554030eafd07
npandeshwar-f <npandeshwar-f@npandeshwar-10513-rm-ltmp-x.local>
2022-06-03 00:15:16 -0700

    SCIPROD-540 CAWFR: Updated tests with new dataset and cohort

--- src/python/test/test_extract_dataset.py
+++ src/python/test/test_extract_dataset.py
@@ -28,74 +28,82 @@ import pandas as pd
 
 class TestDXExtractDataset(unittest.TestCase):
     def test_e2e_dataset_ddd(self):
-        dataset_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-G9k12VQ06G1P42KK7fFK3yKB"
+        dataset_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-GBGpKZ80G8j8k4G84P7GP0B5"
         out_directory = tempfile.mkdtemp()
         cmd = ["dx", "extract_dataset", dataset_record, "-ddd", "-o", out_directory]
         subprocess.check_call(cmd)
-        self.end_to_end_ddd(out_directory=out_directory, rec_name = "test_dml_out01")
+        self.end_to_end_ddd(out_directory=out_directory, rec_name = "extract_dataset_test")
 
     def test_e2e_cohortbrowser_ddd(self):
-        cohort_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-G9k3pGj0vGPvKg77BP1Yxq8q"
+        cohort_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-GBGq1pQ0vGPf0qYg30Jg7kkG"
         out_directory = tempfile.mkdtemp()
         cmd = ["dx", "extract_dataset", cohort_record, "-ddd", "-o", out_directory]
         subprocess.check_call(cmd)
-        self.end_to_end_ddd(out_directory=out_directory, rec_name = "test_cohort")
+        self.end_to_end_ddd(out_directory=out_directory, rec_name = "Combined_Cohort_Test")
 
     def test_e2e_dataset_sql(self):
-        dataset_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-G9k12VQ06G1P42KK7fFK3yKB"
-        truth_output = "SELECT `patient_1`.`patient_id` AS `patient.patient_id`, `patient_1`.`name` AS `patient.name`, `patient_1`.`weight` AS `patient.weight`, `patient_1`.`height` AS `patient.height`, `patient_1`.`size` AS `patient.size` FROM `database_g9k1260089qpxpf468f9zybj__test_dml_out01`.`patient` AS `patient_1` WHERE `patient_1`.`patient_id` IN (SELECT DISTINCT `patient_1`.`patient_id` AS `patient_id` FROM `database_g9k1260089qpxpf468f9zybj__test_dml_out01`.`patient` AS `patient_1`);"
+        dataset_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-GBGpKZ80G8j8k4G84P7GP0B5"
+        truth_output = "SELECT `patient_0001_1`.`patient_id` AS `patient.patient_id`, `patient_0001_1`.`name` AS `patient.name`, `patient_0002_1`.`weight` AS `patient.weight`, `patient_0001_1`.`date_of_birth` AS `patient.date_of_birth`, `patient_0002_1`.`verified_dtm` AS `patient.verified_dtm`, `test_1`.`test_id` AS `test.test_id`, `trial_visit_0001_1`.`visit_id` AS `trial_visit.visit_id`, `baseline_0001_1`.`baseline_id` AS `baseline.baseline_id`, `hospital_0001_1`.`hospital_id` AS `hospital.hospital_id`, `doctor_0001_1`.`doctor_id` AS `doctor.doctor_id` FROM `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`patient_0001` AS `patient_0001_1` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`patient_0002` AS `patient_0002_1` ON `patient_0001_1`.`patient_id` = `patient_0002_1`.`patient_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`trial_visit_0001` AS `trial_visit_0001_1` ON `patient_0001_1`.`patient_id` = `trial_visit_0001_1`.`visit_patient_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`test` AS `test_1` ON `trial_visit_0001_1`.`visit_id` = `test_1`.`test_visit_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`baseline_0001` AS `baseline_0001_1` ON `patient_0001_1`.`patient_id` = `baseline_0001_1`.`b_patient_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`hospital_0001` AS `hospital_0001_1` ON `patient_0001_1`.`hid` = `hospital_0001_1`.`hospital_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`doctor_0001` AS `doctor_0001_1` ON `trial_visit_0001_1`.`visit_did` = `doctor_0001_1`.`doctor_id` WHERE `patient_0001_1`.`patient_id` IN (SELECT DISTINCT `patient_0001_1`.`patient_id` AS `patient_id` FROM `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`patient_0001` AS `patient_0001_1`);"
         cmd = ["dx", "extract_dataset", dataset_record, "--fields", "patient.patient_id" , ",", "patient.name", ",", "patient.weight", ",",
-            "patient.height", ",", "patient.size","--sql", "-o", "-"]
+               "patient.date_of_birth", ",", "patient.verified_dtm", ",", "test.test_id", ",", "trial_visit.visit_id", ",", "baseline.baseline_id", 
+               ",", "hospital.hospital_id", ",", "doctor.doctor_id","--sql", "-o", "-"]
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        print(truth_output)
         stdout = process.communicate()[0]
+        print(stdout.strip())
         self.assertTrue(truth_output==stdout.strip())
 
     def test_e2e_cohortbrowser_sql(self):
-        cohort_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-GB8ZQ9Q0vGPk8xzV4JZF288p"
-        truth_output = "SELECT `patient_1`.`patient_id` AS `patient.patient_id`, `patient_1`.`name` AS `patient.name`, `patient_1`.`weight` AS `patient.weight`, `patient_1`.`height` AS `patient.height`, `patient_1`.`size` AS `patient.size` FROM `database_g9k1260089qpxpf468f9zybj__test_dml_out01`.`patient` AS `patient_1` WHERE `patient_1`.`patient_id` IN (SELECT DISTINCT `patient_1`.`patient_id` AS `patient_id` FROM `database_g9k1260089qpxpf468f9zybj__test_dml_out01`.`patient` AS `patient_1` WHERE `patient_1`.`patient_id` IN (SELECT `patient_id` FROM (SELECT DISTINCT `patient_1`.`patient_id` AS `patient_id` FROM `database_g9k1260089qpxpf468f9zybj__test_dml_out01`.`patient` AS `patient_1` WHERE `patient_1`.`height` BETWEEN 68 AND 70 INTERSECT SELECT DISTINCT `patient_1`.`patient_id` AS `patient_id` FROM `database_g9k1260089qpxpf468f9zybj__test_dml_out01`.`patient` AS `patient_1` WHERE UNIX_TIMESTAMP(`patient_1`.`dob`) BETWEEN 925516800 AND 988675200)));"
+        cohort_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-GBGq1pQ0vGPf0qYg30Jg7kkG"
+        truth_output = "SELECT `patient_0001_1`.`patient_id` AS `patient.patient_id`, `patient_0001_1`.`name` AS `patient.name`, `patient_0002_1`.`weight` AS `patient.weight`, `patient_0001_1`.`date_of_birth` AS `patient.date_of_birth`, `patient_0002_1`.`verified_dtm` AS `patient.verified_dtm`, `test_1`.`test_id` AS `test.test_id`, `trial_visit_0001_1`.`visit_id` AS `trial_visit.visit_id`, `baseline_0001_1`.`baseline_id` AS `baseline.baseline_id`, `hospital_0001_1`.`hospital_id` AS `hospital.hospital_id`, `doctor_0001_1`.`doctor_id` AS `doctor.doctor_id` FROM `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`patient_0001` AS `patient_0001_1` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`patient_0002` AS `patient_0002_1` ON `patient_0001_1`.`patient_id` = `patient_0002_1`.`patient_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`trial_visit_0001` AS `trial_visit_0001_1` ON `patient_0001_1`.`patient_id` = `trial_visit_0001_1`.`visit_patient_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`test` AS `test_1` ON `trial_visit_0001_1`.`visit_id` = `test_1`.`test_visit_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`baseline_0001` AS `baseline_0001_1` ON `patient_0001_1`.`patient_id` = `baseline_0001_1`.`b_patient_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`hospital_0001` AS `hospital_0001_1` ON `patient_0001_1`.`hid` = `hospital_0001_1`.`hospital_id` LEFT OUTER JOIN `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`doctor_0001` AS `doctor_0001_1` ON `trial_visit_0001_1`.`visit_did` = `doctor_0001_1`.`doctor_id` WHERE `patient_0001_1`.`patient_id` IN (SELECT DISTINCT `patient_0001_1`.`patient_id` AS `patient_id` FROM `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`patient_0001` AS `patient_0001_1` WHERE `patient_0001_1`.`patient_id` IN (SELECT `patient_id` FROM (SELECT DISTINCT `cohort_subquery`.`patient_id` AS `patient_id` FROM (SELECT DISTINCT `patient_0001_1`.`patient_id` AS `patient_id`, `patient_0001_1`.`hid` AS `hid` FROM `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`patient_0001` AS `patient_0001_1` WHERE EXISTS (SELECT `hospital_0001_1`.`hospital_id` AS `hospital_id` FROM `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`hospital_0001` AS `hospital_0001_1` WHERE `hospital_0001_1`.`hospital_id` BETWEEN 2 AND 5 AND `hospital_0001_1`.`hospital_id` = `patient_0001_1`.`hid`)) AS `cohort_subquery` INTERSECT SELECT DISTINCT `patient_0001_1`.`patient_id` AS `patient_id` FROM `database_gbgpgyj0vgpy91x32vxygv0z__extract_dataset_test`.`patient_0001` AS `patient_0001_1` WHERE `patient_0001_1`.`patient_id` BETWEEN 2 AND 9)));"
         cmd = ["dx", "extract_dataset", cohort_record, "--fields", "patient.patient_id" , ",", "patient.name", ",", "patient.weight", ",",
-            "patient.height", ",", "patient.size","--sql", "-o", "-"]
+               "patient.date_of_birth", ",", "patient.verified_dtm", ",", "test.test_id", ",", "trial_visit.visit_id", ",", "baseline.baseline_id", 
+               ",", "hospital.hospital_id", ",", "doctor.doctor_id","--sql", "-o", "-"]
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
         stdout = process.communicate()[0]
         self.assertTrue(truth_output==stdout.strip())
 
     def test_e2e_dataset_fields(self):
-        dataset_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-G9k12VQ06G1P42KK7fFK3yKB"
+        dataset_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-GBGpKZ80G8j8k4G84P7GP0B5"
         out_directory = tempfile.mkdtemp()
         cmd = ["dx", "extract_dataset", dataset_record, "--fields", "patient.patient_id" , ",", "patient.name", ",", "patient.weight", ",",
-            "patient.height", ",", "patient.size", "-o", out_directory]
+               "patient.date_of_birth", ",", "patient.verified_dtm", ",", "test.test_id", ",", "trial_visit.visit_id", ",", "baseline.baseline_id", 
+               ",", "hospital.hospital_id", ",", "doctor.doctor_id", "-o", out_directory]
         subprocess.check_call(cmd)
-        truth_file = "project-G9j1pX00vGPzF2XQ7843k2Jq:file-GBBpbq80vGPVB1Q1K9Vq7YQV"
-        self.end_to_end_fields(out_directory=out_directory, rec_name = "test_dml_out01.txt", truth_file=truth_file)
+        truth_file = "project-G9j1pX00vGPzF2XQ7843k2Jq:file-GBGpx800vGPy4qf84JJQBX3x"
+        self.end_to_end_fields(out_directory=out_directory, rec_name = "extract_dataset_test.txt", truth_file=truth_file)
 
     def test_e2e_cohortbrowser_fields(self):
-        cohort_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-GB8ZQ9Q0vGPk8xzV4JZF288p"
+        cohort_record = "project-G9j1pX00vGPzF2XQ7843k2Jq:record-GBGq1pQ0vGPf0qYg30Jg7kkG"
         out_directory = tempfile.mkdtemp()
         cmd = ["dx", "extract_dataset", cohort_record, "--fields", "patient.patient_id" , ",", "patient.name", ",", "patient.weight", ",",
-            "patient.height", ",", "patient.size", "-o", out_directory]
+               "patient.date_of_birth", ",", "patient.verified_dtm", ",", "test.test_id", ",", "trial_visit.visit_id", ",", "baseline.baseline_id", 
+               ",", "hospital.hospital_id", ",", "doctor.doctor_id", "-o", out_directory]
         subprocess.check_call(cmd)
-        truth_file = "project-G9j1pX00vGPzF2XQ7843k2Jq:file-GBBpbq80vGPy35K9B1kyVQ6k"
-        self.end_to_end_fields(out_directory=out_directory, rec_name = "Combined_Cohort.txt", truth_file=truth_file)
+        truth_file = "project-G9j1pX00vGPzF2XQ7843k2Jq:file-GBGq7FQ0vGPpK3GbF0Xbjz17"
+        self.end_to_end_fields(out_directory=out_directory, rec_name = "Combined_Cohort_Test.txt", truth_file=truth_file)
 
     def end_to_end_ddd(self, out_directory, rec_name):
         truth_files_directory = tempfile.mkdtemp()
         os.chdir(truth_files_directory)
-        cmd = ["dx", "download", "project-G9j1pX00vGPzF2XQ7843k2Jq:file-G9k2Yv80vGPbgP551jJ8Xbpx", 
-                                 "project-G9j1pX00vGPzF2XQ7843k2Jq:file-G9jv8pj0vGPx7yPZBP49y9KB",
-                                 "project-G9j1pX00vGPzF2XQ7843k2Jq:file-G9jv8pj0vGPj85byBG764zxV"]
+        cmd = ["dx", "download", "project-G9j1pX00vGPzF2XQ7843k2Jq:file-GBGp61j0vGPpz58XK9f7Vkj6", 
+                                 "project-G9j1pX00vGPzF2XQ7843k2Jq:file-GBGp61Q0vGPj1XzfB2Zb137F",
+                                 "project-G9j1pX00vGPzF2XQ7843k2Jq:file-GBGp61j0vGPq6X9k4GkfyGzv"]
         subprocess.check_call(cmd)
         os.chdir("..")
         truth_file_list = os.listdir(truth_files_directory)
 
         for file in truth_file_list:
             dframe1 = pd.read_csv(os.path.join(truth_files_directory, file)).dropna(axis=1, how='all').sort_index(axis=1)
-            dframe2 = pd.read_csv(os.path.join(out_directory, f"{rec_name}.{file}")).dropna(axis=1, how='all').sort_index(axis=1)
+            fil_nam = rec_name + "." + file
+            dframe2 = pd.read_csv(os.path.join(out_directory, fil_nam)).dropna(axis=1, how='all').sort_index(axis=1)
             if file == 'codings.csv':
-                dframe1 = dframe1.sort_values(by='code', axis=0, ignore_index=True)
-                dframe2 = dframe2.sort_values(by='code', axis=0, ignore_index=True)
-            if file == 'entity_dictionary.csv':
-                dframe1 = dframe1.sort_values(by='entity', axis=0, ignore_index=True)
-                dframe2 = dframe2.sort_values(by='entity', axis=0, ignore_index=True)
+                #continue
+                dframe1 = dframe1.sort_values(by=['code','coding_name'], axis=0).reset_index(drop=True)
+                dframe2 = dframe2.sort_values(by=['code','coding_name'], axis=0).reset_index(drop=True)
+            elif file in ['entity_dictionary.csv', 'data_dictionary.csv']:
+                dframe1 = dframe1.sort_values(by='entity', axis=0).reset_index(drop=True)
+                dframe2 = dframe2.sort_values(by='entity', axis=0).reset_index(drop=True)
             self.assertTrue(dframe1.equals(dframe2))
         
         shutil.rmtree(out_directory)
@@ -112,6 +120,9 @@ class TestDXExtractDataset(unittest.TestCase):
         dframe2 = pd.read_csv(os.path.join(out_directory, rec_name))
         dframe2 = dframe2.sort_values(by=list(dframe2.columns), axis=0, ignore_index=True)
         self.assertTrue(dframe1.equals(dframe2))
+
+        shutil.rmtree(out_directory)
+        shutil.rmtree(truth_files_directory)
 
 if __name__ == '__main__':
     unittest.main()
