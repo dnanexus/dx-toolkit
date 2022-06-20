630469be759af80e8a004412767d955e203ab842
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-06-20 12:54:42 +0200

    Bugfixes, added tests, changed templates, added types, removed secret directive output file

---
+++ src/python/dxpy/nextflow/nextflow_builder.py
@@ -0,0 +1,138 @@
+import os
+from dxpy.nextflow.nextflow_templates import get_nextflow_dxapp
+from dxpy.nextflow.nextflow_templates import get_nextflow_src
+import tempfile
+import dxpy
+import json
+from distutils.dir_util import copy_tree
+
+def get_template_dir():
+    return os.path.join(os.path.dirname(dxpy.__file__), 'templating', 'templates', 'nextflow')
+
+
+def write_exec(folder, content):
+    exec_file = f"{folder}/nextflow.sh"
+    os.makedirs(os.path.dirname(os.path.abspath(exec_file)), exist_ok=True)
+    with open(exec_file, "w") as exec:
+        exec.write(content)
+
+
+def write_dxapp(folder, content):
+    dxapp_file = f"{folder}/dxapp.json"
+    with open(dxapp_file, "w") as dxapp:
+        json.dump(content, dxapp)
+
+
+def build_pipeline_from_repository(repository, tag, profile, brief):
+    """
+    :param repository: URL to git repository
+    :type repository: string
+    :param tag: tag of given git repository. if not given, default branch is used.
+    :type tag: string
+    :param profile: Custom NF profile, for more information visit https://www.nextflow.io/docs/latest/config.html#config-profiles
+    :type profile: string
+    :param brief: Level of verbosity
+    :type brief: boolean
+
+    Runs the Nextflow Pipeline Importer app, which creates NF applet from given repository.
+    """
+    build_project_id = dxpy.WORKSPACE_ID
+    if build_project_id is None:
+        parser.error(
+            "Can't create an applet without specifying a destination project; please use the -d/--destination flag to explicitly specify a project")
+    input_hash = {
+        "repository_url": repository,
+        "repository_tag": tag,
+        "config_profile": profile
+    }
+
+    api_options = {
+        "name": "Nextflow build of %s" % (repository),
+        "input": input_hash,
+        "project": build_project_id,
+    }
+
+    # TODO: this will have to be an app app_run!
+    app_run_result = dxpy.api.applet_run('applet-GFb8kQj0469zQ5P5BQGYpKJz', input_params=api_options)
+    job_id = app_run_result["id"]
+    if not brief:
+        print("Started builder job %s" % (job_id,))
+    dxpy.DXJob(job_id).wait_on_done(interval=1)
+    applet_id, _ = dxpy.get_dxlink_ids(dxpy.api.job_describe(job_id)['output']['output_applet'])
+    if not brief:
+        print("Created Nextflow pipeline %s" % (applet_id))
+    else:
+        print(applet_id)
+    return applet_id
+
+def prepare_nextflow(resources_dir, profile):
+    """
+    :param resources_dir: Directory with all resources needed for Nextflow Pipeline. Usually directory with user's NF files.
+    :type resources_dir: str or Path
+    :param profile: Custom NF profile, for more information visit https://www.nextflow.io/docs/latest/config.html#config-profiles
+    :type profile: string
+
+    Creates files for creating applet, such as dxapp.json and source file. These files are created in temp directory.
+    """
+    assert os.path.exists(resources_dir)
+    inputs = []
+    # dxapp_dir = tempfile.mkdtemp(prefix="dx.nextflow.")
+    os.makedirs(".dx.nextflow", exist_ok=True)
+    dxapp_dir = os.path.join(resources_dir, '.dx.nextflow')
+    if os.path.exists(f"{resources_dir}/nextflow_schema.json"):
+        inputs = prepare_inputs(f"{resources_dir}/nextflow_schema.json")
+    DXAPP_CONTENT = get_nextflow_dxapp(inputs)
+    EXEC_CONTENT = get_nextflow_src(inputs=inputs, profile=profile)
+    copy_tree(get_template_dir(), dxapp_dir)
+    write_dxapp(dxapp_dir, DXAPP_CONTENT)
+    write_exec(dxapp_dir, EXEC_CONTENT)
+    return dxapp_dir
+
+# TODO: Add docstrings for all the methods.
+def prepare_inputs(schema_file):
+    def get_default_input_value(key):
+        types = {
+            "hidden": False,
+            "help": "Default help message"
+            # TODO: add directory + file + path
+        }
+        if key in types:
+            return types[key]
+        return "NOT_IMPLEMENTED"
+
+    def get_dx_type(nf_type):
+        types = {
+            "string": "str",
+            "integer": "int",
+            "number": "float",
+            "boolean": "boolean",
+            "object": "hash"  # TODO: check default values
+            # TODO: add directory + file + path
+        }
+        if nf_type in types:
+            return types[nf_type]
+        return "string"
+        # raise Exception(f"type {nf_type} is not supported by DNAnexus")
+
+    inputs = []
+    try:
+        with open(schema_file, "r") as fh:
+            schema = json.load(fh)
+    except Exception as json_e:
+        raise AssertionError(json_e)
+    for d_key, d_schema in schema.get("definitions", {}).items():
+        required_inputs = d_schema.get("required", [])
+        for property_key, property in d_schema.get("properties", {}).items():
+            dx_input = {}
+            dx_input["name"] = property_key
+            dx_input["title"] = dx_input['name']
+            dx_input["help"] = property.get('help_text', get_default_input_value('help'))
+            if "default" in property:
+                dx_input["default"] = property.get("default")
+            dx_input["hidden"] = property.get('hidden', get_default_input_value('hidden'))
+            dx_input["class"] = get_dx_type(property_key)
+            if property_key not in required_inputs:
+                dx_input["optional"] = True
+                dx_input["help"] = "(Optional) {}".format(dx_input["help"])
+            inputs.append(dx_input)
+    return inputs
---
+++ src/python/dxpy/nextflow/nextflow_templates.py
@@ -0,0 +1,130 @@
+def get_default_inputs():
+    return [
+        {
+            "name": "nf_run_args_and_pipeline_params",
+            "label": "Nextflow Run Arguments and Pipeline Parameters",
+            "help": "Additional run arguments and pipeline parameters for Nextflow (i.e. -queue-size).",
+            "class": "string",
+            "optional": True
+        },
+        {
+            "name": "resume",
+            "label": "Resume",
+            "help": "Enables resume functionality in Nextflow workflows.",
+            "class": "boolean",
+            "default": False
+        },
+        {
+            "name": "resume_session",
+            "label": "Resume Session",
+            "help": "Session or job to be resumed.",
+            "class": "string",
+            "optional": True
+        },
+        {
+            "name": "nf_advanced_opts",
+            "label": "Nextflow Advanced Options",
+            "help": "Advanced options for Nextflow (i.e. -quiet).",
+            "class": "string",
+            "optional": True
+        },
+        {
+            "name": "docker_creds",
+            "label": "Docker Credentials",
+            "help": "Docker Credentials used to obtain private docker images.",
+            "class": "file",
+            "optional": True
+        },
+        {
+            "name": "debug",
+            "label": "Debug Mode",
+            "help": "Shows additional information in Nextflow logs.",
+            "class": "boolean",
+            "default": False
+        },
+        {
+            "name": "no_future_resume",
+            "label": "No Future Resume",
+            "help": "Allow saving workspace and cache files to the platform to be used later for resume functionality.",
+            "class": "boolean",
+            "default": False
+        }
+    ]
+def get_nextflow_dxapp(custom_inputs=[]):
+    inputs = custom_inputs + get_default_inputs()
+    return {
+        "name": "nextflow pipeline",
+        "title": "Nextflow Pipeline",
+        "summary": "nextflow",
+        "dxapi": "1.0.0",
+        "version": "1.0.0",
+        "inputSpec": inputs,
+        "outputSpec": [
+        ],
+        "runSpec": {
+            "interpreter": "bash",
+            "execDepends": [
+                {
+                    "name": "default-jre"
+                }
+            ],
+            "distribution": "Ubuntu",
+            "release": "20.04",
+            "file": "nextflow.sh",
+            "version": "0"
+        },
+        "regionalOptions": {
+            "aws:us-east-1": {
+                "systemRequirements": {
+                    "*": {
+                        "instanceType": "mem1_ssd1_v2_x8"
+                    }
+                }
+            }
+        },
+        "details": {
+            "whatsNew": "1.0.0: Initial version"
+        },
+        "categories": [],
+        "access": {
+            "network": [
+                "*"
+            ],
+            "project": "CONTRIBUTE",
+            "allProjects": "VIEW"
+        }
+    }
+
+# TODO: change args to individual arguments.
+def get_nextflow_src(inputs=[], profile=None):
+    run_inputs = ""
+    for i in inputs:
+        # wverride arguments that were not given at the runtime
+        run_inputs = run_inputs + f'''
+        if [ -n "${i['name']}" ]; then
+            filtered_inputs="${{filtered_inputs}} --{i['name']}=${i['name']}"
+        fi
+        '''
+
+    profile_arg = "-profile {}".format(profile) if profile else ""
+    return f'''
+    #!/usr/bin/env bash
+
+    if [ -n "$docker_creds" ]; then
+        dx download "$docker_creds" -o /home/dnanexus/credentials
+        ls /home/dnanexus
+        source /.dx.nextflow/resources/usr/local/bin/dx-registry-login
+    fi
+    curl -s https://get.nextflow.io | bash
+    mv nextflow /usr/bin
+    filtered_inputs=""
+    
+    if "$debug" ; then
+        export NXF_DEBUG=2
+    fi
+    {run_inputs}
+    nextflow run {profile_arg} / $nf_run_args_and_pipeline_params ${{filtered_inputs}}
+    '''
+
+# iterate through inputs of dxapp.json and add them here?
+# put them in params file?
