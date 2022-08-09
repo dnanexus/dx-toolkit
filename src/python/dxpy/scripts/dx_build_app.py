8d8031a793e1b886b259994d3d66a42b2bc83609
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-08-09 15:03:22 +0200

    minor

--- src/python/dxpy/scripts/dx_build_app.py
+++ src/python/dxpy/scripts/dx_build_app.py
@@ -1027,7 +1027,7 @@ def _build_app(args, extra_args):
 
         return output['id']
 
-    if not args.remote:
+    if not args.remote or args.repository:  # building with NF repository is implicitly remote
         # LOCAL BUILD
         # change nextflow HERE TODO:
         try:
