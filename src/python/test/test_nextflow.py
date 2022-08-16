3108f0828d62784b8155a55e023ce20e36b1858f
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-08-16 15:31:40 +0200

    test update

--- src/python/test/test_nextflow.py
+++ src/python/test/test_nextflow.py
@@ -121,13 +121,16 @@ class TestNextflowTemplates(DXTestCase):
     def test_prepare_inputs(self):
         inputs = prepare_inputs("./nextflow/schema2.json")
         names = [i["name"] for i in inputs]
+        print(inputs)
         self.assertTrue("input" in names and "outdir" in names and "save_merged_fastq" in names)
+        i_input = inputs["name"]
+        i_outdir = inputs["outdir"]
+        i_save_merged_fastq = inputs["save_merged_fastq"]
         self.assertEqual(len(names), 3)
 
     def test_prepare_inputs(self):
         inputs = prepare_inputs("./nextflow/schema1.json")
-        print(len(inputs))
-        self.assertEqual(len(inputs), 72)
+        self.assertEqual(len(inputs), 93)
 
 
 
