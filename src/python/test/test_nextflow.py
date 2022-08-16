c7df60d5f44eb0752feecef222535e8fc1f093b7
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-08-16 15:08:20 +0200

    test update

--- src/python/test/test_nextflow.py
+++ src/python/test/test_nextflow.py
@@ -98,7 +98,7 @@ class TestNextflowTemplates(DXTestCase):
         [input1, input2]
     ])
     def test_dxapp_single_custom_input(self, *inputs):
-        print(inputs)
+        inputs = list(inputs)
         dxapp = get_nextflow_dxapp(custom_inputs=inputs)
         self.assertTrue(self.are_inputs_in_spec(dxapp.get("inputSpec"), inputs))
         self.assertEqual(len(dxapp.get("inputSpec")), default_input_len + len(inputs))
