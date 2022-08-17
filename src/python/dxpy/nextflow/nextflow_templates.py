893c590ef3f7bff7d2eb22fc7fe307262a2bf8f7
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-08-17 15:42:30 +0200

    change

--- src/python/dxpy/nextflow/nextflow_templates.py
+++ src/python/dxpy/nextflow/nextflow_templates.py
@@ -110,6 +110,37 @@ def get_nextflow_src(inputs=[], profile=None):
     return f'''
     #!/usr/bin/env bash
 
+    on_exit() {{
+      ret=$?
+      # upload log file
+      dx upload $LOG_NAME --path $DX_LOG --wait --brief --no-progress --parents || true
+      # backup cache
+      echo "=== Execution complete — uploading Nextflow cache metadata files"
+      dx rm -r "$DX_PROJECT_CONTEXT_ID:/.nextflow/cache/$NXF_UUID/*" 2>&1 >/dev/null || true
+      dx upload ".nextflow/cache/$NXF_UUID" --path "$DX_PROJECT_CONTEXT_ID:/.nextflow/cache/$NXF_UUID" --no-progress --brief --wait -p -r || true
+      # done
+      exit $ret
+    }}
+    
+      dx_path() {{
+      local str=${{1#"dx://"}}
+      local tmp=$(mktemp -t nf-XXXXXXXXXX)
+      case $str in
+        project-*)
+          dx download $str -o $tmp --no-progress --recursive -f
+          echo file://$tmp
+          ;;
+        container-*)
+          dx download $str -o $tmp --no-progress --recursive -f
+          echo file://$tmp
+          ;;
+        *)
+          echo "Invalid $2 path: $1"
+          return 1
+          ;;
+      esac
+    }}
+
     if [ -n "$docker_creds" ]; then
         dx download "$docker_creds" -o /home/dnanexus/credentials
         ls /home/dnanexus
