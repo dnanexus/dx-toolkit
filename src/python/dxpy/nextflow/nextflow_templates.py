3979634ffcc322602bece71b39dcb0e30e3d5a87
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-08-18 13:36:02 +0200

    minor

--- src/python/dxpy/nextflow/nextflow_templates.py
+++ src/python/dxpy/nextflow/nextflow_templates.py
@@ -206,7 +206,7 @@ main() {{
     filtered_inputs=""
     
     {run_inputs}
-    nextflow -trace nextflow.plugin $nf_advanced_opts run {profile_arg} / $nf_run_args_and_pipeline_params ${{filtered_inputs}}
+    nextflow $nf_advanced_opts run {profile_arg} / $nf_run_args_and_pipeline_params ${{filtered_inputs}}
     set +f
 }}
     '''
