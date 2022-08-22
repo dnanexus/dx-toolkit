323dbc3caefe9fe50def7f1728d2a1be61c15025
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-08-22 11:23:29 -0700

    templates

--- src/python/dxpy/nextflow/nextflow_templates.py
+++ src/python/dxpy/nextflow/nextflow_templates.py
@@ -54,8 +54,9 @@ def get_default_inputs():
     ]
 def get_nextflow_dxapp(custom_inputs=[]):
     inputs = custom_inputs + get_default_inputs()
-    dxapp =
-    return
+    dxapp = json.load(os.path.join(get_template_dir, "dxapp.json"))
+    dxapp["inputSpec"] = inputs
+    return dxapp
 
 
 # TODO:
