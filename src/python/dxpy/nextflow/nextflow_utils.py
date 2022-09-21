60d3392a3fc9969cff620350f2f0769cffb19aa8
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-09-21 15:55:26 +0200

    updated order of assets

--- src/python/dxpy/nextflow/nextflow_utils.py
+++ src/python/dxpy/nextflow/nextflow_utils.py
@@ -40,7 +40,7 @@ def write_dxapp(folder, content):
 
 def get_regional_options():
     region = dxpy.DXProject().describe()["region"]
-    nextaur_asset, nextflow_asset = get_nextflow_assets(region)
+    nextflow_asset, nextaur_asset = get_nextflow_assets(region)
     regional_options = {
         region: {
             "systemRequirements": {
@@ -67,13 +67,13 @@ def get_nextflow_assets(region):
         "aws:eu-west-2-g": ("record-GGbPk72KX7VbgXp34y4z119x", "record-GGbPqXpKyyZzyfPg4yX55JXb")
     }
     stg_assets = {
-        "aws:ap-southeast-2": ("record-GGbJpPj59YgV92b98Yz216qZ", "record-GGbJz3j52fYPG2fp8Y1Q3vvG"),
-        "aws:eu-central-1": ("record-GGbJpFj4YYB26fg18YKxFQq8", "record-GGbJz204G2gQ2bpj8BzyYQzj"),
-        "aws:eu-west-2": ("record-GGbJp7jJ8F8V92b98Yz216qQ", "record-GGbJz10JpGq192b98Yz21pJQ"),
-        "aws:us-east-1": ("record-GG0q3X00fVVZQP9G4kFF16zp", "record-GGb8zb801pBqB2PF8XjBf7Px"),
-        "azure:westeurope": ("record-GGbJq6jBB8k26fg18YKxFQqf", "record-GGbJyZ8BP1f3X0xP8GkgJP41"),
-        "azure:westus": ("record-GGbJxfQ9qK380ZZJ8Y2KXVFb", "record-GGbJy489qFGpB0468YFXxyX5"),
-        "aws:eu-west-2-g": ("record-GGbJp92K0KbzG2fp8Y1Q2bkG", "record-GGbJyxpKkJJFB2PF8XjBpV00")
+        "aws:ap-southeast-2": ("record-GGbJz3j52fYPG2fp8Y1Q3vvG", "record-GGbVF5857Zj7PbQQ8FP1KKxX"),
+        "aws:eu-central-1": ("record-GGbJz204G2gQ2bpj8BzyYQzj", "record-GGbVBb84ZBgV92b98Yz263QK"),
+        "aws:eu-west-2": ("record-GGbJz10JpGq192b98Yz21pJQ", "record-GGbVBp8JXvXj0ZZJ8Y2KYjj7"),
+        "aws:us-east-1": ("record-GGb8zb801pBqB2PF8XjBf7Px", "record-GG23y1j00vk1BJ5zKzykGXvY"),
+        "azure:westeurope": ("record-GGbJyZ8BP1f3X0xP8GkgJP41", "record-GGbVFZjBQZzGv2p88F39ZJY3"),
+        "azure:westus": ("record-GGbJy489qFGpB0468YFXxyX5", "record-GGbVG009zzFX6fg18YKxKXy7"),
+        "aws:eu-west-2-g": ("record-GGbJyxpKkJJFB2PF8XjBpV00", "record-GGbVBv2KqxX767f98FXG6p0Q")
     }
 
     try:
