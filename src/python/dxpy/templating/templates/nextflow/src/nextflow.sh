4450c6e43d371b27deb0248f4d8eda4e7923f159
Yuxin Shi <yshi@dnanexus.com>
2022-10-14 15:01:28 -0700

    add cache store

--- src/python/dxpy/templating/templates/nextflow/src/nextflow.sh
+++ src/python/dxpy/templating/templates/nextflow/src/nextflow.sh
@@ -69,6 +69,13 @@ on_exit() {
     set -xe
   fi
 
+  # backup cache
+  if [[ $no_future_cache == false ]]; then
+    echo "=== Execution complete — uploading Nextflow cache and working files"
+    dx rm -r "$DX_PROJECT_CONTEXT_ID:/.nextflow/$NXF_UUID/*" 2>&1 >/dev/null || true
+    # TODO: tar cache and history files
+    # dx upload ".nextflow/cache/$NXF_UUID" --path "$DX_PROJECT_CONTEXT_ID:/.nextflow/cache/$NXF_UUID" --no-progress --brief --wait -p -r || true
+  fi
 
   # remove .nextflow from the current folder /home/dnanexus/output_files
   rm -rf .nextflow
