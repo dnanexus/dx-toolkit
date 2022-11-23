f8f5016f06359bb69045e5cb8a07b6cfddd922e5
Ola Zalcman <biokere@gmail.com>
2022-11-23 11:57:39 -0800

    Typo

--- src/python/test/test_nextflow.py
+++ src/python/test/test_nextflow.py
@@ -101,7 +101,7 @@ class TestNextflowTemplates(DXTestCase):
         self.assertTrue("-profile test_profile" in src)
 
     @unittest.skipIf(USING_PYTHON2,
-        'Skipping as the Nextflow template from which applets are built is for Py3 interpreter only'
+        'Skipping as the Nextflow template from which applets are built is for Py3 interpreter only')
     def test_src_inputs(self):
         src = get_nextflow_src(custom_inputs=[input1, input2])
         self.assertTrue("if [ -n \"${}\" ];".format(input2.get("name")) in src)
