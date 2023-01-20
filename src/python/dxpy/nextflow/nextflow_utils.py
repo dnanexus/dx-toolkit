c3d5baaeea35d732c3297712bba01b99d0c97b56
Marek Hrvol <mhrvol-cf@dnanexus.com>
2023-01-20 14:37:37 +0100

    minmor

--- src/python/dxpy/nextflow/nextflow_utils.py
+++ src/python/dxpy/nextflow/nextflow_utils.py
@@ -90,7 +90,7 @@ def get_nextflow_assets(region):
             "record-GKv1Jp0Jf425x9FK9jbqgQJP",
             "record-GKv18b0Jz9YV9vqx9j9GGq05"),
         "aws:us-east-1": (
-            "record-GP58y000ZGj469BK0J6J4qf4",
+            "record-GP59VV80zXP4843ffZ0Y7fv2",
             "record-GKv0P7Q00Q82gyJjGG8y03xf"),
         "azure:westeurope": (
             "record-GKv1PGQBK7fx1z529j3fKVB8",
