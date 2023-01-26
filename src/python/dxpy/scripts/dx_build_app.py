e6b1a15bdab9d5b821247fad6c54d7324a0d6fff
Ola Zalcman <biokere@gmail.com>
2023-01-26 11:58:48 -0800

    Do not check the billTo of the project for error message

--- src/python/dxpy/scripts/dx_build_app.py
+++ src/python/dxpy/scripts/dx_build_app.py
@@ -1034,8 +1034,7 @@ def verify_nf_license(destination, extra_args):
     features = dxpy.DXHTTPRequest("/" + dest_project_to_check + "/checkFeatureAccess", {"features": ["dxNextflow"]}).get("features", {})
     dx_nextflow_lic = features.get("dxNextflow", False)
     if not dx_nextflow_lic:
-        billTo = dxpy.api.project_describe(dest_project_to_check, input_params={"fields": {"billTo": True}})["billTo"]
-        raise dxpy.app_builder.AppBuilderException("PermissionDenied: billTo " + billTo + " of the applet's destination project must have the dxNextflow feature enabled. For inquiries, please contact support@dnanexus.com")
+        raise dxpy.app_builder.AppBuilderException("PermissionDenied: billTo of the applet's destination project must have the dxNextflow feature enabled. For inquiries, please contact support@dnanexus.com")
 
 def _build_app(args, extra_args):
     """Builds an app or applet and returns the resulting executable ID
