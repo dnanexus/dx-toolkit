e247634cd58889ad81d224349ace6fec5a95e43b
Marek Hrvol <mhrvol-cf@dnanexus.com>
2023-02-09 12:20:01 +0100

    minor

--- src/python/test/test_nextflow.py
+++ src/python/test/test_nextflow.py
@@ -340,7 +340,7 @@ class TestRunNextflowApplet(DXTestCaseBuildNextflowApps):
             run("dx build --nextflow --json " + applet_dir))["id"]
         applet = dxpy.DXApplet(applet_id)
         job = applet.run({})
-        job.wait_on_done()
+        self.assertRaises(job.wait_on_done(), dxpy.exceptions.DXJobFailureError)
         desc = job.describe()
         self.assertEqual(desc.get("properties",{}).get("nextflow_errorStrategy"), "retry-exceedsMaxValue")
         self.assertEqual(desc.get("state"), "failed")
