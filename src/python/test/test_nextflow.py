bed9bb9dcfa9555209717122b697b4d336b45c14
Marek Hrvol <mhrvol-cf@dnanexus.com>
2023-02-09 11:48:44 +0100

    minor

--- src/python/test/test_nextflow.py
+++ src/python/test/test_nextflow.py
@@ -333,7 +333,7 @@ class TestRunNextflowApplet(DXTestCaseBuildNextflowApps):
     @unittest.skipUnless(testutil.TEST_RUN_JOBS,
                          'skipping tests that would run jobs')
     def test_dx_run_retry_fail(self):
-        pipeline_name = "retryMaxError"
+        pipeline_name = "retryMaxRetries"
         applet_dir = self.write_nextflow_applet_directory(
             pipeline_name, existing_nf_file_path=self.retry_nextflow_nf)
         applet_id = json.loads(
