7fd910a6b0674051d84454c3c7ed97793d275b3b
Bill Agee <billagee@gmail.com>
2023-03-23 22:21:34 -0700

    DEVEX-2216 Ubuntu 22.04 + py3.10 'make python' fix (#931)
    * DEVEX-2216 Ubuntu 22.04 + py3.10 'make python' fix
    
    * Use PYTHON_VERSION_NUMBER var at build time

--- src/python/setup.py
+++ src/python/setup.py
@@ -104,7 +104,7 @@ setup(
     install_requires = dependencies,
     extras_require={
         'pandas': ["pandas==1.3.5; python_version>='3.7'", "pandas>=0.23.3,<=0.25.3; python_version>='3.5.3' and python_version<'3.7'", "pandas>=0.23.3,< 0.25.0; python_version<'3.5.3'"],
-        'xattr': ["xattr==0.9.6; sys_platform == 'linux2' or sys_platform == 'linux'"]
+        'xattr': ["xattr==0.10.1; sys_platform == 'linux2' or sys_platform == 'linux'"]
     },
     tests_require = test_dependencies,
     test_suite = "test",
