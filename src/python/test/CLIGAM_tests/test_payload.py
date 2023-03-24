af377a3d4e1f5635fa6af0a45408e817fcb6a3e6
JoeMulka <joseph.mulka1@gmail.com>
2023-03-23 19:26:44 -0500

    reorganizing test input

--- src/python/test/CLIGAM_tests/test_payload.py
+++ src/python/test/CLIGAM_tests/test_payload.py
@@ -2,8 +2,8 @@
 # exucute with: python /dx-toolkit/src/python/test/CLIGAM_tests/test_payload.py
 import subprocess
 
-filter = "/dx-toolkit/src/python/test/CLIGAM_tests/test_input/unit_tests/allele_01.json"
-output = "allele_01_output.json"
+filter = "/dx-toolkit/src/python/test/CLIGAM_tests/test_input/unit_tests/single_filters/allele_rsid.json"
+output = "allele_rsid_output.json"
 dataset = "project-FkyXg38071F1vGy2GyXyYYQB:record-FyFPyz0071F54Zjb32vG82Gj"
 
 command = "dx extract_assay germline {} --retrieve-allele {} --output {}".format(
