b4f8bceaf5ca9a997f2971ac02852a398aef3fb5
JoeMulka <joseph.mulka1@gmail.com>
2023-03-28 13:26:17 -0500

    adding arg for test script to switch to sql mode

--- src/python/test/CLIGAM_tests/test_payload.py
+++ src/python/test/CLIGAM_tests/test_payload.py
@@ -1,6 +1,11 @@
 # To be run within a docker container that the dxpy package has been build locally in
 # exucute with: python /dx-toolkit/src/python/test/CLIGAM_tests/test_payload.py
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--sql", action="store_true")
+args = parser.parse_args()
 
 filter = "/dx-toolkit/src/python/test/CLIGAM_tests/test_input/unit_tests/single_filters/allele_rsid.json"
 output = "allele_rsid_output.json"
@@ -9,5 +14,8 @@ dataset = "project-FkyXg38071F1vGy2GyXyYYQB:record-FyFPyz0071F54Zjb32vG82Gj"
 command = "dx extract_assay germline {} --retrieve-allele {} --output {}".format(
     dataset, filter, output
 )
+
+if args.sql:
+    command += " --sql"
 
 process = subprocess.run(command, shell=True)
