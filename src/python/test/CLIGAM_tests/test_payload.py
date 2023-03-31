520793b312b12fb8fb4d21b5326e4f69184c1581
JoeMulka <joseph.mulka1@gmail.com>
2023-03-31 15:48:18 -0500

    Modify user input (#985)
    * logic for handling case sensitivity
    
    * We can't actually assume that there will be an underscore before UTR
    
    * updating one off test script to modern version
    
    * removing modification to consequences column
    
    * had order of hom/hom-alt replacement inverted
    
    * removing to-uppercase coercion for gene_name and putative_impact
    
    * returning putative_impact to list of case-insensitive fields
    
    * removing debug print statement

--- src/python/test/CLIGAM_tests/test_payload.py
+++ src/python/test/CLIGAM_tests/test_payload.py
@@ -1,27 +1,13 @@
 # To be run within a docker container that the dxpy package has been build locally in
-import json
-from dxpy.dx_extract_utils.turbo_filter import FinalPayload
+# exucute with: python /dx-toolkit/src/python/test/CLIGAM_tests/test_payload.py
+import subprocess
 
-filter = "/dx-toolkit/src/python/test/CLIGAM_tests/test_input/unit_tests/allele_malformatted.json"
-output = "~/allele_malformatted_output.json"
-type = "allele"
-project_context = "project-FkyXg38071F1vGy2GyXyYYQB"
-name = "veo_demo_dataset_assay"
-id = "da6a4ffc-7571-4b2f-853d-445460a18396"
-ref_genome = "GRCh38.92"
-sql_flag = False
+filter = "/dx-toolkit/src/python/test/CLIGAM_tests/test_input/unit_tests/single_filters/allele_rsid.json"
+output = "allele_rsid_output.json"
+dataset = "project-FkyXg38071F1vGy2GyXyYYQB:record-FyFPyz0071F54Zjb32vG82Gj"
 
-
-with open(filter, "r") as infile:
-    filter_dict = json.load(infile)
-full_payload = FinalPayload(
-    filter_dict, name, id, project_context, ref_genome, type, sql_flag
+command = "dx extract_assay germline {} --retrieve-allele {} --output {}".format(
+    dataset, filter, output
 )
 
-print(full_payload)
-
-# Send payload to vizserver
-# response = requests.post("/viz-data/3.0/record-xxxx/raw", json=json.dumps(full_payload))
-
-# print("Status Code: {}".format(response.status_code))
-# print("Response JSON: {}".format(response.json()))
+process = subprocess.run(command, shell=True)
