dbf95ce73d069928919dec8cf30040bd6c0528b0
Jan Stourac <jstourac@dnanexus.com>
2023-04-13 13:45:17 +0200

    Revert "add invert matcher for pyenvs"
    This reverts commit 3c3855b16beaaa91d10a38eb8256045092dc3238.

--- build/dependencies_sanity_tests/run_macos.py
+++ build/dependencies_sanity_tests/run_macos.py
@@ -76,6 +76,7 @@ class DXPYTestsRunner:
     workers: int = 1
     print_logs: bool = False
     print_failed_logs: bool = False
+    skip_official_pythons: bool = False
     _macos_version: float = float('.'.join(platform.mac_ver()[0].split('.')[:2]))
     _brew_in_opt: bool = Path("/opt/homebrew").is_dir()
     _test_results: Dict[str, bool] = field(default_factory=dict, init=False)
@@ -89,6 +90,9 @@ class DXPYTestsRunner:
         has_filters = self.pyenv_filters is not None and len(self.pyenv_filters) > 0
         pyenvs = [p for p in PYENVS if any(map(lambda x: x.match(p), self.pyenv_filters))] if has_filters else PYENVS
         pyenvs.sort()
+
+        if self.skip_official_pythons:
+            pyenvs = [p for p in pyenvs if not p.startswith("official-")]
 
         logging.info("Python environments: " + ", ".join(pyenvs))
 
@@ -186,11 +190,14 @@ if __name__ == "__main__":
 
     init_base_argparser(parser)
 
+    parser.add_argument("--skip-official-pythons", action="store_true", help="Skip official Python distributions as they need to be manually installed using GUI")
+
     args = parser.parse_args()
 
     init_logging(args.verbose)
 
     ret = DXPYTestsRunner(
-        **parse_common_args(args)
+        **parse_common_args(args),
+        skip_official_pythons=args.skip_official_pythons,
     ).run()
     sys.exit(ret)
