621c79e56b6d033eb76c3fae76566da8584133ff
Jan Stourac <jstourac@dnanexus.com>
2023-04-19 12:53:10 +0200

    WIP GHA watch failure

--- build/dependencies_sanity_tests/dependencies_sanity_tests.py
+++ build/dependencies_sanity_tests/dependencies_sanity_tests.py
@@ -16,7 +16,7 @@ FILESIZE = 100
 TEST_DIR = os.path.abspath(os.path.join(__file__, os.pardir))
 IS_LINUX = platform.system() == "Linux"
 IS_WINDOWS = platform.system() == "Windows"
-GHA_WATCH_RETRIES = 10
+GHA_WATCH_RETRIES = 5
 GHA_KNOWN_WATCH_ERRORS = (
     "[Errno 110] Connection timed out", "[Errno 104] Connection reset by peer", "1006: Connection is already closed.", "[Errno 32] Broken pipe",
     "1006: EOF occurred in violation of protocol"
@@ -266,16 +266,12 @@ def test_job_watch(project, applet):
     for i in range(GHA_WATCH_RETRIES):
         res = subprocess.run(["dx", "watch", job_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
-        if res.returncode != 0:
-            for e in GHA_KNOWN_WATCH_ERRORS:
-                print("%s    =    %s" % (e, e in res.stderr))
+        assert res.returncode == 0
 
-        if res.returncode != 0 and any(map(lambda x: x in res.stderr, GHA_KNOWN_WATCH_ERRORS)):
-            time.sleep(30)
-            print("Retrying")
+        if any(map(lambda x: x in res.stderr, GHA_KNOWN_WATCH_ERRORS)):
+            time.sleep(15)
             continue
 
-        assert res.returncode == 0
         assert "Started" in res.stdout
         assert "Test to stderr" in res.stdout
         assert "Finished" in res.stdout
