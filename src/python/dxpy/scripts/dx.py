33d9a0b970775462129bfa1acd4c722a3f49e01a
Niranjan Pandeshwar <45427747+Niranjanpandeshwar@users.noreply.github.com>
2023-06-08 17:46:42 -0400

    SCIPROD-1333, SCIPROD-1336, SCIPROD-1338 entry point for dx extract assay somatic (#1054)
    * SCIPROD-1333 Added command line arguments for extract_assay somatic
    
    * SCIPROD-1333, SCIPROD-1336, SCIPROD-1338, Entry point to extract_assay, validations and help messages
    
    * SCIPROD-1333 Update help text for Info and Format changes
    
    * Add dataset validation logic
    
    ---------
    
    Co-authored-by: Niranjan Pandeshwar <niranjanpandeshwar@gmail.com>

--- src/python/dxpy/scripts/dx.py
+++ src/python/dxpy/scripts/dx.py
@@ -41,7 +41,7 @@ from dxpy.exceptions import PermissionDenied, InvalidState, ResourceNotFound
 from ..cli import try_call, prompt_for_yn, INTERACTIVE_CLI
 from ..cli import workflow as workflow_cli
 from ..cli.cp import cp
-from ..cli.dataset_utilities import extract_dataset, extract_assay_germline
+from ..cli.dataset_utilities import extract_dataset, extract_assay_germline, extract_assay_somatic
 from ..cli.download import (download_one_file, download_one_database_file, download)
 from ..cli.parsers import (no_color_arg, delim_arg, env_args, stdout_args, all_arg, json_arg, parser_dataobject_args,
                            parser_single_dataobject_output_args, process_properties_args,
@@ -6142,6 +6142,9 @@ subparsers_extract_assay = parser_extract_assay.add_subparsers(
 parser_extract_assay.metavar = "class"
 register_parser(parser_extract_assay)
 
+#####################################
+# germline
+#####################################
 parser_extract_assay_germline = subparsers_extract_assay.add_parser(
     "germline",
     help="Retrieve the selected data or generate SQL to retrieve the data from an genetic variant assay in a dataset or cohort based on provided rules.",
@@ -6197,7 +6200,7 @@ parser_extract_assay_germline.add_argument(
     '--json-help',
     help=argparse.SUPPRESS,
     action="store_true",
-    )
+)
 parser_extract_assay_germline.add_argument(
     "--sql",
     action="store_true",
@@ -6211,6 +6214,91 @@ parser_extract_assay_germline.add_argument(
 )
 parser_extract_assay_germline.set_defaults(func=extract_assay_germline)
 register_parser(parser_extract_assay_germline)
+
+#####################################
+# somatic
+#####################################
+parser_extract_assay_somatic = subparsers_extract_assay.add_parser(
+    "somatic",
+    help="Retrieve the selected data or generate SQL to retrieve the data from an somatic variant assay in a dataset or cohort based on provided rules.",
+    description="Retrieve the selected data or generate SQL to retrieve the data from an somatic variant assay in a dataset or cohort based on provided rules.",
+    formatter_class=argparse.RawTextHelpFormatter
+)
+
+parser_extract_assay_somatic.add_argument(
+    "path",
+    type=str,
+    help='v3.0 Dataset or Cohort object ID (project-id:record-id where ":record-id" indicates the record-id in current selected project) or name.',
+)
+
+parser_extract_assay_somatic.add_argument(
+    "--list-assays",
+    action="store_true",
+    help="List somatic variant assays available for query in the specified Dataset or Cohort object.",
+)
+
+parser_extract_assay_somatic.add_argument(
+    "--assay-name",
+    default=None,
+    help="Specify a specific somatic variant assay to query. If the argument is not specified, the default assay used is the first assay listed when using the argument, “--list-assays”",
+)
+
+parser_extract_assay_somatic.add_argument(
+    "--retrieve-meta-info",
+    action="store_true",
+    help="List meta information, as it exists in the original VCF headers for both INFO and FORMAT fields.",
+)
+
+parser_extract_assay_somatic.add_argument(
+    "--retrieve-variant",
+    type=str,
+    const='{}',
+    default=None,
+    nargs='?',
+    help="A JSON object, either in a file (.json extension) or as a string, specifying criteria of somatic variants to retrieve. By default returns the following set of fields; “assay_sample_id”, “allele_id”, “chrom”, “pos”, “ref”, and “allele”. Additional fields may be returned using --additional-fields. Specify “--json-help” following this option to get detailed information on the json format and filters. When filtering, must supply one, and only one of “location”, “annotation.gene_name”, “annotation.gene_id”, “annotation.feature_id”, “allele.allele_id”.",
+)
+
+parser_extract_assay_somatic.add_argument(
+    "--include-normal-sample",
+    action="store_true",
+    help="Include variants associated with normal samples in the assay. If no flag is supplied, variants from normal samples will not be supplied.",
+)
+
+parser_extract_assay_somatic.add_argument(
+    "--additional-fields",
+    nargs='*',
+    default=None,
+    help="A list of strings to specify what fields in the assay to return, in addition to the default fields always returned, “assay_sample_id,” “allele_id,”  “chrom,”  “pos,”  “ref,”  “allele.” Supplied fields must be separated by commas. For a full list of what fields are available, use --additional-fields –help to learn more. Use “--additional-fields-help” to get the full list of output fields available.",
+)
+
+parser_extract_assay_somatic.add_argument(
+    "--additional-fields-help",
+    action="store_true",
+    help="List all fields available for output.",
+)
+
+parser_extract_assay_somatic.add_argument(
+    '--json-help',
+    help=argparse.SUPPRESS,
+    action="store_true",
+)
+
+parser_extract_assay_somatic.add_argument(
+    "--sql",
+    action="store_true",
+    help="If the flag is provided, a SQL statement (a string) will be returned for user to further query the specified data instead of actual value of the requested fields.",
+)
+
+parser_extract_assay_somatic.add_argument(
+    "-o", "--output", 
+    type=str,
+    default=None,
+    help="Path to store the output file."
+)
+
+parser_extract_assay_somatic.set_defaults(func=extract_assay_somatic)
+register_parser(parser_extract_assay_somatic)
+
 
 #####################################
 # help
