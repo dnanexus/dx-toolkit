bcda3b47110bfe959f5a472af2d717e99b8c03f0
Kurt Jensen <kjensen@dnanexus.com>
2023-06-22 17:24:43 -0400

    Remove unnecessary changes

--- src/python/dxpy/scripts/dx.py
+++ src/python/dxpy/scripts/dx.py
@@ -18,6 +18,7 @@
 #   under the License.
 
 from __future__ import print_function, unicode_literals, division, absolute_import
+
 import os, sys, datetime, getpass, collections, re, json, argparse, copy, hashlib, io, time, subprocess, glob, logging, functools
 import shlex # respects quoted substrings when splitting
 
--- src/python/dxpy/toolkit_version.py
+++ src/python/dxpy/toolkit_version.py
@@ -1,1 +1,1 @@
-version = '0.350.0+ge54ec1c37'
+version = '0.350.0'
