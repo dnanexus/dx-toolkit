5b0304d2ce66c74027fc98b99b11a99bc5fe1dbb
JoeMulka <joseph.mulka1@gmail.com>
2023-07-05 19:36:43 -0500

    removing shlex import

--- src/python/dxpy/scripts/dx.py
+++ src/python/dxpy/scripts/dx.py
@@ -20,7 +20,6 @@
 from __future__ import print_function, unicode_literals, division, absolute_import
 
 import os, sys, datetime, getpass, collections, re, json, argparse, copy, hashlib, io, time, subprocess, glob, logging, functools
-import shlex # respects quoted substrings when splitting
 
 import requests
 import csv
