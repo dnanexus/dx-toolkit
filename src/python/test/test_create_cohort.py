f259416c0726eb9d59dcc4dfb982adf83feedb3c
JoeMulka <joseph.mulka1@gmail.com>
2023-08-14 12:49:24 -0500

    creating placeholders for remaining tests

--- src/python/test/test_create_cohort.py
+++ src/python/test/test_create_cohort.py
@@ -30,6 +30,7 @@ import subprocess
 import pandas as pd
 import dxpy
 import sys
+import hashlib
 from dxpy_testutil import cd, chdir
 
 from dxpy.cli.dataset_utilities import (
@@ -60,16 +61,58 @@ class TestCreateCohort(unittest.TestCase):
     def test_help_text(self):
         print("testing help text")
 
+        # An MD5sum hash of the correct error message
         expected_result = "fae9f07f1aad8cf69223ca666b20de35"
 
         command = 'dx create_cohort --help'
 
         process = subprocess.check_output(command, shell=True,text=True)
 
+        # Get the md5sum hash of the captured error message
         test_md5sum = hashlib.md5(process.encode("utf-8")).hexdigest()
 
         self.assertEqual(expected_result,test_md5sum)
 
+    # Supplied IDs do not match IDs of main entity in Dataset/Cohort
+    def test_errmsg_id_match(self):
+        pass
+
+    # The structure of “Path” is invalid. This should be able to be reused from other dx functions
+    def test_errmsg_invalid_path(self):
+        pass
+
+    # The user does not have access to the object
+    def test_errmsg_no_data_access(self):
+        pass
+
+    # The record id or path is not a cohort or dataset
+    def test_errmsg_not_cohort_dataset(self):
+        pass
+
+    # The record id or path is a cohort or dataset but is invalid (maybe corrupted, descriptor not accessible...etc)
+    def test_errmsg_invalid_record(self):
+        pass
+    
+    # The record id or path is a cohort or dataset but the version is less than 3.0.
+    def test_errmsg_dataset_version(self):
+        pass
+
+    # If PATH is of the format `project-xxxx:folder/` and the project does not exist
+    def test_errmsg_project_not_exist(self):
+        pass
+    
+    # If PATH is of the format `project-xxxx:folder/` and the user does not have CONTRIBUTE or ADMINISTER access
+    # Note that this is the PATH that the output cohort is being created in, not the input dataset or cohort
+    def test_errmsg_no_path_access(self):
+        pass
+
+    # If PATH is of the format `folder/subfolder/` and the path does not exist
+    def test_errmsg_subfolder_not_exist(self):
+        pass
+
+    # If both --cohort-ids and --cohort-ids-file are supplied in the same call
+    def test_errmsg_incompat_args(self):
+        pass
 
     def test_retrieve_cohort_id(self):
         pass
