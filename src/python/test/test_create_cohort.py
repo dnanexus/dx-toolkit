4bb2aa4c9b32d4c3a37f3fc7638f3afa5b835c5f
piader1 <mstevuliak@dnanexus.com>
2023-08-14 15:57:17 +0200

    Set up testing environment

---
+++ src/python/test/test_create_cohort.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 DNAnexus, Inc.
+#
+# This file is part of dx-toolkit (DNAnexus platform client libraries).
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#   use this file except in compliance with the License. You may obtain a copy
+#   of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+import unittest
+import tempfile
+import shutil
+import os
+import re
+import subprocess
+import pandas as pd
+import dxpy
+from dxpy_testutil import cd, chdir
+
+from dxpy.cli.dataset_utilities import (
+    create_cohort
+)
+
+class TestDXCreateCohort(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        proj_name = "dx-toolkit_test_data"
+        proj_id = list(dxpy.find_projects(describe=False, level='VIEW', name=proj_name))[0]['id']
+        cd(proj_id + ":/")
+        cls.general_input_dir = os.path.join(dirname, "create_cohort_test_files/input/")
+        # cls.general_output_dir = os.path.join(dirname, "create_cohort_test_files/output/")
+
+        #TODO: setup project folders 
+        cls.test_record = "{}:/Create_Cohort/somatic_indels_1k".format(
+                proj_name
+            )
