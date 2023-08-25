891e0dc1cc805539f666b59f10192c4ce4568110
Christian Suloway <csuloway@dnanexus.com>
2023-08-25 11:50:00 -0400

    update comments and formatting in cohort_filter_payload

--- src/python/dxpy/dx_extract_utils/cohort_filter_payload.py
+++ src/python/dxpy/dx_extract_utils/cohort_filter_payload.py
@@ -1,21 +1,27 @@
 import copy
 
-from ..exceptions import InvalidInput
-
 
 def generate_pheno_filter(values, entity, field, cohort_filter):
     cohort_filter = copy.deepcopy(cohort_filter)
 
     if "pheno_filters" not in cohort_filter:
-        # No pheno_filter in filters so create a compound template
+        # Create a pheno_filter if none exists. This will be a compound filter
+        # even though only be one filter defined for the entity field values.
         cohort_filter["pheno_filters"] = {"compound": [], "logic": "and"}
 
     if "compound" not in cohort_filter["pheno_filters"]:
-        # No compound filter in pheno_filter so move the existing pheno_fitler into a compound filter
+        # Only compound pheno_filters are supported, and the existing
+        # pheno_filter is not a compound filter. Move the pheno_filter into a
+        # compound filter so a filter for the primary entity field values can
+        # be added.
         cohort_filter["pheno_filters"] = {"compound": [cohort_filter["pheno_filters"]], "logic": "and"}
     elif "logic" in cohort_filter["pheno_filters"] and cohort_filter["pheno_filters"]["logic"] != "and":
-        print("WARNING: pheno_filters logic is not \"and\", creating a nested compound filter")
-        # The compound filter in the pheno_filter is not "and" so move the existing pheno_fitler into a compound filter
+        # pheno_filter is a compound filter, but the logic is not "and" so
+        # entity field values cannot be selected by adding a filter to the
+        # existing compound pheno_filter. Move the existing pheno_filter into a
+        # new pheno_filter level compound filter.
+        print("WARNING: pheno_filter compound filter logic is not \"and\", creating a nested compound filter."
+              " The Cohort Browser may not work as expected with this cohort.")
         cohort_filter["pheno_filters"] = {"compound": [cohort_filter["pheno_filters"]], "logic": "and"}
 
     entity_field = "$".join([entity, field])
@@ -25,10 +31,11 @@ def generate_pheno_filter(values, entity, field, cohort_filter):
     for compound_filter in cohort_filter["pheno_filters"]["compound"]:
         try:
             if "logic" in compound_filter and compound_filter["logic"] != "and":
-                # The entity and field filter does not have "and" logic so fall though
-                print("WARNING: found entity field filter, but logic is not \"and\"")
+                # The entity field filter does not have "and" logic so fall though
+                print("WARNING: found filter for entity \"{}\", field \"{}\" but logic is not \"and\","
+                      " skipping".format(entity, field))
             else:
-                # The entity and field filter is valid for addition of the "in" values condition
+                # The entity field filter is valid for addition of the "in" values condition
                 compound_filter["filters"][entity_field].append(entity_field_filter)
                 return cohort_filter
         except KeyError:
@@ -41,7 +48,7 @@ def generate_pheno_filter(values, entity, field, cohort_filter):
         if compound_filter["entity"]["name"] == entity:
             if "logic" in compound_filter and compound_filter["logic"] != "and":
                 # The entity filter does not have "and" logic so fall though
-                print("WARNING: found entity filter, but logic is not \"and\"")
+                print("WARNING: found filter for entity \"{}\" but logic is not \"and\", skipping".format(entity))
             else:
                 # The entity filter is valid for addition of field filter
                 compound_filter["filters"][entity_field] = [entity_field_filter]
@@ -52,12 +59,13 @@ def generate_pheno_filter(values, entity, field, cohort_filter):
         if "filters" not in compound_filter:
             continue
         for other_entity_field in compound_filter["filters"]:
+            print(compound_filter)
             if other_entity_field.split("$")[0] == entity:
                 if "logic" in compound_filter and compound_filter["logic"] != "and":
-                    # The entity filter does not have "and" logic so fall though
-                    print("WARNING: found entity filter, but logic is not \"and\"")
+                    # The filter with entity does not have "and" logic so fall though
+                    print("WARNING: found filter with entity \"{}\" but logic is not \"and\", skipping".format(entity))
                 else:
-                    # The entity filter is valid for addition of field filter
+                    # Filter with the entity is valid for addition of field filter
                     compound_filter["filters"][entity_field] = [entity_field_filter]
                     return cohort_filter
 
