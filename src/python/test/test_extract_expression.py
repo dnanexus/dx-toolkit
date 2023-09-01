d24cffe3c9435874807f0c2e05b3d0541768e395
JoeMulka <joseph.mulka1@gmail.com>
2023-09-01 18:09:44 -0400

    start of test script and test input filters

---
+++ src/python/test/test_extract_expression.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 DNAnexus, Inc.
+#
+# This file is part of dx-toolkit (DNAnexus platform client libraries).
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#   use this file except in compliance with the License. You may obtain a copy
+#   of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+
+# Run manually with python3 src/python/test/test_extract_expression.py
+
+import unittest
+import subprocess
+import sys
+import os
+import dxpy
+from dxpy_testutil import cd, chdir
+
+dirname = os.path.dirname(__file__)
+
+python_version = sys.version_info.major
+
+class TestDXExtractExpression(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        test_project_name = "dx-toolkit_test_data"
+        cls.proj_id = list(
+            dxpy.find_projects(describe=False, level="VIEW", name=test_project_name)
+        )[0]["id"]
+        cd(cls.proj_id + ":/")
+        cls.general_input_dir = os.path.join(dirname, "expression_test_files/input/")
+        cls.general_output_dir = os.path.join(dirname, "expression_test_files/output/")
+
+    def bad_toplevel_key(self):
+        pass
