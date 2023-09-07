96e40721ccf58cfde2d8413926d3a9957fcfc6ec
piader1 <mstevuliak@dnanexus.com>
2023-09-07 15:52:58 +0200

    fix Non-ASCII character

--- src/python/dxpy/dx_extract_utils/cohort_filter_payload.py
+++ src/python/dxpy/dx_extract_utils/cohort_filter_payload.py
@@ -19,7 +19,7 @@ def generate_pheno_filter(values, entity, field, filters):
         # pheno_filter is a compound filter, but the logic is not "and" so
         # entity field values cannot be selected by adding a filter to the
         # existing compound pheno_filter.
-        raise ValueError("Invalid input cohort. Cohorts must have “and” logic on the primary entity and field.")
+        raise ValueError("Invalid input cohort. Cohorts must have 'and' logic on the primary entity and field.")
 
     entity_field = "$".join([entity, field])
     entity_field_filter = {"condition": "in", "values": values}
