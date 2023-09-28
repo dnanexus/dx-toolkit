9cc3fa6b0c3c95a877d926647c9dda0e35721d46
JoeMulka <joseph.mulka1@gmail.com>
2023-09-28 13:26:10 -0500

    Sciprod 1647 test dataset validation (#1193)
    * adding tests for incorrect record type and invalid dataset version
    
    * starting import of functions responsible for path validation
    
    * reconfiguring tests to directly test functions
    
    * creating standard path validation test function
    
    * removing redundant resolve existing path call
    
    * updating test record
    
    * checking for type in type field
    
    * renaming error handler
    
    * Update src/python/dxpy/bindings/apollo/path_validator.py
    
    Co-authored-by: Ashkan Z. <89841417+ashkan0x@users.noreply.github.com>
    
    * reducing redundancy
    
    * investigating record_http_request_info object
    
    * clarifying what is being printed
    
    * updating path validation tests to reflect updates to path validator
    
    * skipping additional tests
    
    ---------
    
    Co-authored-by: Ashkan Z. <89841417+ashkan0x@users.noreply.github.com>

--- src/python/test/test_extract_expression.py
+++ src/python/test/test_extract_expression.py
@@ -25,12 +25,16 @@ import subprocess
 import sys
 import os
 import dxpy
+import copy
 import json
 import tempfile
 
 import shutil
 from dxpy_testutil import cd, chdir
 from dxpy.bindings.apollo.ValidateJSONbySchema import JSONValidator
+from dxpy.bindings.apollo.path_validator import PathValidator
+from dxpy.utils.resolver import resolve_existing_path
+
 from dxpy.bindings.apollo.assay_filtering_json_schemas import (
     EXTRACT_ASSAY_EXPRESSION_JSON_SCHEMA,
 )
@@ -55,8 +59,8 @@ class TestDXExtractExpression(unittest.TestCase):
         cd(cls.proj_id + ":/")
         cls.general_input_dir = os.path.join(dirname, "expression_test_files/input/")
         cls.general_output_dir = os.path.join(dirname, "expression_test_files/output/")
-        cls.schema = EXTRACT_ASSAY_EXPRESSION_JSON_SCHEMA
-        #cls.test_record = cls.proj_id + ":/Extract_Expression/standin_test_record"
+        cls.json_schema = EXTRACT_ASSAY_EXPRESSION_JSON_SCHEMA
+        cls.input_args_schema = EXTRACT_ASSAY_EXPRESSION_INPUT_ARGS_SCHEMA
         cls.test_record = "project-G5Bzk5806j8V7PXB678707bv:record-GYPg9Jj06j8pp3z41682J23p"
         cls.cohort_browser_record = (
             cls.proj_id + ":/Extract_Expression/cohort_browser_object"
@@ -98,9 +102,66 @@ class TestDXExtractExpression(unittest.TestCase):
         if not os.path.exists(cls.general_output_dir):
             os.makedirs(cls.general_output_dir)
 
+        cls.default_entity_describe = {
+            "id": cls.test_record,
+            "project": cls.proj_id,
+            "class": "record",
+            "sponsored": False,
+            "name": "fake_assay",
+            "types": ["Dataset"],
+            "state": "closed",
+            "hidden": False,
+            "links": ["database-xxxx", "file-zzzzzz"],
+            "folder": "/",
+            "tags": [],
+            "created": 0,
+            "modified": 0,
+            "createdBy": {
+                "user": "user-test",
+                "job": "job-xyz",
+                "executable": "app-xyz",
+            },
+            "size": 0,
+            "properties": {},
+            "details": {
+                "descriptor": {"$dnanexus_link": "file-xyz"},
+                "version": "3.0",
+                "schema": "ds-molecular_expression_quantification",
+                "databases": [{"assay": {"$dnanexus_link": "database-yyyyyy"}}],
+                "name": "fake_assay",
+                "description": "Dataset: assay",
+            },
+        }
+
+        cls.default_parser_dict = {
+            "apiserver_host": None,
+            "apiserver_port": None,
+            "apiserver_protocol": None,
+            "project_context_id": None,
+            "workspace_id": None,
+            "security_context": None,
+            "auth_token": None,
+            "env_help": None,
+            "version": None,
+            "command": "extract_assay",
+            "path": None,
+            "list_assays": False,
+            "retrieve_expression": False,
+            "additional_fields_help": False,
+            "assay_name": None,
+            "input_json": None,
+            "input_json_file": None,
+            "json_help": False,
+            "sql": False,
+            "additional_fields": None,
+            "expression_matrix": False,
+            "delim": None,
+            "output": None,
+        }
+
     @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls.general_output_dir)
+    def path_validation_error_handler(cls, message):
+        raise ValueError(message)
 
     @classmethod
     def input_arg_error_handler(cls, message):
@@ -110,9 +171,34 @@ class TestDXExtractExpression(unittest.TestCase):
     def json_error_handler(cls, message):
         raise ValueError(message)
 
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.general_output_dir)
+
+    #
+    # Helper functions used by different types of tests
+    #
+
+    def common_negative_path_validation_test(
+        self, expected_error_message, parser_dict, entity_describe
+    ):
+        validator = PathValidator(
+            parser_dict,
+            self.proj_id,
+            entity_describe,
+            error_handler=self.path_validation_error_handler,
+        )
+
+        with self.assertRaises(ValueError) as cm:
+            validator.validate()
+
+        self.assertEqual(expected_error_message, str(cm.exception).strip())
+
     def common_negative_filter_test(self, json_name, expected_error_message):
         input_json = CLIEXPRESS_TEST_INPUT["malformed"][json_name]
-        validator = JSONValidator(self.schema, error_handler=self.json_error_handler)
+        validator = JSONValidator(
+            self.json_schema, error_handler=self.json_error_handler
+        )
 
         with self.assertRaises(ValueError) as cm:
             validator.validate(input_json)
@@ -122,7 +208,9 @@ class TestDXExtractExpression(unittest.TestCase):
     def common_positive_filter_test(self, json_name):
         input_json = CLIEXPRESS_TEST_INPUT["valid"][json_name]
 
-        validator = JSONValidator(self.schema, error_handler=self.json_error_handler)
+        validator = JSONValidator(
+            self.json_schema, error_handler=self.json_error_handler
+        )
 
         validator.validate(input_json)
 
@@ -518,6 +606,122 @@ class TestDXExtractExpression(unittest.TestCase):
     
 
     #
+    # Path Validation tests
+    # There are 5 ways this function can detect a bad dataset/path.  Checked in the following order:
+    # 1. (EM-1?) Object in wrong project
+    # 2. (EM-1?) Object not of class record
+    # 3. (EM-3) Object not of recordType "Dataset" or "CohortBrowser"
+    # 4. (EM-5) Object is not of correct version (3.0 at the time of this writing)
+    # 5. (EM-6) Object is CohortBrowser type and --assay-name or --list-assays has been given on the command line
+    #
+
+    # EM-1
+    # 1. Object in wrong project
+    def test_bad_dataset_project(self):
+        # deep copy the standard entity describe and parser dictionaries
+        entity_describe = copy.deepcopy(self.default_entity_describe)
+        parser_dict = copy.deepcopy(self.default_parser_dict)
+        parser_dict["path"] = "{}:{}".format(self.proj_id, self.test_record)
+        # Overwrite project, but not record id, so there is a project
+        entity_describe["project"] = "project-fakeproject419857"
+        expected_error_message = 'Unable to resolve "{}:{}" to a data object or folder name in {}. Please make sure your object is in your selected project.'.format(
+            self.proj_id, self.test_record, self.proj_id
+        )
+
+        self.common_negative_path_validation_test(
+            expected_error_message, parser_dict, entity_describe
+        )
+
+    # EM-1
+    # 2. Object not of class record
+    def test_object_not_class_record(self):
+        # deep copy the standard entity describe and parser dictionaries
+        entity_describe = copy.deepcopy(self.default_entity_describe)
+        parser_dict = copy.deepcopy(self.default_parser_dict)
+        parser_dict["path"] = "{}:{}".format(self.proj_id, self.test_record)
+        entity_describe["class"] = "not_record"
+        expected_error_message = "Invalid path. The path must point to a record type of cohort or dataset and not a {} object.".format(
+            entity_describe["class"]
+        )
+
+        self.common_negative_path_validation_test(
+            expected_error_message, parser_dict, entity_describe
+        )
+
+    # EM-3
+    # 3. Object not of recordType "Dataset" or "CohortBrowser"
+    def test_bad_dataset_type(self):
+        entity_describe = copy.deepcopy(self.default_entity_describe)
+        parser_dict = copy.deepcopy(self.default_parser_dict)
+        parser_dict["path"] = "{}:{}".format(self.proj_id, self.test_record)
+        # Overwrite type in default entity_describe dict with something other than CohortBrowser or Dataset
+        entity_describe["types"] = ["bad_type"]
+        expected_error_message = "{} Invalid path. The path must point to a record type of cohort or dataset and not a ['bad_type'] object.".format(
+            entity_describe["id"]
+        )
+
+        self.common_negative_path_validation_test(
+            expected_error_message, parser_dict, entity_describe
+        )
+
+    # EM-5
+    # 4. Object is not of correct version (3.0 at the time of this writing)
+    def test_bad_dataset_version(self):
+        entity_describe = copy.deepcopy(self.default_entity_describe)
+        parser_dict = copy.deepcopy(self.default_parser_dict)
+        parser_dict["path"] = "{}:{}".format(self.proj_id, self.test_record)
+        entity_describe["details"]["version"] = "2.0"
+        expected_error_message = "2.0: Version of the cohort or dataset is too old. Version must be at least 3.0.".format(
+            self.test_record
+        )
+
+        self.common_negative_path_validation_test(
+            expected_error_message, parser_dict, entity_describe
+        )
+
+    # (EM-6)
+    # 5. Object is CohortBrowser type and --assay-name or --list-assays has been given on the command line
+    def test_cohort_browser_assay_name(self):
+        entity_describe = copy.deepcopy(self.default_entity_describe)
+        parser_dict = copy.deepcopy(self.default_parser_dict)
+        parser_dict["path"] = "{}:{}".format(self.proj_id, self.test_record)
+        entity_describe["types"] = ["CohortBrowser"]
+        parser_dict["assay_name"] = True
+        expected_error_message = "Currently --assay-name and --list-assays may not be used with a CohortBrowser record (Cohort Object) as input. To select a specific assay or to list assays, please use a Dataset Object as input."
+        self.common_negative_path_validation_test(
+            expected_error_message, parser_dict, entity_describe
+        )
+
+    # (EM-6)
+    # 5. Object is CohortBrowser type and --assay-name or --list-assays has been given on the command line
+    def test_cohort_browser_list_assays(self):
+        entity_describe = {
+            key: value for key, value in self.default_entity_describe.items()
+        }
+        parser_dict = {key: value for key, value in self.default_parser_dict.items()}
+        parser_dict["path"] = "{}:{}".format(self.proj_id, self.test_record)
+        entity_describe["types"] = ["CohortBrowser"]
+        parser_dict["list_assays"] = True
+        expected_error_message = "Currently --assay-name and --list-assays may not be used with a CohortBrowser record (Cohort Object) as input. To select a specific assay or to list assays, please use a Dataset Object as input."
+        self.common_negative_path_validation_test(
+            expected_error_message, parser_dict, entity_describe
+        )
+
+    def test_positive_path_validation(self):
+        entity_describe = {
+            key: value for key, value in self.default_entity_describe.items()
+        }
+        parser_dict = {key: value for key, value in self.default_parser_dict.items()}
+        parser_dict["path"] = "{}:{}".format(self.proj_id, self.test_record)
+        validator = PathValidator(
+            parser_dict,
+            self.proj_id,
+            entity_describe,
+            error_handler=self.path_validation_error_handler,
+        )
+        validator.validate()
+
+    #
     # Malformed input json tests
     # EM-18, EM-19, EM-20
     #
@@ -528,6 +732,7 @@ class TestDXExtractExpression(unittest.TestCase):
             "Conflicting keys feature_name and feature_id cannot be present together.",
         )
 
+    @unittest.skip
     def test_annotation_id_maxitem(self):
         self.common_negative_filter_test(
             "annotation_id_maxitem", "error message not yet defined"
@@ -539,6 +744,7 @@ class TestDXExtractExpression(unittest.TestCase):
             "Key 'feature_id' has an invalid type. Expected <{0} 'list'> but got <{0} 'dict'>".format(self.type_representation).format(self.type_representation),
         )
 
+    @unittest.skip
     def test_annotation_name_maxitem(self):
         self.common_negative_filter_test(
             "annotation_name_maxitem",
@@ -610,6 +816,7 @@ class TestDXExtractExpression(unittest.TestCase):
             "Key 'chromosome' has an invalid type. Expected <{0} 'str'> but got <{0} 'int'>".format(self.type_representation),
         )
 
+    @unittest.skip
     def test_location_end_before_start(self):
         self.common_negative_filter_test(
             "location_end_before_start", "error message not yet defined"
@@ -626,6 +833,7 @@ class TestDXExtractExpression(unittest.TestCase):
             "location_item_type", "Expected items of type <{0} 'dict'> but got <{0} 'list'>".format(self.type_representation)
         )
 
+    @unittest.skip
     def test_location_max_width(self):
         self.common_negative_filter_test(
             "location_max_width", "error message not yet defined"
@@ -660,6 +868,7 @@ class TestDXExtractExpression(unittest.TestCase):
             "location_type", "Key 'location' has an invalid type. Expected <{0} 'list'> but got <{0} 'dict'>".format(self.type_representation)
         )
 
+    @unittest.skip
     def test_sample_id_maxitem(self):
         self.common_negative_filter_test(
             "sample_id_maxitem", "error message not yet defined"
