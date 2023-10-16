88353a7e1e7292cde30fe90da84577fe050e59ce
ashkan0x <89841417+ashkan0x@users.noreply.github.com>
2023-10-16 13:50:39 +0200

    unset shell=True

--- src/python/test/test_extract_expression.py
+++ src/python/test/test_extract_expression.py
@@ -1133,7 +1133,6 @@ class TestDXExtractExpression(unittest.TestCase):
 
         process = subprocess.check_output(
             command,
-            shell=True,
             universal_newlines=True,
         )
         return process
