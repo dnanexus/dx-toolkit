b3e16f55ebcf5e5040411c566094556c81ac6ad5
Jan Stourac <jstourac@dnanexus.com>
2023-12-19 09:28:50 +0100

    better version implementation

--- src/python/dxpy/utils/version.py
+++ src/python/dxpy/utils/version.py
@@ -6,43 +6,23 @@ class Version:
         if len(parts) > 3:
             raise ValueError("Unsupported version format")
 
-        self.major = int(parts[0])
-        self.minor = None
-        self.patch = None
-        if len(parts) > 1:
-            self.minor = int(parts[1])
-        if len(parts) > 2:
-            self.patch = int(parts[2])
-
-    def _cmp(self, other):
-        if self.major > other.major:
-            return 1
-        if self.major < other.major:
-            return -1
-
-        if (self.minor or 0) > (other.minor or 0):
-            return 1
-        if (self.minor or 0) < (other.minor or 0):
-            return -1
-
-        if (self.patch or 0) > (other.patch or 0):
-            return 1
-        if (self.patch or 0) < (other.patch or 0):
-            return -1
-
-        return 0
+        self.version = (
+            int(parts[0]),
+            int(parts[1]) if len(parts) > 1 else 0,
+            int(parts[2]) if len(parts) > 2 else 0,
+        )
 
     def __eq__(self, other):
-        return self._cmp(other) == 0
+        return self.version == other.version
 
     def __lt__(self, other):
-        return self._cmp(other) < 0
+        return self.version < other.version
 
     def __le__(self, other):
-        return self._cmp(other) <= 0
+        return self.version <= other.version
 
     def __gt__(self, other):
-        return self._cmp(other) > 0
+        return self.version > other.version
 
     def __ge__(self, other):
-        return self._cmp(other) >= 0
+        return self.version >= other.version
