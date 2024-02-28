0b42bbf530a42b1d5f8bdcbe4922b2d9611018fe
Matej Stevuliak <mstevuliak@dnanexus.com>
2024-02-28 12:33:08 +0100

    Use warning instead of exit

--- src/python/dxpy/cli/dataset_utilities.py
+++ src/python/dxpy/cli/dataset_utilities.py
@@ -703,20 +703,40 @@ def validate_infer_flags(args, ex_nocall, ex_ref, ex_halfref):
                 f"The --infer-nocall flag can only be used when exclusion parameters at ingestion were set to 'exclude_nocall=true', 'exclude_halfref=false', and 'exclude_ref=false'.\n{ingestion_parameters_str}"
             )
 
-def validate_filter_applicable_genotype_types(infer_nocall, infer_ref, filter_dict, ex_ref, ex_nocall):
+def validate_filter_applicable_genotype_types(infer_nocall, infer_ref, filter_dict, ex_ref, ex_nocall, ex_halfref):
     # Check filter provided genotype_types against exclusion options at ingestion. 
     # e.g. no-call is not applicable when exclude_genotype set and infer-nocall false
     if "genotype_type" in filter_dict:
         if ex_nocall == True and not infer_nocall:
             if "no-call" in filter_dict["genotype_type"]:
-                err_exit(
-                    "The genotype type 'no-call' is not applicable when the dataset ingestion was set to exclude no-call genotypes and the --infer-nocall flag is not set."
+                print(
+                    "WARNING: Filter requested genotype type 'no-call', genotype entries of this type were not ingested in the provided dataset and the --infer-nocall flag is not set!"
+                )
+            if filter_dict["genotype_type"] == []:
+                print(
+                    "WARNING: No genotype type requested in the filter. All genotype types will be returned. Genotype entries of type 'no-call' were not ingested in the provided dataset and the --infer-nocall flag is not set!"
                 )
         if ex_ref == True and not infer_ref:
             if "ref" in filter_dict["genotype_type"]:
-                err_exit(
-                    "The genotype type 'ref' is not applicable when the dataset ingestion was set to exclude reference genotypes and the --infer-ref flag is not set."
+                print(
+                    "WARNING: Filter requested genotype type 'ref' genotype entries of this type were was not ingested in the provided dataset and the --infer-ref flag is not set."
                 )
+            if filter_dict["genotype_type"] == []:
+                print(
+                    "WARNING: No genotype type requested in the filter. All genotype types will be returned. Genotype entries of type 'ref' were not ingested in the provided dataset and the --infer-ref flag is not set!"
+                )
+        if ex_halfref == True:
+            if "half" in filter_dict["genotype_type"]:
+                print(
+                    "WARNING: Filter requested genotype type 'half', 'half-ref genotype' entries (0/.) were not ingested in the provided dataset!"
+                )
+            if filter_dict["genotype_type"] == []:
+                print(
+                    "WARNING: No genotype type requested in the filter. All genotype types will be returned.  'half-ref' genotype entries (0/.) were not ingested in the provided dataset!"
+                )
+        
+
+        
 
 
 
