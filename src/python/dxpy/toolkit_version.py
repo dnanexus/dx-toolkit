f8c9c24089a049a7db8a02c8dcc56a0a3bfed0aa
Jacob <jlecoq@dnanexus.com>
2024-03-04 11:27:27 -0600

    chore: pr comments

--- src/python/dxpy/toolkit_version.py
+++ src/python/dxpy/toolkit_version.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 version = '0.372.0'
+=======
+version = "0.370.0"
+>>>>>>> 3fad42822 (chore: pr comments)
--- src/python/test/jwt/run.sh
+++ src/python/test/jwt/run.sh
@@ -1,6 +1,11 @@
 main() {
     dx-download-all-inputs
 
+    # check if subject_claims is an empty string
+    if [ -z "$subject_claims" ]; then
+        subject_claims=
+    fi
+
     token=$(dx-jobutil-get-identity-token --aud "$audience" --subject_claims "$subject_claims")
 
     dx-jobutil-add-output token "$token"
