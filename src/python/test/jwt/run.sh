f04e3d9e98fdc54ccc796b03bc148759927c73d2
Hikari <lecoqjacob@gmail.com>
2024-03-04 16:12:28 -0600

    COMPUTE-413: make control flow better within jwt test script(#1337)

--- src/python/test/jwt/run.sh
+++ src/python/test/jwt/run.sh
@@ -3,10 +3,10 @@ main() {
 
     # check if subject_claims is an empty string
     if [ -z "$subject_claims" ]; then
-        subject_claims=
+        token=$(dx-jobutil-get-identity-token --aud "$audience")
+    else
+        token=$(dx-jobutil-get-identity-token --aud "$audience" --subject_claims "$subject_claims")
     fi
-
-    token=$(dx-jobutil-get-identity-token --aud "$audience" --subject_claims "$subject_claims")
 
     dx-jobutil-add-output token "$token"
 }
