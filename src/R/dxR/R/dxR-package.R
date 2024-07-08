46c757f4c5e119db343a4528402941a40dd532bf
Kurt Jensen <kjensen@dnanexus.com>
2024-07-08 14:34:00 +0000

    Version updates for v0.380.0

--- src/R/dxR/R/dxR-package.R
+++ src/R/dxR/R/dxR-package.R
@@ -4,7 +4,7 @@
 ##' the new DNAnexus platform.
 ##' 
 ##' \tabular{ll}{ Package: \tab dxR\cr Type: \tab Package\cr Version: \tab
-##' 0.379.0\cr License: \tab Apache License (== 2.0)\cr
+##' 0.380.0\cr License: \tab Apache License (== 2.0)\cr
 ##' }
 ##' 
 ##' @name dxR-package
