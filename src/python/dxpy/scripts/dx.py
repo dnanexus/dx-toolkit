e59fb016e0ed9bd91184f160529c61df45d9b826
Kurt Jensen <kjensen@dnanexus.com>
2024-12-09 16:52:13 -0500

    Default to False

--- src/python/dxpy/scripts/dx.py
+++ src/python/dxpy/scripts/dx.py
@@ -4805,7 +4805,7 @@ cp_sources_action = parser_cp.add_argument('sources', help='Objects and/or folde
                                            nargs='+')
 cp_sources_action.completer = DXPathCompleter()
 parser_cp.add_argument('destination', help=fill('Folder into which to copy the sources or new pathname (if only one source is provided).  Must be in a different project/container than all source paths.', width_adjustment=-15))
-parser_cp.add_argument('--target-file-relocation', help='Allow symlink target file relocation in external storage while cloning a symlink.', action='store_true')
+parser_cp.add_argument('--target-file-relocation', help='Allow symlink target file relocation in external storage while cloning a symlink.', action='store_true', default=False)
 parser_cp.set_defaults(func=cp)
 register_parser(parser_cp, categories='fs')
 
@@ -4821,7 +4821,7 @@ mv_sources_action = parser_mv.add_argument('sources', help='Objects and/or folde
                                            nargs='+')
 mv_sources_action.completer = DXPathCompleter()
 parser_mv.add_argument('destination', help=fill('Folder into which to move the sources or new pathname (if only one source is provided).  Must be in the same project/container as all source paths.', width_adjustment=-15))
-parser_mv.add_argument('--target-file-relocation', help='Allow symlink target file relocation in external storage while moving a symlink.', action='store_true')
+parser_mv.add_argument('--target-file-relocation', help='Allow symlink target file relocation in external storage while moving a symlink.', action='store_true', default=False)
 parser_mv.set_defaults(func=mv)
 register_parser(parser_mv, categories='fs')
 
