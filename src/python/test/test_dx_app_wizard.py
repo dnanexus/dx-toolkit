5bf9ba2062010e1daad9706f7b7332cd1189c9f0
jsitarova <jsitarova@dnanexus.com>
2025-02-25 15:27:56 +0100

    update tests

--- src/python/test/test_dx_app_wizard.py
+++ src/python/test/test_dx_app_wizard.py
@@ -153,7 +153,7 @@ class TestDXAppWizard(DXTestCase):
         self.assertEqual(dxapp_json['regionalOptions']['aws:us-east-1']['systemRequirements']['*']['instanceType'],
                          InstanceTypesCompleter.default_instance_type.Name)
         self.assertEqual(dxapp_json['runSpec']['distribution'], 'Ubuntu')
-        self.assertEqual(dxapp_json['runSpec']['release'], '20.04')
+        self.assertEqual(dxapp_json['runSpec']['release'], '24.04')
         self.assertEqual(dxapp_json['runSpec']['version'], '0')
         self.assertEqual(dxapp_json['runSpec']['interpreter'], 'python3')
         self.assertEqual(dxapp_json['runSpec']['timeoutPolicy']['*']['hours'], 24)
