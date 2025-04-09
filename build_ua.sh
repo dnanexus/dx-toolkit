f397f630270b560faf17afdb94c8297958b01df6
Riva Kepych <rkepych@dnanexus.com>
2025-04-09 20:01:43 +0200

    try configuring c-ares with --enable-static=yes

--- build_ua.sh
+++ build_ua.sh
@@ -6,6 +6,6 @@ docker build -t ua-builder -f "$(pwd)/Dockerfile" .
 # Build upload agent in the ua-builder container
 docker run --rm \
     -v "$(pwd):/dx-toolkit" \
-    -w /dx-toolkit/src \
+    -w /dx-toolkit \
     ua-builder \
     bash -c "make ua 2>&1 | tee make.log"
