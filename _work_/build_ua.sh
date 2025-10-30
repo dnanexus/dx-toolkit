6b23ce0fee069944c21346c64ec581576a30b70e
Riva Kepych <rkepych@dnanexus.com>
2025-10-30 17:17:12 +0100

    attempt build

--- _work_/build_ua.sh
+++ _work_/build_ua.sh
@@ -8,7 +8,8 @@ docker run --rm \
     -v "$(pwd):/dx-toolkit" \
     -w /dx-toolkit/src \
     ua-builder \
-    bash -c "make ua 2>&1 | tee ../_work_/build.log"
+    bash -c "make ua_deps 2>&1 | tee ../_work_/build.log"
+    # bash -c "make ua 2>&1 | tee ../_work_/build.log"
 
 # Run interactive
 # docker run -it -v "$(pwd):/dx-toolkit" -w /dx-toolkit/src ua-builder
