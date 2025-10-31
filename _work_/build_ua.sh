d09581188aae776db8aecae79aa61b4adbf5a7f6
Riva Kepych <rkepych@dnanexus.com>
2025-10-31 14:12:45 +0100

    image name

--- _work_/build_ua.sh
+++ _work_/build_ua.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Prepare the builder image
-docker build -t ua-builder -f "$(pwd)/_work_/Dockerfile" .
+docker build -t upload-agent-builder -f "$(pwd)/_work_/Dockerfile" .
 
-# Build upload agent in the ua-builder container
+# Build upload agent in the upload-agent-builder container
 docker run --rm \
     -v "$(pwd):/dx-toolkit" \
     -w /dx-toolkit/src \
-    ua-builder \
+    upload-agent-builder \
     bash -c "make ua_deps 2>&1 | tee ../_work_/build.log"
     # bash -c "make ua 2>&1 | tee ../_work_/build.log"
 
 # Run interactive
-# docker run -it -v "$(pwd):/dx-toolkit" -w /dx-toolkit/src ua-builder
+# docker run -it -v "$(pwd):/dx-toolkit" -w /dx-toolkit/src upload-agent-builder
