d9f3d53615aae960e7d55a7fffa4bc56f4d026a9
Riva Kepych <rkepych@dnanexus.com>
2025-11-13 00:08:49 +0100

    general make script

--- build/docker/make.sh
+++ build/docker/make.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Script for building Upload Agent with builder image.
+# Script for building dx-toolkit components  Upload Agent with builder image.
 # This could be extended to support building other dx-toolkit components.
 
-MAKE_CMD="make clean && make ua"
+# MAKE_CMD="dx-verify-file"
+MAKE_CMD="${1:-all}"
 
 # Prepare the builder image
 docker build -t upload-agent-builder -f "$(pwd)/build/docker/Dockerfile" .
@@ -13,4 +14,4 @@ docker run --rm \
     -v "$(pwd):/dx-toolkit" \
     -w /dx-toolkit/src \
     upload-agent-builder \
-    bash -c "${MAKE_CMD}"
+    bash -c "make ${MAKE_CMD}"
--- src/python/dxpy/toolkit_version.py
+++ src/python/dxpy/toolkit_version.py
@@ -1,1 +1,1 @@
-version = '0.400.0'
+version = ''
