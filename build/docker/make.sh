4dbc9466795f823c662e123ae0551b55ad99658c
Riva Kepych <rkepych@dnanexus.com>
2025-11-13 16:31:59 +0100

    rename to dx-toolkit-builder

--- build/docker/make.sh
+++ build/docker/make.sh
@@ -6,11 +6,11 @@ MAKE_CMD="clean dx-verify-file"
 # MAKE_CMD="${1:-all}"
 
 # Prepare the builder image
-docker build -t upload-agent-builder -f "$(pwd)/build/docker/Dockerfile" .
+docker build -t dx-toolkit-builder -f "$(pwd)/build/docker/Dockerfile" .
 
-# Build upload agent in the upload-agent-builder container
+# Build upload agent in the dx-toolkit-builder container
 docker run --rm \
     -v "$(pwd):/dx-toolkit" \
     -w /dx-toolkit/src \
-    upload-agent-builder \
+    dx-toolkit-builder \
     bash -c "make ${MAKE_CMD}"
